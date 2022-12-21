5b4137c169e3aa08abdaea2462ae610c04c75cec
krtonga <kristen.tonga@gmail.com>
2022-12-22 02:35:21 +0300

    Formatting and Copyright for login_route, missed in original commits

--- src/backend/auth/login_route.py
+++ src/backend/auth/login_route.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2020, 2021, 2022 Humanitarian OpenStreetMap Team
+#
+# This file is part of FMTM.
+#
+#     FMTM is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     FMTM is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with FMTM.  If not, see <https:#www.gnu.org/licenses/>.
+#
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -10,6 +28,7 @@ router = APIRouter(
     dependencies=[Depends(database.get_db)],
     responses={404: {"description": "Not found"}},
 )
+
 
 @router.post("/", response_model=user_schemas.UserOut)
 def login(user: user_schemas.UserIn, db: Session = Depends(database.get_db)):
