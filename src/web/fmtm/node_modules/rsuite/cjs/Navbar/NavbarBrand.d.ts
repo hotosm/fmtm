3fbd3867fb4a6e2b8af4dd9ad1403c5f335a862a
krtonga <kristen.tonga@gmail.com>
2023-01-05 03:47:49 +0300

    Removed node_modules

--- src/web/fmtm/node_modules/rsuite/cjs/Navbar/NavbarBrand.d.ts
+++
@@ -1,4 +0,0 @@
-import { ComponentProps } from '../utils/createComponent';
-export declare type NavbarBrandProps = ComponentProps;
-declare const NavbarBrand: import("../@types/common").RsRefForwardingComponent<"div", ComponentProps>;
-export default NavbarBrand;
