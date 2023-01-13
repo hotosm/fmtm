5d81bd50dd8248116019c187b8ab6e3548651558
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-13 15:37:15 +0300

    restored login

--- src/backend/main.py
+++ src/backend/main.py
@@ -25,6 +25,7 @@ from fastapi import FastAPI
 from fastapi.logger import logger as fastapi_logger
 from fastapi.responses import FileResponse
 
+from .auth import login_route
 from .auth import routers as auth_routers
 from .db.database import Base, SessionLocal, engine
 from .projects import project_routes
@@ -57,8 +58,10 @@ Base.metadata.create_all(bind=engine)
 api = FastAPI()
 
 api.include_router(user_routes.router)
+api.include_router(login_route.router)
 api.include_router(project_routes.router)
 api.include_router(tasks_routes.router)
+
 
 api.include_router(auth_routers)
 
