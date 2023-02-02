419042792618efc0e6fb38fd96cae2c02f44a44d
ivangayton <ivangayton@gmail.com>
2023-02-02 13:41:12 +0900

    Extract csv and image submissions from zip archive after download

--- old_scripts/odk_fieldmap_original/utils/dl_submissions.py
+++ old_scripts/odk_fieldmap_original/utils/dl_submissions.py
@@ -31,6 +31,10 @@
 import os
 import sys
 
+from io import BytesIO, StringIO
+from zipfile import ZipFile as zf
+import csv
+
 from odk_requests import (forms, submissions, csv_submissions)
 
 def project_forms(url, aut, pid):
@@ -43,12 +47,53 @@ def project_forms(url, aut, pid):
 def project_submissions(url, aut, pid, formsl, outdir):
     """Downloads all of the submissions frm a given ODK Central project"""
     for form in formsl:
-        form_id = (form['xmlFormId'])
+        form_id = form['xmlFormId']
         print(f'Checking submissions from {form_id}.')
         subs_zip = csv_submissions(url, aut, pid, form_id)
+        
         outfilename = os.path.join(outdir, f'{form_id}.csv.zip')
         outfile = open(outfilename, 'wb')
         outfile.write(subs_zip.content)
+
+def project_submissions_unzipped(url, aut, pid, formsl, outdir):
+    """Downloads and unzips all of the submissions from a given ODK project"""
+    collated_project_csv_outfile = 
+    for form in formsl:
+        form_id = form['xmlFormId']
+        print(f'Checking submissions from {form_id}.')
+        subs_zip = csv_submissions(url, aut, pid, form_id)
+        subs_bytes = BytesIO(subs_zip.content)
+        subs_bytes.seek(0)
+        subs_unzipped = zf(subs_bytes)
+        sub_namelist = subs_unzipped.namelist()
+        print(f'Files in submissions from {form_id}:')
+        print(sub_namelist)
+        for sub_name in sub_namelist:
+            subs_bytes = subs_unzipped.read(sub_name)
+            outfilename = os.path.join(outdir, sub_name)
+            
+            # Some attachments need a subdirectory
+            suboutdir = os.path.split(outfilename)[0])
+            if not os.path.exists(suboutdir):
+                os.makedirs(suboutdir)
+
+            # If it is a csv, open it and see if it is more than one line
+            # This might go wrong if something is encoded in other than UTF-8
+            if os.path.splitext(sub_name)[1] == '.csv':
+                subs_stringio = StringIO(subs_bytes.decode())
+                subs_list = list(csv.reader(subs_stringio))
+                # Check if there are CSV lines after the headers
+                subs_num = len(subs_list)
+                print(f'{sub_name} has {subs_num - 1} submissions')
+                if subs_num > 1:
+                    with open(outfilename, 'w') as outfile:
+                        w = csv.writer(outfile)
+                        w.writerows(subs_list)
+                    
+                        
+            else:
+                with open(outfilename, 'wb') as outfile:
+                    outfile.write(subs_bytes)
 
 if __name__ == "__main__":
     """Downloads all of the submissions from a given ODK Central project"""
@@ -58,5 +103,5 @@ if __name__ == "__main__":
     outdir = sys.argv[5]
     
     formsl = project_forms(url, aut, pid)
-    subs = project_submissions(url, aut, pid, formsl, outdir)
+    subs = project_submissions_unzipped(url, aut, pid, formsl, outdir)
     
