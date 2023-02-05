baa56eb7fdd117921669c9d0d680a8052d3126d2
Rob Savoye <rob@senecass.com>
2023-02-05 14:23:09 -0700

    Put all ODK Central support here.

--- src/backend/central/central_crud.py
+++ src/backend/central/central_crud.py
@@ -24,33 +24,49 @@ from sqlalchemy.orm import Session
 from typing import List
 import json
 import base64
+import epdb
+from fastapi.responses import FileResponse
 
+from ..env_utils import is_docker, config_env
 from ..db import db_models
 from ..db.postgis_utils import geometry_to_geojson, get_centroid
 from ..models.enums import TaskStatus, TaskAction, get_action_for_status_change, verify_valid_status_update
 from ..users import user_crud, user_schemas
 from ..tasks import tasks_schemas
 from ..central import central_schemas
-# from pyodk.client import Client
-
-import csv
-from fastapi.responses import FileResponse
+from ..odkconvert.OdkCentral import OdkProject, OdkAppUser, OdkForm
 
 
-class OdkCentral(object):
-    """Test class for fastapi"""
+from ..env_utils import is_docker, config_env
 
-    def __init__(self):
-        pass
+# FIXME: I am not sure this is thread-safe
+project = OdkProject()
+project.listProjects()
+xform = OdkForm()
 
-    def exists(self):
-        return "Yes I exists!"
+url = config_env["ODK_CENTRAL_URL"]
+user = config_env["ODK_CENTRAL_USER"]
+pw = config_env["ODK_CENTRAL_PASSWD"]
+project.authenticate(url, user, pw)
 
-# odk = OdkCentral()
+def create_odk_project(name):
+    result = project.createProject(name)
+    project.id = result['id']
+    logger.info(f"Project {name} has been created on the ODK Central server.")
+    return result
 
+def delete_odk_project(project_id: int):
+    result = project.deleteProject(project_id)
+    logger.info(f"Project {project_id} has been from the ODK Central server.")
+    return result
 
-def does_central_exist():
-    # xxx = odk.exists()
-    xxx = "Yes, Central exists"
-    logger.debug("Yes, Central exists")
-    return xxx
+def create_app_user(project_id: int, name: str):
+    project.listAppUsers(project_id)
+    user = project.findAppUser(name)
+    if not user:
+        appuser = OdkAppUser()
+        result = appuser.create(user)
+    return result
+
+def delete_app_user(project_id: int, name: str):
+    epdb.st()
--- src/backend/central/central_routes.py
+++ src/backend/central/central_routes.py
@@ -19,13 +19,28 @@
 from fastapi import APIRouter, Depends, HTTPException, File, UploadFile, Form
 from sqlalchemy.orm import Session
 import logging.config
+import json
+from os import getenv
 from fastapi.logger import logger as logger
 from fastapi.responses import FileResponse
 
 from ..db import database
 from ..models.enums import TaskStatus
+from ..users import user_schemas, user_crud
 from ..central import central_schemas, central_crud
+from ..projects import project_schemas, project_crud
+from ..env_utils import is_docker, config_env
 
+# # FIXME: I am not sure this is thread-safe
+# from ..odkconvert.OdkCentral import OdkProject, OdkAppUser, OdkForm
+# project = OdkProject()
+# xform = OdkForm()
+# appuser = OdkAppUser()
+
+# url = config_env["ODK_CENTRAL_URL"]
+# user = config_env["ODK_CENTRAL_USER"]
+# pw = config_env["ODK_CENTRAL_PASSWD"]
+# project.authenticate(url, user, pw)
 
 router = APIRouter(
     prefix="/central",
@@ -34,23 +49,32 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/", response_model=central_schemas.CentralOut)
-async def central():
-    return {"message": "Hello World!"}
-    #raise HTTPException(status_code=404, detail="Tasks not found")
-
 @router.get("/appuser")
 async def create_appuser(name):
     """Create an appuser in Central"""
     logger.info("/central/appuser is Unimplemented!")
     return {"message": "Hello World from /central/appuser"}
 
-@router.get("/project")
-async def create_project(name: str, boundary: str):
-    """Create a project in Central"""
-    logger.info("/central/project is Unimplemented!")
-    # TODO: Return project_id instead of debug
-    return {"message": "Hello World from /central/project"}
+# @router.get("/project")
+# async def create_project(name: str, boundary: str):
+#     """Create a project in Central"""
+#     project.listProjects()
+#     result = project.createProject(name)
+#     logger.info(f"Project {name} has been created on the ODK Central server.")
+#     import epdb; epdb.st()
+#     user = user_schemas.User(author='', city='', country='', username='', id=0)
+#     info = project_schemas.ProjectInfo(name=result['name'],
+#                                              locale=getenv('LANG'),
+#                                              id=result['id'],
+#                                              short_description='',
+#                                              description='',
+#                                              instructions='',
+#                                              per_task_instructions='',
+#                                              )
+#     project = project_crud.create_project_with_project_info(db, project_info)
+
+#     data = json.dumps(result)
+#     return {"message": f"Project {name} created", "data": {data}}
 
 @router.get("/submissions")
 async def download_submissions(project_id: int, xform_id: str):
