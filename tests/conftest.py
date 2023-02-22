be03f0dfd132e755a30ca084d92cd7a52167706b
sam <sam.woodcock@protonmail.com>
2023-02-22 23:25:39 +0000

    test: minimum pytest fastapi config + dummy test

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2020, 2021, 2022 Humanitarian OpenStreetMap Team
+# This file is part of FMTM.
+#
+#     FMTM is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     FMTM is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with FMTM.  If not, see <https:#www.gnu.org/licenses/>.
+#
+"""Configure PyTest for FastAPI."""
+
+import pytest
+
+from fastapi import FastAPI
+
+# from fastapi.testclient import TestClient
+
+from backend.main import get_application
+
+
+@pytest.fixture
+def app() -> FastAPI:
+    return get_application()
