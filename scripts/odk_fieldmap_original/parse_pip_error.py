4d38faf96fa682e01e2fa3e99e5286260338b55c
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2023-03-08 05:44:15 +0300

    clean code by removing trails

--- scripts/odk_fieldmap_original/parse_pip_error.py
+++ scripts/odk_fieldmap_original/parse_pip_error.py
@@ -13,7 +13,7 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with FMTM.  If not, see <https:#www.gnu.org/licenses/>.
-# 
+#
 
 #!/usr/bin/python3
 import os
