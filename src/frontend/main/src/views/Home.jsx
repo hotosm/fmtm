7c67ec4572259c12ace6ab026c886967bda77af3
Prasidha1 <r.prasidha@gmail.com>
2023-03-20 21:57:30 +0530

    Removed testdata from home.tsx

--- src/frontend/main/src/views/Home.jsx
+++ src/frontend/main/src/views/Home.jsx
@@ -1,21 +1,33 @@
 import React, { useEffect, useState } from "react";
-import '../styles/home.css'
+import "../styles/home.css";
 import ExploreProjectCard from "../components/home/ExploreProjectCard";
+<<<<<<< HEAD:src/frontend/main/src/views/Home.jsx
 import windowDimention from "../hooks/WindowDimension";
+=======
+import Box from "@mui/material/Box";
+import { Container, Grid, Pagination, TableRow } from "@mui/material";
+import windowDimention from "../hooks/WindowDimension";
+import BasicPagination from "../utilities/BasicPagination";
+import { useDispatch, useSelector } from "react-redux";
+>>>>>>> 2aa2f19f (Removed testdata from home.tsx):src/frontend/main/src/views/Home.tsx
 import { HomeSummaryService } from "../api/HomeService";
 import enviroment from "../environment";
 import ProjectCardSkeleton from "../components/home/ProjectCardSkeleton";
 import SearchablesRow from "../components/home/HomePageFilters";
+<<<<<<< HEAD:src/frontend/main/src/views/Home.jsx
 import CoreModules from "../shared/CoreModules";
+=======
+>>>>>>> 2aa2f19f (Removed testdata from home.tsx):src/frontend/main/src/views/Home.tsx
 
 const Home = () => {
-   const state:any = useSelector<any>(state=>state.project.projectData)
-   console.log('state main :',state)
-    const[pageNumber, setPageNumber] = useState(1);
+  const state: any = useSelector<any>((state) => state.project.projectData);
+  console.log("state main :", state);
+  const [pageNumber, setPageNumber] = useState(1);
 
-    const { type } = windowDimention();
-    //get window dimension
+  const { type } = windowDimention();
+  //get window dimension
 
+<<<<<<< HEAD:src/frontend/main/src/views/Home.jsx
     const dispatch = CoreModules.useDispatch()
     //dispatch function to perform redux state mutation
 
@@ -30,340 +42,63 @@ const Home = () => {
         dispatch(HomeSummaryService(`${enviroment.baseApiUrl}/projects/summaries?skip=0&limit=100`))
         //creating a manual thunk that will make an API call then autamatically perform state mutation whenever we navigate to home page
     }, [])
+=======
+  const dispatch = useDispatch();
+  //dispatch function to perform redux state mutation
 
+  const stateHome = useSelector((state: any) => state.home);
+  //we use use selector from redux to get all state of home from home slice
 
-   const testData = [
-       {
-         "id": 1,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Stonetown2",
-         "location_str": "Stonetown, Tanzania",
-         "description": "test dataset3",
-         "num_contributors": 5,
-         "total_tasks": 204,
-         "tasks_mapped": 4,
-         "tasks_validated": 1,
-         "tasks_bad": 46
-       },
-       {
-         "id": 2,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Naivasha",
-         "location_str": "Naivasha, Kenya",
-         "description": "test dataset4",
-         "num_contributors": 0,
-         "total_tasks": 23,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 3,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 4,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience Centroids Only",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience Centroids Only",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 5,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "SUZA",
-         "location_str": "Zanzibar, Tanzania",
-         "description": "SUZA project",
-         "num_contributors": 0,
-         "total_tasks": 204,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 6,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Vulamba",
-         "location_str": "Vulamba, Angola",
-         "description": "test dataset2",
-         "num_contributors": 0,
-         "total_tasks": 69,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 7,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Stonetown2",
-         "location_str": "Stonetown, Tanzania",
-         "description": "test dataset3",
-         "num_contributors": 5,
-         "total_tasks": 204,
-         "tasks_mapped": 4,
-         "tasks_validated": 1,
-         "tasks_bad": 46
-       },
-       {
-         "id": 8,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Naivasha",
-         "location_str": "Naivasha, Kenya",
-         "description": "test dataset4",
-         "num_contributors": 0,
-         "total_tasks": 23,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 9,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 10,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience Centroids Only",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience Centroids Only",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 11,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "SUZA",
-         "location_str": "Zanzibar, Tanzania",
-         "description": "SUZA project",
-         "num_contributors": 0,
-         "total_tasks": 204,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 12,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Vulamba",
-         "location_str": "Vulamba, Angola",
-         "description": "test dataset2",
-         "num_contributors": 0,
-         "total_tasks": 69,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 13,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Stonetown2",
-         "location_str": "Stonetown, Tanzania",
-         "description": "test dataset3",
-         "num_contributors": 5,
-         "total_tasks": 204,
-         "tasks_mapped": 4,
-         "tasks_validated": 1,
-         "tasks_bad": 46
-       },
-       {
-         "id": 14,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Naivasha",
-         "location_str": "Naivasha, Kenya",
-         "description": "test dataset4",
-         "num_contributors": 0,
-         "total_tasks": 23,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 15,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 16,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience Centroids Only",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience Centroids Only",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 17,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "SUZA",
-         "location_str": "Zanzibar, Tanzania",
-         "description": "SUZA project",
-         "num_contributors": 0,
-         "total_tasks": 204,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 18,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Vulamba",
-         "location_str": "Vulamba, Angola",
-         "description": "test dataset2",
-         "num_contributors": 0,
-         "total_tasks": 69,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 19,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Stonetown2",
-         "location_str": "Stonetown, Tanzania",
-         "description": "test dataset3",
-         "num_contributors": 5,
-         "total_tasks": 204,
-         "tasks_mapped": 4,
-         "tasks_validated": 1,
-         "tasks_bad": 46
-       },
-       {
-         "id": 20,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Naivasha",
-         "location_str": "Naivasha, Kenya",
-         "description": "test dataset4",
-         "num_contributors": 0,
-         "total_tasks": 23,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 21,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 22,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Dominica Resilience Centroids Only",
-         "location_str": "Roseau, Dominica",
-         "description": "Dominica Resilience Centroids Only",
-         "num_contributors": 0,
-         "total_tasks": 42,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 23,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "SUZA",
-         "location_str": "Zanzibar, Tanzania",
-         "description": "SUZA project",
-         "num_contributors": 0,
-         "total_tasks": 204,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 24,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Vulamba",
-         "location_str": "Vulamba, Angola",
-         "description": "test dataset2",
-         "num_contributors": 0,
-         "total_tasks": 69,
-         "tasks_mapped": 0,
-         "tasks_validated": 0,
-         "tasks_bad": 0
-       },
-       {
-         "id": 25,
-         "priority": 2,
-         "priority_str": "MEDIUM",
-         "title": "Stonetown2",
-         "location_str": "Stonetown, Tanzania",
-         "description": "test dataset3",
-         "num_contributors": 5,
-         "total_tasks": 204,
-         "tasks_mapped": 4,
-         "tasks_validated": 1,
-         "tasks_bad": 46
-       }
-     ];
-      
+  let cardsPerRow: any = new Array(
+    type == "xl"
+      ? 7
+      : type == "lg"
+      ? 5
+      : type == "md"
+      ? 4
+      : type == "sm"
+      ? 3
+      : type == "s"
+      ? 2
+      : 1
+  ).fill(0);
+  //calculating number of cards to to display per row in order to fit our window dimension respectively and then convert it into dummy array
 
-    return (
-        <div style={{ padding: 7 }}>
+  const theme: any = useSelector<any>((state) => state.theme.hotTheme);
+  useEffect(() => {
+    dispatch(
+      HomeSummaryService(
+        `${enviroment.baseApiUrl}/projects/summaries?skip=0&limit=100`
+      )
+    );
+    //creating a manual thunk that will make an API call then autamatically perform state mutation whenever we navigate to home page
+  }, []);
+>>>>>>> 2aa2f19f (Removed testdata from home.tsx):src/frontend/main/src/views/Home.tsx
 
+  return (
+    <div style={{ padding: 7 }}>
+      {/*pagingation*/}
+
+      <SearchablesRow />
+
+      {/*pagingationtop*/}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: "1%",
+        }}
+      >
+        <Pagination
+          color="standard"
+          count={Math.ceil(stateHome.homeProjectSummary.length / 10)}
+          variant="outlined"
+          onChange={(event, pageNumber) => setPageNumber(pageNumber)}
+        />
+      </Box>
+
+<<<<<<< HEAD:src/frontend/main/src/views/Home.jsx
             {/*pagingation*/}
  
             
@@ -411,5 +146,59 @@ const Home = () => {
     )
 
 }
+=======
+      {stateHome.homeProjectLoading == false ? (
+        <Grid
+          mt={2}
+          container
+          spacing={1}
+          columns={{ xs: 1, sm: 3, md: 4, lg: 6, xl: 7 }}
+        >
+          {stateHome.homeProjectSummary
+            .slice((pageNumber - 1) * 10, pageNumber * 10)
+            .map((value, index) => (
+              <Grid item xs={1} sm={1} md={1} lg={1} xl={1} key={index}>
+                <ExploreProjectCard data={value} key={index} />
+              </Grid>
+            ))}
+        </Grid>
+      ) : (
+        <Box
+          sx={{
+            display: {
+              xs: "flex",
+              sm: "flex",
+              md: "flex",
+              lg: "flex",
+              xl: "flex",
+              flexDirection: "row",
+              justifyContent: "left",
+              width: "100%",
+            },
+          }}
+        >
+          <ProjectCardSkeleton defaultTheme={theme} cardsPerRow={cardsPerRow} />
+        </Box>
+      )}
+      {/*pagingation bottom*/}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: "1%",
+        }}
+      >
+        <Pagination
+          color="standard"
+          count={Math.ceil(stateHome.homeProjectSummary.length / 10)}
+          variant="outlined"
+          onChange={(event, pageNumber) => setPageNumber(pageNumber)}
+        />
+      </Box>
+    </div>
+  );
+};
+>>>>>>> 2aa2f19f (Removed testdata from home.tsx):src/frontend/main/src/views/Home.tsx
 
 export default Home;
