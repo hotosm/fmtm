49abb30218be41776c8a8a83720a7b21c3d3111f
Neelima Mohanty <97789856+neelimagoogly@users.noreply.github.com>
2023-03-28 16:52:55 +0530

    Update pagination.py

--- src/backend/pagination/pagination.py
+++ src/backend/pagination/pagination.py
@@ -1,6 +1,7 @@
 from typing import Any, List
 import math
 
+"""Helper function to determine the next and previous pages given the current page and total number of pages."""
 def get_pages_nav(total_pages, current_page):
     next_page = None
     prev_page = None
@@ -11,6 +12,7 @@ def get_pages_nav(total_pages, current_page):
     return next_page, prev_page
 
 
+"""Function to paginate data based on the provided page number, page size and total content."""
 def paginate_data(
         data: List[dict],
         page_no:int,
@@ -21,8 +23,8 @@ def paginate_data(
     next_page, prev_page = get_pages_nav(total_pages, page_no)
 
     return {
-        'count':total_content,
-        'next_page':next_page,
-        'prev_page':prev_page,
-        'results':data
+        'count':total_content, # total number of items in the content
+        'next_page':next_page, # next page number, if exists
+        'prev_page':prev_page, # previous page number, if exists
+        'results':data # paginated results
     }
