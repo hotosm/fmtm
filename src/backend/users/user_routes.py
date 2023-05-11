80d8524a3d4a95d12e7412cd245d903fa3060d57
“nrjadkry” <nrjadkry@gmail.com>
2023-05-11 14:45:56 +0545

    api to create new user roles

--- src/backend/users/user_routes.py
+++ src/backend/users/user_routes.py
@@ -60,3 +60,36 @@ async def get_user_by_id(id: int, db: Session = Depends(database.get_db)):
         return user
     else:
         raise HTTPException(status_code=404, detail="User not found")
+
+
+@router.post("/user-role")
+async def create_user_role(
+                            user_role: user_schemas.UserRoles,
+                            db: Session = Depends(database.get_db)
+                           ):
+    """
+    This api creates a new role for the user. 
+    The role can be Admin, Organization Admin, Field Admin, Mapper, Validator or Read Only.
+
+    Request Parameters:
+        user_id (required): ID of the user for whom the role is being created
+        organization_id (optional): ID of the organization for which the user is being assigned a role
+        project_id (optional): ID of the project for which the user is being assigned a role
+        role (required): Role being assigned to the user
+
+    Response:
+        Status Code 200 (OK): If the role is successfully created
+        Status Code 400 (Bad Request): If the user is already assigned a role
+    """
+
+    existing_user_role = user_crud.get_user_role_by_user_id(db, user_id=user_role.user_id)
+    if existing_user_role is not None:
+        raise HTTPException(status_code=400, detail="User is already assigned a role")
+
+    user = user_crud.get_user(db, user_id=user_role.user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    print('Hellooo')
+
+    return await user_crud.create_user_roles(user_role, db)
