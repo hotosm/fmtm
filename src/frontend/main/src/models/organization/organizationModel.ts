c751960f42cdecac4ca25c4fda1a4bd5cb7cd8d2
shushila21 <shushila.budhathoki28@gmail.com>
2023-05-16 13:18:19 +0545

    feat : organization model created

---
+++ src/frontend/main/src/models/organization/organizationModel.ts
@@ -0,0 +1,33 @@
+
+export interface OrganizationModal {
+    name: string,
+    description: string,
+    url: string,
+    type: number,
+}
+
+  export interface FormCategoryListModel {
+    id: number,
+    title: string,
+  }
+  export interface OrganisationListModel {
+    name: string;
+    slug: string;
+    description: string;
+    type: number;
+    subscription_tier: null | string;
+    id: number;
+    logo: string;
+    url: string;
+  }
+
+  export interface OrganizationDataModel {
+    name : string;
+    slug : string;
+    description : string;
+    type : number;
+    subscription_tier : null;
+    id: number;
+    logo : string;
+    url : string;
+  }
