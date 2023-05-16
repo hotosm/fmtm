ddd242a3b173441e30d2a32936f8453a31b45535
shushila21 <shushila.budhathoki28@gmail.com>
2023-05-16 16:55:31 +0545

    feat : website url validation aaded

--- src/frontend/main/src/components/organization/Validation/OrganizationAddValidation.tsx
+++ src/frontend/main/src/components/organization/Validation/OrganizationAddValidation.tsx
@@ -1,39 +1,49 @@
-
 interface OrganizationValues {
-    logo: string;
-    name: string;
-    description: string;
-    url: string;
-    type: number;
+  logo: string;
+  name: string;
+  description: string;
+  url: string;
+  type: number;
 }
 interface ValidationErrors {
-    logo?: string;
-    name?: string;
-    description?: string;
-    url?: string;
-    type?: string;
+  logo?: string;
+  name?: string;
+  description?: string;
+  url?: string;
+  type?: string;
+}
+
+function isValidUrl(url: string) {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 function OrganizationAddValidation(values: OrganizationValues) {
-    const errors: ValidationErrors = {};
+  const errors: ValidationErrors = {};
 
-    if (!values?.logo) {
-        errors.logo = 'Logo is Required.';
-    }
-    if (!values?.name) {
-        errors.name = 'Name is Required.';
-    }
-    if (!values?.description) {
-        errors.description = 'Description is Required.';
-    }
-    if (!values?.url) {
-        errors.url = 'Organization Url is Required.';
-    }
-    if (!values?.type) {
-        errors.type = 'Type is Required.';
-    }
+  if (!values?.logo) {
+    errors.logo = 'Logo is Required.';
+  }
+  if (!values?.name) {
+    errors.name = 'Name is Required.';
+  }
+  if (!values?.description) {
+    errors.description = 'Description is Required.';
+  }
+  if (!values?.url) {
+    errors.url = 'Organization Url is Required.';
+  } else if (!isValidUrl(values.url)) {
+    errors.url = 'Invalid URL.';
+  }
+  if (!values?.type) {
+    errors.type = 'Type is Required.';
+  }
 
-    return errors;
+  return errors;
 }
 
 export default OrganizationAddValidation;
