28dcd549a8599f6945a6ec987530f04b0d0a9a23
Niraj Adhikari <nrjadkry@gmail.com>
2023-05-29 13:35:29 +0545

    get_project function

--- src/backend/projects/project_crud.py
+++ src/backend/projects/project_crud.py
@@ -124,6 +124,15 @@ def get_project_by_id_w_all_tasks(db: Session, project_id: int):
     return convert_to_app_project(db_project)
 
 
+def get_project(db: Session, project_id: int):
+    db_project = (
+        db.query(db_models.DbProject)
+        .filter(db_models.DbProject.id == project_id)
+        .first()
+    )
+    return db_project
+
+
 def get_project_by_id(db: Session, project_id: int):
 
     db_project = (
