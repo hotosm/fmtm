d1d8aa2a8a05770f6e73e23197ad32478e4bbfae
Niraj Adhikari <nrjadkry@gmail.com>
2023-06-01 09:30:47 +0545

    env.py for alembic is moved inside app directory

--- src/backend/app/migrations/env.py
+++ src/backend/app/migrations/env.py
@@ -5,26 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-from db.database import FmtmMetadata
-target_metadata = FmtmMetadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+from db.db_models import Base
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
@@ -59,15 +46,13 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
