92bf5eaf67d6c646356bc6563e7deaec8873b2c1
Niraj Adhikari <nrjadkry@gmail.com>
2023-06-13 13:10:50 +0545

    tasks feature list api

--- src/backend/app/tasks/tasks_routes.py
+++ src/backend/app/tasks/tasks_routes.py
@@ -25,6 +25,7 @@ from ..db import database
 from ..models.enums import TaskStatus
 from ..users import user_schemas
 from . import tasks_crud, tasks_schemas
+from ..projects import project_crud, project_schemas
 
 router = APIRouter(
     prefix="/tasks",
@@ -108,3 +109,43 @@ async def get_qr_code_list(
     db: Session = Depends(database.get_db),
 ):
     return tasks_crud.get_qr_codes_for_task(db=db, task_id=task_id)
+
+
+@router.get("/tasks-features/")
+async def task_features_count(
+    project_id: int,
+    db: Session = Depends(database.get_db),
+    ):
+
+    task_list = await tasks_crud.get_task_lists(db, project_id)
+
+    # Get the project object.
+    project = project_crud.get_project(db, project_id)
+
+    # ODK Credentials
+    odk_credentials = project_schemas.ODKCentral(
+        odk_central_url = project.odk_central_url,
+        odk_central_user = project.odk_central_user,
+        odk_central_password = project.odk_central_password,
+        )
+
+    # odk_forms = central_crud.list_odk_xforms(project.odkid , odk_credentials)
+
+    import random
+
+    data = []
+    for task in task_list:
+        
+        feature_count_query = f"""
+            select count(*) from features where project_id = {project_id} and task_id = {task['id']}
+        """
+        result = db.execute(feature_count_query)
+        feature_count = result.fetchone()
+
+        data.append({
+            'task_id': task['id'],
+            'feature_count': feature_count['count'],
+            'submission_count': random.randint(0,feature_count['count'])
+        })
+
+    return data
