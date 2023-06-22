58d59529d21bbd8a846c155f7bd2b0255974a67d
Freedisch <freeproduc@gmail.com>
2023-06-22 13:35:28 +0300

    added more tests
    Signed-off-by: Freedisch <freeproduc@gmail.com>

--- src/backend/tests/test_projects.py
+++ src/backend/tests/test_projects.py
@@ -18,13 +18,13 @@
 
 import pytest
 from fastapi import status
+from ..projects.project_schemas import BETAProjectUpload
 from ..projects.project_crud import create_project_with_project_info
-from ..projects import project_schemas
 
 
 @pytest.fixture
 def projects(db):
-    create_project_with_project_info(db, project_schemas.BETAProjectUpload(author={
+    create_project_with_project_info(db, BETAProjectUpload(author={
         "username": "Freenine"
     }, project_info={
         "name": "project1",
@@ -37,7 +37,7 @@ def projects(db):
         "odk_central_password": "password"
     }))
 
-    create_project_with_project_info(db, project_schemas.BETAProjectUpload(author={
+    create_project_with_project_info(db, BETAProjectUpload(author={
         "username": "Freenine2"
     }, project_info={
         "name": "project2",
@@ -50,7 +50,7 @@ def projects(db):
         "odk_central_password": "password2"
     }))
 
-    create_project_with_project_info(db, project_schemas.BETAProjectUpload(author={
+    create_project_with_project_info(db, BETAProjectUpload(author={
         "username": "Freenine3"
     }, project_info={
         "name": "project3",
@@ -98,5 +98,127 @@ def test_categories(client):
     response = client.get("/categories")
     assert response.status_code == status.HTTP_200_OK
 
+
+def test_get_task(client):
+    response = client.post('/projects/near_me', json={
+        "id": -1,
+        "priority": 2,
+        "priority_str": "MEDIUM",
+        "title": "string",
+        "location_str": "string",
+        "description": "string",
+        "num_contributors": 0,
+        "total_tasks": 0,
+        "tasks_validated": 0,
+        "tasks_validated": 0,
+        "tasks_bad": 0
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_create_project(client):
+    response = client.post('/projects/create_project', json={
+        {
+            "author": {
+                "username": "Freenine2"
+            },
+            "project_info": {
+                "name": "project2",
+                "short_description": "short_description2",
+                "description": "description2",
+            },
+            "xform_title": "form1",
+            "odk_central": {
+                "odk_central_url": "centralurl2",
+                "odk_central_user": "centraluser2",
+                "odk_central_password": "password2"
+            }
+        }
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_delete_project(client):
+    response = client.post('/projects/delete/{project_id}', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_upload_project(client):
+    response = client.post('/projects/beta/{project_id}/upload', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_upload_xlsform(client):
+    response = client.post('/projects/upload_xlsform', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_upload_multi_polygon(client):
+    response = client.post('/projects/{poroject_id}/upload_multi_polygon', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_upload_boundary(client):
+    response = client.post('prjects/{porject_id}/upload', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_download_project(client):
+    response = client.post('/projects/{project_id}/download', json={
+        "project_id"
+    })
+    assert response.status == status.HTTP_200_OK
+
+
+def test_download_task(client):
+    response = client.post('/projects/{project_id}/download_tasks', json={
+        "project_id"
+    })
+    assert response.status == status.HTTP_200_OK
+
+
+def test_generate_files(client):
+    response = client.post('/projects/{projects_id}/generate', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_create_organization(client):
+    response = client.post('/projects/organization/', json={
+        {
+            "slug": "string",
+            "logo": "string",
+            "name": "string",
+            "description": "string",
+            "url": "string",
+            "type": 0
+        }
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_preview_task(client):
+    response = client.post('/projects/preview_tasks', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_add_features(client):
+    response = client.post('/projects/add_features/', json={
+        "project_id"
+    })
+    assert response.status_code == status.HTTP_200_OK
 
 # @freedischTODO adding tests for the remaining endpoints
