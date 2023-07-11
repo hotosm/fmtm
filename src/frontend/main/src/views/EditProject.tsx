f3cd90548a5c2a5f6971516a4740950f5b6f82d2
Varun <deepak.pradhan048@gmail.com>
2023-07-11 14:07:55 +0545

    feat: added back icon on EditProject

--- src/frontend/main/src/views/EditProject.tsx
+++ src/frontend/main/src/views/EditProject.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/home.css';
 import CoreModules from '../shared/CoreModules';
+import AssetModules from '../shared/AssetModules';
 import { useDispatch } from 'react-redux';
 import environment from '../environment';
 import { GetIndividualProjectDetails, OrganisationService } from '../api/CreateProjectService';
@@ -11,9 +12,10 @@ import { CreateProjectActions } from '../store/slices/CreateProjectSlice';
 import UpdateForm from '../components/editproject/UpdateForm';
 
 const EditProject: React.FC = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = CoreModules.useDispatch();
   const [selectedTab, setSelectedTab]= useState('project-description');
   const params = CoreModules.useParams();
+  const navigate = useNavigate()
   const encodedProjectId = params.projectId;
   const decodedProjectId = environment.decode(encodedProjectId);
   const defaultTheme: any = CoreModules.useSelector<any>((state) => state.theme.hotTheme);
@@ -32,38 +34,42 @@ const EditProject: React.FC = () => {
     }
   }, [decodedProjectId])
 
-// useEffect(() => {
-//   if(!editProjectResponse)return;
-//   const encodedProjectId= environment.encode(editProjectResponse.id);
-//   navigate(`/project_details/${encodedProjectId}`);
-//   dispatch(CreateProjectActions.SetPatchProjectDetails(null));
-// }, [editProjectResponse])
 
   return (
     <div>
       <CoreModules.Stack>
-        <CoreModules.Typography variant="subtitle2" color={'info'} noWrap sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          position: 'sticky',
-          top: 0,
-          background: 'white',
-          zIndex: 1,
-          paddingBottom: '1.5rem',
-        }} ml={'3%'}>
-          Edit Project
-        </CoreModules.Typography>
         <CoreModules.Stack flexDirection="row">
-          <CoreModules.Stack sx={{m:4, display:'flex', flex: '30%',gap:1}}>
+          <CoreModules.IconButton
+            sx={{display:'flex',justifyContent:'center', alignItems:'center', width:'80px',ml:3}}
+            onClick={() => {
+              navigate(-1);
+              // setOpen(true);
+            }}
+            color="info"
+          >
+            <AssetModules.ArrowBackIcon color='info' sx={{ fontSize: '30px' }} />
+            <CoreModules.Typography ml={2} variant="h1" >
+              Back
+            </CoreModules.Typography>
+          </CoreModules.IconButton>
+          <CoreModules.Typography variant="subtitle2" color={'info'} sx={{
+            width:'100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            }} ml={'3%'} mt={3}>
+            Edit Project
+          </CoreModules.Typography>
+        </CoreModules.Stack>
+        <CoreModules.Stack flexDirection="row">
+          <CoreModules.Stack sx={{m:4, display:'flex', flex: '20%',gap:1}}>
             {SidebarContent.map((content)=>
               <CoreModules.Typography onClick={()=>setSelectedTab(content.slug)} sx={{p:1, ...tabHover, backgroundColor:selectedTab === content.slug ? defaultTheme.palette.grey.light : 'white'}}    variant="subtitle2">
                 {content.name}
               </CoreModules.Typography>
             )}
           </CoreModules.Stack>
-          <CoreModules.Stack sx={{display:'flex', flex: '70%',p:3}}>
+          <CoreModules.Stack sx={{display:'flex', flex: '80%',p:3}}>
             {selectedTab === 'project-description' ?<EditProjectDetails projectId={decodedProjectId} />:null}
             {selectedTab === 'form-update' ?<UpdateForm projectId={decodedProjectId}/>:null}
           </CoreModules.Stack>
