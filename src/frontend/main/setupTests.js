a215677948defc9904e816a024813de6ee350736
Varun <deepak.pradhan048@gmail.com>
2023-07-27 13:27:54 +0545

    Feat: test Integration Setup For App.tsx

---
+++ src/frontend/main/setupTests.js
@@ -0,0 +1,4 @@
+global.console = {
+  ...console,
+  error: jest.fn(),
+};
--- src/frontend/main/tests/CreateProject.test.tsx
+++ src/frontend/main/tests/CreateProject.test.tsx
@@ -1,62 +1,22 @@
 import React from 'react';
-import { act, render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import PrimaryAppBar from '../src/utilities/PrimaryAppBar';
 import MainView from '../src/views/MainView';
 import { Provider } from 'react-redux';
 import { store } from '../src/store/Store.js';
-import { BrowserRouter } from 'react-router-dom';
-export const renderWithRouter = (ui, { route = '/' } = {}) => {
-  act(() => window.history.pushState({}, 'Test page', route));
+import { renderWithRouter } from '../src/utilfunctions/testUtils';
 
-  return {
-    ...render(ui, { wrapper: BrowserRouter }),
-  };
-};
-global.console = {
-  ...console,
-  // uncomment to ignore a specific log level
-  // log: jest.fn(),
-  // debug: jest.fn(),
-  // info: jest.fn(),
-  // warn: jest.fn(),
-  error: jest.fn(),
-};
 jest.mock('axios'); // Mock axios module
 
-describe('MyComponent', () => {
-  test('renders the app bar with correct elements', () => {
-    render(
+describe('Frontend Application Running', () => {
+  it('renders App.jsx without errors', () => {
+    // Render the App component in a virtual DOM environment
+    const { container } = renderWithRouter(
       <Provider store={store}>
-        <BrowserRouter>
-          <MainView />
-        </BrowserRouter>
+        <MainView />
       </Provider>,
     );
 
-    // Check if the "EXPLORE PROJECTS" tab is rendered
-    const exploreTabElement = screen.getByText('EXPLORE PROJECTS');
-    expect(exploreTabElement).toBeInTheDocument();
-
-    // Check if the "MANAGE ORGANIZATIONS" tab is rendered
-    const manageOrgTabElement = screen.getByText('MANAGE ORGANIZATIONS');
-    expect(manageOrgTabElement).toBeInTheDocument();
-  });
-  test('renders the app bar with correct elements', () => {
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <PrimaryAppBar />
-        </BrowserRouter>
-      </Provider>,
-    );
-
-    // Check if the "EXPLORE PROJECTS" tab is rendered
-    const exploreTabElement = screen.getByText('EXPLORE PROJECTS');
-    expect(exploreTabElement).toBeInTheDocument();
-
-    // Check if the "MANAGE ORGANIZATIONS" tab is rendered
-    const manageOrgTabElement = screen.getByText('MANAGE ORGANIZATIONS');
-    expect(manageOrgTabElement).toBeInTheDocument();
+    // Assert that the component renders without any errors
+    expect(container).toBeDefined();
   });
 });
