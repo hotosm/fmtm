2b97625c49b352c5c3ddc0ff49f0b5c3e7a68fda
Varun <deepak.pradhan048@gmail.com>
2023-08-14 15:59:13 +0545

    fix: hashtag validation create project

--- src/frontend/main/src/components/createproject/validation/DataExtractValidation.tsx
+++ src/frontend/main/src/components/createproject/validation/DataExtractValidation.tsx
@@ -1,17 +1,16 @@
-
 interface ProjectValues {
   xform_title: string;
   form_ways: string;
   data_extractWays: string;
   data_extractFile: object;
-  data_extract_options:string;
+  data_extract_options: string;
 }
 interface ValidationErrors {
   xform_title?: string;
   form_ways?: string;
   data_extractWays?: string;
   data_extractFile?: string;
-  data_extract_options?:string;
+  data_extract_options?: string;
 }
 
 function DataExtractValidation(values: ProjectValues) {
@@ -20,16 +19,19 @@ function DataExtractValidation(values: ProjectValues) {
   if (!values?.xform_title) {
     errors.xform_title = 'Form Category is Required.';
   }
-  if(!values.data_extract_options){
-    errors.data_extract_options= 'Select Data Extract Options.';
+  if (!values.data_extract_options) {
+    errors.data_extract_options = 'Select Data Extract Options.';
   }
-  if(values.data_extract_options && values.data_extract_options === 'Upload Custom Data Extract' && !values.data_extractFile){
+  if (
+    values.data_extract_options &&
+    values.data_extract_options === 'Upload Custom Data Extract' &&
+    !values.data_extractFile
+  ) {
     errors.data_extractFile = 'Data Extract File is Required.';
   }
-  if(values.data_extract_options && values.data_extract_options === 'Data Extract Ways' && !values.data_extractWays){
+  if (values.data_extract_options && values.data_extract_options === 'Data Extract Ways' && !values.data_extractWays) {
     errors.data_extractWays = 'Data Extract Ways is Required.';
   }
-
 
   console.log(errors);
   return errors;
