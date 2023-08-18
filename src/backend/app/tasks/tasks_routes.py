1c39865b870735e6fda8aed172462d127f9cf34c
Neelima Mohanty <97789856+neelimagoogly@users.noreply.github.com>
2023-08-18 12:27:06 +0530

    Update tasks_routes.py

--- src/backend/app/tasks/tasks_routes.py
+++ src/backend/app/tasks/tasks_routes.py
@@ -45,6 +45,21 @@ async def read_task_list(
     limit: int = 1000,
     db: Session = Depends(database.get_db),
     ):
+    """
+    Get a list of tasks for a project.
+
+    Args:
+        project_id (int): Project ID.
+        limit (int, optional): Maximum number of tasks to return. Defaults to 1000.
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Raises:
+        HTTPException: If no tasks are found.
+
+    Returns:
+        List[TaskOut]: List of TaskOut objects.
+    """
+
     tasks = tasks_crud.get_tasks(db, project_id, limit)
     if tasks:
         return tasks
@@ -60,6 +75,22 @@ async def read_tasks(
     limit: int = 1000,
     db: Session = Depends(database.get_db),
 ):
+    """
+    Get a list of tasks for a project or user.
+
+    Args:
+        project_id (int): Project ID.
+        user_id (int, optional): User ID. Defaults to None.
+        skip (int, optional): Number of tasks to skip. Defaults to 0.
+        limit (int, optional): Maximum number of tasks to return. Defaults to 1000.
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Raises:
+        HTTPException: If no tasks are found or if both `user_id` and `task_id` are provided.
+
+    Returns:
+        List[TaskOut]: List of TaskOut objects.
+    """
     if user_id:
         raise HTTPException(
             status_code=300,
@@ -75,12 +106,36 @@ async def read_tasks(
 
 @router.post("/near_me", response_model=tasks_schemas.TaskOut)
 def get_task(lat: float, long: float, project_id: int = None, user_id: int = None):
-    """Get tasks near the requesting user."""
+    """
+    Get tasks near the requesting user.
+
+    Args:
+        lat (float): Latitude of the user's location.
+        long (float): Longitude of the user's location.
+        project_id (int, optional): Project ID. Defaults to None.
+        user_id (int, optional): User ID. Defaults to None.
+
+    Returns:
+        TaskOut: TaskOut object for the nearest task.
+    """
     return "Coming..."
 
 
 @router.get("/{task_id}", response_model=tasks_schemas.TaskOut)
 async def read_tasks(task_id: int, db: Session = Depends(database.get_db)):
+    """
+    Get a task by its ID.
+
+    Args:
+        task_id (int): Task ID.
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Raises:
+        HTTPException: If the task is not found.
+
+    Returns:
+        TaskOut: TaskOut object for the specified task.
+    """
     task = tasks_crud.get_task(db, task_id)
     if task:
         return task
@@ -95,6 +150,21 @@ async def update_task_status(
     new_status: tasks_schemas.TaskStatusOption,
     db: Session = Depends(database.get_db),
 ):
+    """
+    Update the status of a task.
+
+    Args:
+        user (user_schemas.User): User object for the logged in user.
+        task_id (int): Task ID.
+        new_status (tasks_schemas.TaskStatusOption): New status for the task.
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Raises:
+        HTTPException: If the task status could not be updated.
+
+    Returns:
+        TaskOut: Updated TaskOut object for the specified task.
+    """
     # TODO verify logged in user
     user_id = user.id
 
@@ -112,6 +182,16 @@ async def get_qr_code_list(
     task_id: int,
     db: Session = Depends(database.get_db),
 ):
+    """
+    Get the QR code for a task.
+
+    Args:
+        task_id (int): Task ID.
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Returns:
+        dict: Dictionary containing the QR code for the specified task.
+    """
     return tasks_crud.get_qr_codes_for_task(db=db, task_id=task_id)
 
 
@@ -121,6 +201,17 @@ async def edit_task_boundary(
     boundary: UploadFile = File(...),
     db: Session = Depends(database.get_db),
     ):
+    """
+    Edit the boundary of a task.
+
+    Args:
+        task_id (int): Task ID.
+        boundary (UploadFile, optional): New boundary for the task. Defaults to File(...).
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Returns:
+        bool or dict or list or str or NoneType or Response or JSONResponse or HTMLResponse or RedirectResponse or StreamingResponse or FileResponse or UJSONResponse or ORJSONResponse or MsgpackResponse: Result of the boundary edit.
+    """
 
     # read entire file
     content = await boundary.read()
@@ -136,6 +227,16 @@ async def task_features_count(
     project_id: int,
     db: Session = Depends(database.get_db),
     ):
+    """
+    Get the feature count for tasks in a project.
+
+    Args:
+        project_id (int): Project ID.
+        db (Session, optional): Database session. Defaults to Depends(database.get_db).
+
+    Returns:
+        dict or list or str or NoneType or Response or JSONResponse or HTMLResponse or RedirectResponse or StreamingResponse or FileResponse or UJSONResponse or ORJSONResponse or MsgpackResponse: Feature count for tasks in the project.
+    """
 
     task_list = tasks_crud.get_task_lists(db, project_id)
 
@@ -150,6 +251,15 @@ async def task_features_count(
         )
 
     def process_task(task):
+        """
+        Process a task to get its feature and submission counts.
+
+        Args:
+            task (int): Task ID.
+
+        Returns:
+            dict: Dictionary containing the task ID, feature count, and submission count.
+        """
         feature_count_query = f"""
             select count(*) from features where project_id = {project_id} and task_id = {task}
         """
