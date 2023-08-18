bc08977ec7fde64020d01409704a298d7e3d02b5
sam <sam.woodcock@protonmail.com>
2023-08-18 12:20:52 +0100

    fix: only set OAUTHLIB_INSECURE_TRANSPORT in DEBUG mode

--- src/backend/app/auth/auth_routes.py
+++ src/backend/app/auth/auth_routes.py
@@ -16,12 +16,13 @@
 #     along with FMTM.  If not, see <https:#www.gnu.org/licenses/>.
 #
 
-from fastapi import APIRouter, Depends, Request, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.logger import logger as log
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
-from ..db.db_models import DbUser
+
 from ..db import database
+from ..db.db_models import DbUser
 from ..users import user_crud, user_schemas
 from .osm import AuthUser, init_osm_auth, login_required
 
@@ -72,15 +73,14 @@ def callback(request: Request, osm_auth=Depends(init_osm_auth)):
     """
     access_token = osm_auth.callback(str(request.url))
     log.debug(f"Access token returned: {access_token}")
-    print(access_token, 'access_token')
     return JSONResponse(content={"access_token": access_token}, status_code=200)
 
 
 @router.get("/me/", response_model=AuthUser)
 def my_data(
-        db: Session = Depends(database.get_db),
-        user_data: AuthUser = Depends(login_required)
-    ):
+    db: Session = Depends(database.get_db),
+    user_data: AuthUser = Depends(login_required),
+):
     """Read the access token and provide  user details from OSM user's API endpoint,
     also integrated with underpass .
 
@@ -88,21 +88,19 @@ def my_data(
 
     Returns: user_data
     """
-
-
     # Save user info in User table
-    user = user_crud.get_user_by_id(db, user_data['id'])
+    user = user_crud.get_user_by_id(db, user_data["id"])
     if not user:
-        user_by_username = user_crud.get_user_by_username(db, user_data['username'])
+        user_by_username = user_crud.get_user_by_username(db, user_data["username"])
         if user_by_username:
             raise HTTPException(
-                status_code=400, detail=f"User with this username {user_data['username']} already exists. \
-                                            Please contact the administrator for this."
+                status_code=400,
+                detail=f"User with this username {user_data['username']} already exists. \
+                                            Please contact the administrator for this.",
             )
 
-        db_user = DbUser(id=user_data['id'], username=user_data['username'])
+        db_user = DbUser(id=user_data["id"], username=user_data["username"])
         db.add(db_user)
         db.commit()
-
 
     return JSONResponse(content={"user_data": user_data}, status_code=200)
