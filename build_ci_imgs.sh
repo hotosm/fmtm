63011c0c8393081ee54554e370610f5e135be7b4
sam <sam.woodcock@protonmail.com>
2023-08-25 06:58:16 +0100

    build: separate push to ci img build

--- build_ci_imgs.sh
+++ build_ci_imgs.sh
@@ -2,8 +2,10 @@
 
 set -eo pipefail
 
-docker build --push \
+docker build \
     -t ghcr.io/hotosm/fmtm/backend:ci \
     --target ci \
     --build-arg APP_VERSION=ci \
     ./src/backend
+
+docker push ghcr.io/hotosm/fmtm/backend:ci
