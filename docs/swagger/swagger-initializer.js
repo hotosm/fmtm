66305a7a42e1879c2bb8f725435932d404fe664b
spwoodcock <sam.woodcock@protonmail.com>
2023-09-01 11:46:47 +0100

    docs: add swagger ui to docs page

---
+++ docs/swagger/swagger-initializer.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://hotosm.gitlab-pages.wsl.ch/fmtm/openapi.json",
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
+  });
+
+  //</editor-fold>
+};
