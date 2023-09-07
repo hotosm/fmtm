a45e92c5a4a0248b20086eab2e114a1dcf0399f3
spwoodcock <sam.woodcock@protonmail.com>
2023-09-07 12:28:49 +0100

    ci: fix openapi.json link for swagger

--- docs/swagger/swagger-initializer.js
+++ docs/swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://hotosm.gitlab-pages.wsl.ch/fmtm/openapi.json",
+    url: "https://hotosm.github.io/fmtm/openapi.json",
     dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
