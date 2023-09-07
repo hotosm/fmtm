22f710a7e656e40ee45bca468abe89bdbc3b8f3e
Niraj Adhikari <nrjadkry@gmail.com>
2023-09-07 15:54:42 +0545

    fix: raw sql query in tasks feature

--- src/backend/app/tasks/tasks_routes.py
+++ src/backend/app/tasks/tasks_routes.py
@@ -180,9 +180,9 @@ async def task_features_count(
     # Assemble the final data list
     data = []
     for x in odk_details:
-        feature_count_query = f"""
+        feature_count_query = text(f"""
             select count(*) from features where project_id = {project_id} and task_id = {x['xmlFormId']}
-        """
+        """)
         result = db.execute(feature_count_query)
         feature_count = result.fetchone()
 
@@ -190,7 +190,7 @@ async def task_features_count(
             'task_id': x['xmlFormId'],
             'submission_count': x['submissions'],
             'last_submission': x['lastSubmission'],
-            'feature_count': feature_count['count']
+            'feature_count': feature_count[0]
         })
 
     return data
