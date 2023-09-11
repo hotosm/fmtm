ae626228ab0c75858e6b23b26e3fbbb94713857f
NSUWAL123 <suwalnishit@gmail.com>
2023-09-11 16:28:34 +0545

    fix (input field/ explore projects): design fix, id added to text fields

--- src/frontend/main/src/components/common/InputTextField.tsx
+++ src/frontend/main/src/components/common/InputTextField.tsx
@@ -42,7 +42,7 @@ function InputTextField({
     <div className={`fmtm-custom-textField ${classNames}`}>
       <p className="fmtm-text-[1rem] fmtm-mb-2 fmtm-font-semibold">{label}</p>
       <div
-        className={`fmtm-border-[1px] fmtm-border-gray-300 fmtm-h-[1.8rem] fmtm-bg-inactive_bg fmtm-w-full fmtm-justify-between fmtm-flex fmtm-items-center fmtm-overflow-hidden`}
+        className={`fmtm-border-[1px] fmtm-border-gray-300 fmtm-h-[1.8rem] fmtm-w-full fmtm-justify-between fmtm-flex fmtm-items-center fmtm-overflow-hidden`}
       >
         <input
           id={id}
--- src/frontend/main/src/components/createnewproject/ProjectDetailsForm.tsx
+++ src/frontend/main/src/components/createnewproject/ProjectDetailsForm.tsx
@@ -50,52 +50,58 @@ const ProjectDetailsForm = () => {
   };
 
   return (
-    <div className="fmtm-flex fmtm-gap-7">
+    <div className="fmtm-flex fmtm-gap-7 fmtm-flex-col lg:fmtm-flex-row">
       <div className="fmtm-bg-white xl:fmtm-w-[17%] fmtm-px-5 fmtm-py-6">
-        <h6 className="fmtm-text-xl fmtm-font-[600] fmtm-pb-6">Project Details</h6>
-        <p className="fmtm-text-gray-500 fmtm-flex fmtm-flex-col fmtm-gap-3">
+        <h6 className="fmtm-text-xl fmtm-font-[600] fmtm-pb-2 lg:fmtm-pb-6">Project Details</h6>
+        <p className="fmtm-text-gray-500 lg:fmtm-flex lg:fmtm-flex-col lg:fmtm-gap-3">
           <span>Fill in your project basic information such as name, description, hashtag, etc. </span>
           <span>To complete the first step, you will need the account credentials of ODK central server.</span>{' '}
           <span>Here are the instructions for setting up a Central ODK Server on Digital Ocean.</span>
         </p>
       </div>
-      <div className="xl:fmtm-w-[83%] fmtm-h-[60vh] fmtm-bg-white fmtm-px-11 fmtm-py-6 fmtm-overflow-y-scroll scrollbar">
-        <div className="fmtm-w-full fmtm-flex fmtm-gap-14">
-          <div className="fmtm-flex fmtm-flex-col fmtm-gap-6 fmtm-w-[50%]">
+      <div className="xl:fmtm-w-[83%] lg:fmtm-h-[60vh] xl:fmtm-h-[58vh] fmtm-bg-white fmtm-px-11 fmtm-py-6 lg:fmtm-overflow-y-scroll lg:scrollbar">
+        <div className="fmtm-w-full fmtm-flex fmtm-gap-6 md:fmtm-gap-14 fmtm-flex-col md:fmtm-flex-row">
+          <div className="fmtm-flex fmtm-flex-col fmtm-gap-6 md:fmtm-w-[50%]">
             <InputTextField
+              id="name"
               label="Project Name"
-              value={values.name}
+              value={values?.name}
               onChange={(e) => handleCustomChange('name', e.target.value)}
               fieldType="text"
             />
             <TextArea
+              id="short_description"
               label="Short Description"
               rows={3}
-              value={values.short_description}
+              value={values?.short_description}
               onChange={(e) => handleCustomChange('short_description', e.target.value)}
             />
             <InputTextField
+              id="odk_central_url"
               label="ODK Central URL"
-              value={values.odk_central_url}
+              value={values?.odk_central_url}
               onChange={(e) => handleCustomChange('odk_central_url', e.target.value)}
               fieldType="text"
             />
             <InputTextField
+              id="odk_central_password"
               label="Central ODK Password"
-              value={values.odk_central_password}
+              value={values?.odk_central_password}
               onChange={(e) => handleCustomChange('odk_central_password', e.target.value)}
-              fieldType="text"
+              fieldType="password"
             />
             <InputTextField
+              id="odk_central_user"
               label="Central ODK Email/Username"
-              value={values.odk_central_user}
+              value={values?.odk_central_user}
               onChange={(e) => handleCustomChange('odk_central_user', e.target.value)}
               fieldType="text"
             />
             <div>
               <InputTextField
+                id="hashtags"
                 label="Changeset Comment"
-                value={values.hashtags}
+                value={values?.hashtags}
                 onChange={(e) => {
                   handleHashtagOnChange(e);
                 }}
@@ -111,17 +117,19 @@ const ProjectDetailsForm = () => {
               </p>
             </div>
           </div>
-          <div className="fmtm-w-[50%] fmtm-flex fmtm-flex-col fmtm-gap-6">
+          <div className="md:fmtm-w-[50%] fmtm-flex fmtm-flex-col fmtm-gap-6">
             <InputTextField
+              id="organisation_name"
               label="Organization Name"
-              value={values.organisation_name}
+              value={values?.organisation_name}
               onChange={(e) => handleCustomChange('organisation_name', e.target.value)}
               fieldType="text"
             />
             <TextArea
+              id="description"
               label="Description"
               rows={3}
-              value={values.description}
+              value={values?.description}
               onChange={(e) => handleCustomChange('description', e.target.value)}
             />
           </div>
