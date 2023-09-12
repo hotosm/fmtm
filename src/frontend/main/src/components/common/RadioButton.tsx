873b3806a130b604de348365a6de65956a785deb
NSUWAL123 <suwalnishit@gmail.com>
2023-09-12 16:39:47 +0545

    fix (radio button): radio button column direction updated

--- src/frontend/main/src/components/common/RadioButton.tsx
+++ src/frontend/main/src/components/common/RadioButton.tsx
@@ -18,7 +18,7 @@ const RadioButton: React.FC<RadioButtonProps> = ({ topic, options, direction, on
     <div>
       <p className="fmtm-text-xl fmtm-font-[600] fmtm-mb-3">{topic}</p>
     </div>
-    <div className={`fmtm-flex ${direction === 'column' ? 'fmtm-flex-row' : 'fmtm-flex-wrap fmtm-gap-x-16'}`}>
+    <div className={`fmtm-flex ${direction === 'column' ? 'fmtm-flex-col' : 'fmtm-flex-wrap fmtm-gap-x-16'}`}>
       {options.map((option) => {
         return (
           <div key={option.value} className="fmtm-gap-5 fmtm-flex fmtm-items-center">
