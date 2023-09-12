9b0f96495d71eb4e24aa0268096f791df79dbb54
NSUWAL123 <suwalnishit@gmail.com>
2023-09-12 15:04:49 +0545

    feat (radiobutton): custom radio button added

---
+++ src/frontend/main/src/components/common/RadioButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface IRadioButton {
+  name: string;
+  value: string;
+  label: string | number;
+}
+
+interface RadioButtonProps {
+  topic?: string;
+  options: IRadioButton[];
+  direction: 'row' | 'column';
+  onChangeData: (value: string) => void;
+}
+
+const RadioButton: React.FC<RadioButtonProps> = ({ topic, options, direction, onChangeData }) => (
+  <div>
+    <div>
+      <p className="fmtm-text-xl fmtm-font-[600] fmtm-mb-3">{topic}</p>
+    </div>
+    <div className={`fmtm-flex ${direction === 'column' ? 'fmtm-flex-row' : 'fmtm-flex-wrap fmtm-gap-x-16'}`}>
+      {options.map((option) => {
+        return (
+          <div key={option.value} className="fmtm-gap-5 fmtm-flex fmtm-items-center">
+            <input
+              type="radio"
+              id={option.value}
+              name={option.name}
+              value={option.value}
+              className="fmtm-accent-primaryRed"
+              onChange={(e) => onChangeData(e.target.value)}
+            />
+            <p className="fmtm-text-lg fmtm-bg-white fmtm-text-gray-500">{option.label}</p>
+          </div>
+        );
+      })}
+    </div>
+  </div>
+);
+
+export default RadioButton;
