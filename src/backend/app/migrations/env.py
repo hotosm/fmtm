3c5303916984e2d30e71b938a9b8d97aa7378fb0
Niraj Adhikari <nrjadkry@gmail.com>
2023-10-02 10:36:43 +0545

    created alembic helpers file to check if the tables already exists

--- src/backend/app/migrations/env.py
+++ src/backend/app/migrations/env.py
@@ -4,7 +4,6 @@ from alembic import context
 from config import settings
 from geoalchemy2 import alembic_helpers
 from sqlalchemy import engine_from_config, pool
-from sqlalchemy.engine import reflection
 from db.db_models import Base
 
 config = context.config
@@ -18,27 +17,6 @@ target_metadata = Base.metadata
 
 
 exclude_tables = config.get_section("alembic:exclude").get("tables", "").split(",")
-
-
-def table_does_not_exist(table, schema=None):
-    config = op.get_context().config
-    engine = engine_from_config(
-        config.get_section(config.config_ini_section), prefix='sqlalchemy.')
-    insp = reflection.Inspector.from_engine(engine)
-    return insp.has_table(table, schema) == False
- 
- 
-def table_has_column(table, column):
-    config = op.get_context().config
-    engine = engine_from_config(
-        config.get_section(config.config_ini_section), prefix='sqlalchemy.')
-    insp = reflection.Inspector.from_engine(engine)
-    has_column = False
-    for col in insp.get_columns(table):
-        if column not in col['name']:
-            continue
-        has_column = True
-    return has_column
 
 def include_object(object, name, type_, reflected, compare_to):
     """Custom helper function that enables us to ignore our excluded tables in the autogen sweep."""
