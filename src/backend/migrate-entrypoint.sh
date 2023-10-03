c8f2b1394fc497568acbc35f8593ff7b360f9a37
sam <sam.woodcock@protonmail.com>
2023-10-03 17:49:38 +0100

    build: additional logging to migration script

--- src/backend/migrate-entrypoint.sh
+++ src/backend/migrate-entrypoint.sh
@@ -25,10 +25,12 @@ if [ -z "${FMTM_DB_NAME}" ]; then
     exit 1
 fi
 
+echo "Crerating default svcfmtm user"
 # Create default svc user
 psql "postgresql://${FMTM_DB_USER}:${FMTM_DB_PASSWORD}@${FMTM_DB_HOST}/${FMTM_DB_NAME}" \
     -c "INSERT INTO users (id, username, role, mapping_level, tasks_mapped, tasks_validated, tasks_invalidated) \
         VALUES (20386219, 'svcfmtm', 'MAPPER', 'BEGINNER', 0, 0, 0) \
         ON CONFLICT (id) DO NOTHING;"
+echo "User creation complete"
 
 exec "$@"
