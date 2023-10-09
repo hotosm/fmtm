cc970967f0d05acc41cab563ac79702a1b05572c
spwoodcock <sam.woodcock@protonmail.com>
2023-10-09 22:38:08 +0100

    build(frontend): add vite-pwa to config, remove webpack.config.js

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   server: {
     port: 8080,
     host: '0.0.0.0',
