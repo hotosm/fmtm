a91442c51ead1bedc7c16d19b8aa3a39a1b82ea1
sujanadh <sujanadh07@gmail.com>
2023-10-12 15:07:34 +0545

    fix: issue when single feature polygon uploaded to upload multi polygon api

--- src/backend/app/projects/project_crud.py
+++ src/backend/app/projects/project_crud.py
@@ -402,7 +402,10 @@ def update_multi_polygon_project_boundary(
             return False
 
         # Update the boundary polyon on the database.
-        polygons = boundary["features"]
+        if boundary["type"]=="Feature":
+            polygons = [boundary]
+        else:
+            polygons = boundary["features"]
         log.debug(f"Processing {len(polygons)} task geometries")
         for polygon in polygons:
             # If the polygon is a MultiPolygon, convert it to a Polygon
