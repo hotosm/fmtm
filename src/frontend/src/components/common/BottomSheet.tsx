d7b59b84946fb0977c26471eed93d45d69487ab5
NSUWAL123 <suwalnishit@gmail.com>
2023-10-13 11:48:17 +0545

    fix (bottomSheet): fmtm logo move bottom sheet expand/contract

--- src/frontend/src/components/common/BottomSheet.tsx
+++ src/frontend/src/components/common/BottomSheet.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
+import FmtmLogo from '../../assets/images/hotLog.png';
 
 const BottomSheet = ({ body, onClose }) => {
   const sheetContentRef: any = useRef(null);
   const bottomSheetRef: any = useRef(null);
+  const logoRef: any = useRef(null);
   const [startY, setStartY] = useState(0);
   const [startHeight, setStartHeight] = useState(0);
   const [currSheetHeight, setCurrSheetHeight] = useState(0);
@@ -20,6 +22,9 @@ const BottomSheet = ({ body, onClose }) => {
   const updateSheetHeight = (height) => {
     if (sheetContentRef.current) {
       sheetContentRef.current.style.height = `${height}vh`;
+      const top = sheetContentRef.current.getBoundingClientRect().top;
+      const logoHeight = logoRef.current.getBoundingClientRect().height;
+      logoRef.current.style.top = `${top - logoHeight}px`;
     }
     if (height === 100) {
       setIsFullScreen(true);
@@ -66,16 +71,19 @@ const BottomSheet = ({ body, onClose }) => {
   }, [currSheetHeight]);
 
   return (
-    <div className="fmtm-absolute fmtm-bottom-[400px] fmtm-bg-white sm:fmtm-hidden">
+    <div className="fmtm-absolute fmtm-bottom-[200px] fmtm-bg-white sm:fmtm-hidden">
       <div
         className={`bottom-sheet fmtm-fixed fmtm-w-full fmtm-left-0 fmtm-bottom-0 fmtm-flex fmtm-items-center fmtm-flex-col fmtm-justify-end fmtm-duration-100 fmtm-ease-linear ${
           !show ? 'fmtm-opacity-0 fmtm-pointer-events-none' : 'fmtm-opacity-100 fmtm-pointer-events-auto'
         }`}
         ref={bottomSheetRef}
       >
+        <div className={`fmtm-fixed fmtm-left-0`} ref={logoRef}>
+          <img src={FmtmLogo} alt="Hot Fmtm Logo" className="fmtm-ml-2 fmtm-z-10 fmtm-w-[5.2rem]  fmtm-mb-4" />
+        </div>
         <div
           ref={sheetContentRef}
-          className={`bottom-sheet-content fmtm-shadow-[30px_-10px_40px_25px_rgba(0,0,0,0.2)] fmtm-w-full fmtm-relative fmtm-bg-white fmtm-max-h-[100vh] fmtm-h-[50vh] fmtm-max-w-[1150px] fmtm-py-6 fmtm-px-4 fmtm-duration-300 fmtm-ease-in-out fmtm-overflow-hidden ${
+          className={`bottom-sheet-content fmtm-shadow-[30px_-10px_10px_5px_rgba(0,0,0,0.1)] fmtm-w-full fmtm-relative fmtm-bg-white fmtm-max-h-[100vh] fmtm-h-[50vh] fmtm-max-w-[1150px] fmtm-py-6 fmtm-px-4 fmtm-duration-300 fmtm-ease-in-out fmtm-overflow-hidden ${
             !show ? 'fmtm-translate-y-[100%]' : 'fmtm-translate-y-[0%]'
           } ${isDragging ? 'fmtm-transition-none' : ''} ${isFullScreen ? 'fmtm-rounded-none' : 'fmtm-rounded-t-2xl'}`}
         >
@@ -89,7 +97,7 @@ const BottomSheet = ({ body, onClose }) => {
               onMouseUp={dragStop}
               onTouchEnd={dragStop}
             >
-              <span className="fmtm-h-1 fmtm-w-[2.5rem] fmtm-block fmtm-bg-[#c7d0e1] fmtm-rounded-full"></span>
+              <span className="fmtm-h-1 fmtm-w-[2.5rem] fmtm-block fmtm-bg-[#c7d0e1] fmtm-rounded-full hover:fmtm-bg-primaryRed"></span>
             </div>
           </div>
           <div className="body fmtm-overflow-y-scroll scrollbar fmtm-h-full fmtm-p-[1px]">{body}</div>
