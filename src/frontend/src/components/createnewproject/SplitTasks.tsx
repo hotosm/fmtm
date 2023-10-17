27875f00b474efcebd264acb21ae2ce3efefaa42
Varun <deepak.pradhan048@gmail.com>
2023-10-17 19:41:01 +0545

    feat: Split task added progress bar

--- src/frontend/src/components/createnewproject/SplitTasks.tsx
+++ src/frontend/src/components/createnewproject/SplitTasks.tsx
@@ -19,6 +19,8 @@ import {
 } from '../../api/CreateProjectService';
 import environment from '../../environment';
 import LoadingBar from '../../components/createproject/LoadingBar';
+import { Modal } from '../../components/common/Modal';
+import ProgressBar from '../../components/common/ProgressBar';
 
 const alogrithmList = [
   { name: 'define_tasks', value: 'divide_on_square', label: 'Divide on square' },
@@ -30,6 +32,8 @@ let generateProjectLogIntervalCb: any = null;
 const SplitTasks = ({ flag, geojsonFile, setGeojsonFile, customLineUpload, customPolygonUpload, customFormFile }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [toggleStatus, setToggleStatus] = useState(false);
 
   const divRef = useRef(null);
   const splitTasksSelection = CoreModules.useAppSelector((state) => state.createproject.splitTasksSelection);
@@ -58,8 +62,6 @@ const SplitTasks = ({ flag, geojsonFile, setGeojsonFile, customLineUpload, custo
   };
 
   const submission = () => {
-    console.log('submission triggered');
-
     const blob = new Blob([JSON.stringify(dividedTaskGeojson || drawnGeojson)], { type: 'application/json' });
 
     // Create a file object from the Blob
@@ -105,12 +107,7 @@ const SplitTasks = ({ flag, geojsonFile, setGeojsonFile, customLineUpload, custo
         customLineUpload,
       ),
     );
-    // if (formValues.splitting_algorithm === 'Divide on Square') {
-    //   generateTasksOnMap();
-    // }
     dispatch(CreateProjectActions.SetIndividualProjectDetailsData({ ...projectDetails, ...formValues }));
-    // navigate('/select-form');
-    // toggleStep(5, '/new-select-form');
   };
 
   const {
@@ -162,6 +159,7 @@ const SplitTasks = ({ flag, geojsonFile, setGeojsonFile, customLineUpload, custo
             uuid: generateQrSuccess.task_id,
           }),
         );
+        setToggleStatus(true);
       }
     }
   }, [generateQrSuccess]);
@@ -230,167 +228,176 @@ const SplitTasks = ({ flag, geojsonFile, setGeojsonFile, customLineUpload, custo
 
   const parsedTaskGeojsonCount = dividedTaskGeojson?.features?.length || drawnGeojson?.features?.length;
   const totalSteps = dividedTaskGeojson?.features ? dividedTaskGeojson?.features?.length : parsedTaskGeojsonCount;
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="fmtm-flex fmtm-gap-7 fmtm-flex-col lg:fmtm-flex-row">
-        <div className="fmtm-bg-white lg:fmtm-w-[20%] xl:fmtm-w-[17%] fmtm-px-5 fmtm-py-6">
-          <h6 className="fmtm-text-xl fmtm-font-[600] fmtm-pb-2 lg:fmtm-pb-6">Split Tasks</h6>
-          <p className="fmtm-text-gray-500 lg:fmtm-flex lg:fmtm-flex-col lg:fmtm-gap-3">
-            <span>Fill in your project basic information such as name, description, hashtag, etc. </span>
-            <span>To complete the first step, you will need the account credentials of ODK central server.</span>{' '}
-            <span>Here are the instructions for setting up a Central ODK Server on Digital Ocean.</span>
-          </p>
-        </div>
-        <div className="lg:fmtm-w-[80%] xl:fmtm-w-[83%] lg:fmtm-h-[60vh] xl:fmtm-h-[58vh] fmtm-bg-white fmtm-px-5 lg:fmtm-px-11 fmtm-py-6 lg:fmtm-overflow-y-scroll lg:scrollbar">
-          <div className="fmtm-w-full fmtm-flex fmtm-gap-6 md:fmtm-gap-14 fmtm-flex-col md:fmtm-flex-row fmtm-h-full">
-            <div className="fmtm-flex fmtm-flex-col fmtm-gap-6 lg:fmtm-w-[40%] fmtm-justify-between">
-              <div>
-                <RadioButton
-                  value={splitTasksSelection}
-                  topic="Select an option to split the task"
-                  options={alogrithmList}
-                  direction="column"
-                  onChangeData={(value) => {
-                    handleCustomChange('splitTaskOption', value);
-                    dispatch(CreateProjectActions.SetSplitTasksSelection(value));
-                  }}
-                  errorMsg={errors.splitTaskOption}
-                />
-                {splitTasksSelection === 'divide_on_square' && (
-                  <>
-                    <div className="fmtm-mt-6 fmtm-flex fmtm-items-center fmtm-gap-4">
-                      <p className="fmtm-text-gray-500">Dimension of square in metres: </p>
-                      <input
-                        type="number"
-                        value={formValues.dimension}
-                        onChange={(e) => handleCustomChange('dimension', e.target.value)}
-                        className="fmtm-outline-none fmtm-border-[1px] fmtm-border-gray-600 fmtm-h-7 fmtm-w-16 fmtm-px-2 "
-                      />
-                    </div>
-                    {errors.dimension && (
-                      <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm fmtm-py-1">{errors.dimension}</p>
-                    )}
-                  </>
-                )}
-                {formValues.splitTaskOption === 'task_splitting_algorithm' && (
-                  <>
-                    <div className="fmtm-mt-6 fmtm-flex fmtm-items-center fmtm-gap-4">
-                      <p className="fmtm-text-gray-500">Average number of buildings per task: </p>
-                      <input
-                        type="number"
-                        value={formValues.average_buildings_per_task}
-                        onChange={(e) => handleCustomChange('average_buildings_per_task', e.target.value)}
-                        className="fmtm-outline-none fmtm-border-[1px] fmtm-border-gray-600 fmtm-h-7 fmtm-w-16 fmtm-px-2 "
-                      />
-                    </div>
-                    {errors.average_buildings_per_task && (
-                      <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm fmtm-py-1">
-                        {errors.average_buildings_per_task}
-                      </p>
-                    )}
-                  </>
-                )}
-                {(splitTasksSelection === 'divide_on_square' || splitTasksSelection === 'task_splitting_algorithm') && (
-                  <div className="fmtm-mt-6 fmtm-pb-3">
-                    <div className="fmtm-flex fmtm-items-center fmtm-gap-4">
-                      <Button
-                        btnText="Click to generate task"
-                        btnType="primary"
-                        type="button"
-                        isLoading={dividedTaskLoading || taskSplittingGeojsonLoading}
-                        onClick={generateTaskBasedOnSelection}
-                        className=""
-                        icon={<AssetModules.SettingsIcon className="fmtm-text-white" />}
-                      />
-                      {/* <Button
+    <>
+      <Modal
+        className={`fmtm-w-[700px]`}
+        description={
+          <div>
+            <p className="fmtm-text-base">
+              Thank you for successfully completing the project setup. The QR code for each task is being generated at
+              the moment. This may take several minutes to process.
+            </p>
+            <div className="fmtm-p-10">
+              <ProgressBar totalSteps={totalSteps} currentStep={generateProjectLog?.progress} />
+            </div>
+            <p className="fmtm-text-base">
+              You may click Proceed button whilst the process runs in the background. Click Cancel to terminate the
+              process.
+            </p>
+            <div className="fmtm-flex fmtm-justify-center fmtm-gap-6 fmtm-pt-5  ">
+              {/* <Button btnText="CANCEL" btnType="secondary"></Button> */}
+              <Button btnText="PROCEED" onClick={() => setToggleStatus(false)} btnType="primary"></Button>
+            </div>
+          </div>
+        }
+        open={toggleStatus}
+        onOpenChange={(value) => {
+          setToggleStatus(value);
+        }}
+      />
+      <form onSubmit={handleSubmit}>
+        <div className="fmtm-flex fmtm-gap-7 fmtm-flex-col lg:fmtm-flex-row">
+          <div className="fmtm-bg-white lg:fmtm-w-[20%] xl:fmtm-w-[17%] fmtm-px-5 fmtm-py-6">
+            <h6 className="fmtm-text-xl fmtm-font-[600] fmtm-pb-2 lg:fmtm-pb-6">Split Tasks</h6>
+            <p className="fmtm-text-gray-500 lg:fmtm-flex lg:fmtm-flex-col lg:fmtm-gap-3">
+              <span>Fill in your project basic information such as name, description, hashtag, etc. </span>
+              <span>To complete the first step, you will need the account credentials of ODK central server.</span>{' '}
+              <span>Here are the instructions for setting up a Central ODK Server on Digital Ocean.</span>
+            </p>
+          </div>
+          <div className="lg:fmtm-w-[80%] xl:fmtm-w-[83%] lg:fmtm-h-[60vh] xl:fmtm-h-[58vh] fmtm-bg-white fmtm-px-5 lg:fmtm-px-11 fmtm-py-6 lg:fmtm-overflow-y-scroll lg:scrollbar">
+            <div className="fmtm-w-full fmtm-flex fmtm-gap-6 md:fmtm-gap-14 fmtm-flex-col md:fmtm-flex-row fmtm-h-full">
+              <div className="fmtm-flex fmtm-flex-col fmtm-gap-6 lg:fmtm-w-[40%] fmtm-justify-between">
+                <div>
+                  <RadioButton
+                    value={splitTasksSelection}
+                    topic="Select an option to split the task"
+                    options={alogrithmList}
+                    direction="column"
+                    onChangeData={(value) => {
+                      handleCustomChange('splitTaskOption', value);
+                      dispatch(CreateProjectActions.SetSplitTasksSelection(value));
+                    }}
+                    errorMsg={errors.splitTaskOption}
+                  />
+                  {splitTasksSelection === 'divide_on_square' && (
+                    <>
+                      <div className="fmtm-mt-6 fmtm-flex fmtm-items-center fmtm-gap-4">
+                        <p className="fmtm-text-gray-500">Dimension of square in metres: </p>
+                        <input
+                          type="number"
+                          value={formValues.dimension}
+                          onChange={(e) => handleCustomChange('dimension', e.target.value)}
+                          className="fmtm-outline-none fmtm-border-[1px] fmtm-border-gray-600 fmtm-h-7 fmtm-w-16 fmtm-px-2 "
+                        />
+                      </div>
+                      {errors.dimension && (
+                        <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm fmtm-py-1">{errors.dimension}</p>
+                      )}
+                    </>
+                  )}
+                  {formValues.splitTaskOption === 'task_splitting_algorithm' && (
+                    <>
+                      <div className="fmtm-mt-6 fmtm-flex fmtm-items-center fmtm-gap-4">
+                        <p className="fmtm-text-gray-500">Average number of buildings per task: </p>
+                        <input
+                          type="number"
+                          value={formValues.average_buildings_per_task}
+                          onChange={(e) => handleCustomChange('average_buildings_per_task', e.target.value)}
+                          className="fmtm-outline-none fmtm-border-[1px] fmtm-border-gray-600 fmtm-h-7 fmtm-w-16 fmtm-px-2 "
+                        />
+                      </div>
+                      {errors.average_buildings_per_task && (
+                        <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm fmtm-py-1">
+                          {errors.average_buildings_per_task}
+                        </p>
+                      )}
+                    </>
+                  )}
+                  {(splitTasksSelection === 'divide_on_square' ||
+                    splitTasksSelection === 'task_splitting_algorithm') && (
+                    <div className="fmtm-mt-6 fmtm-pb-3">
+                      <div className="fmtm-flex fmtm-items-center fmtm-gap-4">
+                        <Button
+                          btnText="Click to generate task"
+                          btnType="primary"
+                          type="button"
+                          isLoading={dividedTaskLoading || taskSplittingGeojsonLoading}
+                          onClick={generateTaskBasedOnSelection}
+                          className=""
+                          icon={<AssetModules.SettingsIcon className="fmtm-text-white" />}
+                        />
+                        {/* <Button
                         btnText="Stop generating"
                         btnType="secondary"
                         type="button"
                         onClick={() => console.log('stop gen')}
                         className=""
                       /> */}
+                      </div>
                     </div>
-                  </div>
-                )}
-                {splitTasksSelection && (
-                  <p className="fmtm-text-gray-500 fmtm-mt-5">
-                    Total number of task: <span className="fmtm-font-bold">{totalSteps}</span>
-                  </p>
-                )}
+                  )}
+                  {splitTasksSelection && (
+                    <p className="fmtm-text-gray-500 fmtm-mt-5">
+                      Total number of task: <span className="fmtm-font-bold">{totalSteps}</span>
+                    </p>
+                  )}
+                </div>
+                <div className="fmtm-flex fmtm-gap-5 fmtm-mx-auto fmtm-mt-10 fmtm-my-5">
+                  <Button
+                    btnText="PREVIOUS"
+                    btnType="secondary"
+                    type="button"
+                    onClick={() => toggleStep(3, '/data-extract')}
+                    className="fmtm-font-bold"
+                  />
+                  <Button
+                    isLoading={projectDetailsLoading || generateProjectLogLoading}
+                    btnText="SUBMIT"
+                    btnType="primary"
+                    type="submit"
+                    className="fmtm-font-bold"
+                  />
+                </div>
               </div>
-              <div className="fmtm-flex fmtm-gap-5 fmtm-mx-auto fmtm-mt-10 fmtm-my-5">
-                <Button
-                  btnText="PREVIOUS"
-                  btnType="secondary"
-                  type="button"
-                  onClick={() => toggleStep(3, '/data-extract')}
-                  className="fmtm-font-bold"
-                />
-                <Button
-                  isLoading={projectDetailsLoading || generateProjectLogLoading}
-                  btnText="SUBMIT"
-                  btnType="primary"
-                  type="submit"
-                  className="fmtm-font-bold"
+              <div className="fmtm-w-full lg:fmtm-w-[60%] fmtm-flex fmtm-flex-col fmtm-gap-6 fmtm-bg-gray-300 fmtm-h-[60vh] lg:fmtm-h-full">
+                <NewDefineAreaMap
+                  splittedGeojson={dividedTaskGeojson}
+                  uploadedOrDrawnGeojsonFile={drawnGeojson}
+                  buildingExtractedGeojson={buildingGeojson}
+                  lineExtractedGeojson={lineGeojson}
                 />
               </div>
-            </div>
-            <div className="fmtm-w-full lg:fmtm-w-[60%] fmtm-flex fmtm-flex-col fmtm-gap-6 fmtm-bg-gray-300 fmtm-h-[60vh] lg:fmtm-h-full">
-              <NewDefineAreaMap
-                splittedGeojson={dividedTaskGeojson}
-                uploadedOrDrawnGeojsonFile={drawnGeojson}
-                buildingExtractedGeojson={buildingGeojson}
-                lineExtractedGeojson={lineGeojson}
-              />
-            </div>
-            <div className="fmtm-w-full lg:fmtm-w-[60%] fmtm-flex fmtm-flex-col fmtm-gap-6 fmtm-bg-gray-300 fmtm-h-[60vh] lg:fmtm-h-full">
-              <CoreModules.Stack>
-                {generateProjectLog ? (
-                  <CoreModules.Stack
-                    sx={{ display: 'flex', flexDirection: 'col', gap: 2, width: { xs: '100%', md: '60%' }, pb: '2rem' }}
-                  >
-                    <CoreModules.Stack sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                      <CoreModules.Typography component="h4">Status: </CoreModules.Typography>
-                      <CoreModules.Typography
-                        component="h4"
-                        sx={{ ml: 2, fontWeight: 'bold', borderRadius: '20px', border: '1px solid gray', p: 1 }}
+              {generateProjectLog ? (
+                <div className="fmtm-w-full lg:fmtm-w-[60%] fmtm-flex fmtm-flex-col fmtm-gap-6  fmtm-h-[60vh] lg:fmtm-h-full">
+                  <Button btnText="Show Progress" btnType="primary" onClick={() => setToggleStatus(true)} />
+                  <CoreModules.Stack>
+                    <CoreModules.Stack sx={{ width: '100%', height: '48vh' }}>
+                      <div
+                        ref={divRef}
+                        style={{
+                          backgroundColor: 'black',
+                          color: 'white',
+                          padding: '10px',
+                          fontSize: '12px',
+                          whiteSpace: 'pre-wrap',
+                          fontFamily: 'monospace',
+                          overflow: 'auto',
+                          height: '100%',
+                        }}
                       >
-                        {generateProjectLog.status}
-                      </CoreModules.Typography>
+                        {renderTraceback(generateProjectLog?.logs)}
+                      </div>
                     </CoreModules.Stack>
-                    <LoadingBar
-                      title={'Task Progress'}
-                      activeStep={generateProjectLog.progress}
-                      totalSteps={totalSteps}
-                    />
                   </CoreModules.Stack>
-                ) : null}
-                {generateProjectLog ? (
-                  <CoreModules.Stack sx={{ width: '90%', height: '48vh' }}>
-                    <div
-                      ref={divRef}
-                      style={{
-                        backgroundColor: 'black',
-                        color: 'white',
-                        padding: '10px',
-                        fontSize: '12px',
-                        whiteSpace: 'pre-wrap',
-                        fontFamily: 'monospace',
-                        overflow: 'auto',
-                        height: '100%',
-                      }}
-                    >
-                      {renderTraceback(generateProjectLog?.logs)}
-                    </div>
-                  </CoreModules.Stack>
-                ) : null}
-              </CoreModules.Stack>
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
