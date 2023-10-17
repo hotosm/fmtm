96b0ec5a4016652d4212d20fcc638a750dde16f4
NSUWAL123 <suwalnishit@gmail.com>
2023-10-17 11:30:16 +0545

    fix (projectDetails): mapLegends seperated to different bottomSheet for small screen view

--- src/frontend/src/components/MapLegends.jsx
+++ src/frontend/src/components/MapLegends.jsx
@@ -68,28 +68,33 @@ const MapLegends = ({ direction, spacing, iconBtnProps, defaultTheme, valueStatu
     //     );
     //   })}
     // </CoreModules.Stack>
-
-    <div className="fmtm-flex fmtm-flex-col fmtm-gap-4">
-      {MapDetails.map((data, index) => {
-        return (
-          <div key={index} className="fmtm-flex fmtm-items-center fmtm-gap-3">
-            <div className="fmtm-border-[1px] fmtm-border-gray-200">
-              {data.type !== 'locked' ? (
-                <CoreModules.IconButton
-                  style={{ backgroundColor: data.color, borderRadius: 0 }}
-                  {...iconBtnProps}
-                  color="primary"
-                  component="label"
-                  className="fmtm-w-10 fmtm-h-10"
-                ></CoreModules.IconButton>
-              ) : (
-                <AssetModules.LockIcon sx={{ fontSize: '40px' }} />
-              )}
+    <div>
+      <div className="fmtm-flex fmtm-gap-3 fmtm-border-b-[1px] fmtm-pb-2 fmtm-mb-4">
+        <AssetModules.LegendToggleIcon className=" fmtm-text-primaryRed" sx={{ fontSize: '35px' }} />
+        <p className="fmtm-text-2xl">Map Legend</p>
+      </div>
+      <div className="fmtm-flex fmtm-flex-col fmtm-gap-4">
+        {MapDetails.map((data, index) => {
+          return (
+            <div key={index} className="fmtm-flex fmtm-items-center fmtm-gap-3">
+              <div className="fmtm-border-[1px] fmtm-border-gray-200">
+                {data.type !== 'locked' ? (
+                  <CoreModules.IconButton
+                    style={{ backgroundColor: data.color, borderRadius: 0 }}
+                    {...iconBtnProps}
+                    color="primary"
+                    component="label"
+                    className="fmtm-w-10 fmtm-h-10"
+                  ></CoreModules.IconButton>
+                ) : (
+                  <AssetModules.LockIcon sx={{ fontSize: '40px' }} />
+                )}
+              </div>
+              <p className="fmtm-text-lg">{data.value}</p>
             </div>
-            <p className="fmtm-text-lg">{data.value}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
