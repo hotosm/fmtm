37c17962cbab5ece58e824e5695e6d0b5cc72026
spwoodcock <sam.woodcock@protonmail.com>
2023-10-19 16:09:08 +0100

    build: update odkcentral img, add entrypoint + healthcheck

--- odkcentral/api/init-user-and-start.sh
+++ odkcentral/api/init-user-and-start.sh
@@ -1,5 +1,8 @@
 set -eo pipefail
 
+# Wait for database to be available
+wait-for-it "${CENTRAL_DB_HOST:-central-db}:5432"
+
 ### Init, generate config, migrate db ###
 echo "Stripping pm2 exec command from start-odk.sh script (last 2 lines)"
 head -n -2 ./start-odk.sh > ./init-odk-db.sh
@@ -9,7 +12,6 @@ echo "Running ODKCentral start script to init environment and migrate DB"
 echo "The server will not start on this run"
 ./init-odk-db.sh
 
-
 ### Create admin user ###
 echo "Creating test user ${SYSADMIN_EMAIL} with password ${SYSADMIN_PASSWD}"
 echo "${SYSADMIN_PASSWD}" | odk-cmd --email "${SYSADMIN_EMAIL}" user-create || true
@@ -17,8 +19,7 @@ echo "${SYSADMIN_PASSWD}" | odk-cmd --email "${SYSADMIN_EMAIL}" user-create || t
 echo "Elevating user to admin"
 odk-cmd --email "${SYSADMIN_EMAIL}" user-promote || true
 
-
-### Run server ###
+### Run server (copied from start-odk.sh) ###
 MEMTOT=$(vmstat -s | grep 'total memory' | awk '{ print $1 }')
 if [ "$MEMTOT" -gt "1100000" ]
 then
@@ -28,5 +29,5 @@ else
 fi
 echo "using $WORKER_COUNT worker(s) based on available memory ($MEMTOT).."
 
-echo "Starting server"
-exec pm2-runtime ./pm2.config.js
+echo "Starting server."
+exec npx pm2-runtime ./pm2.config.js
