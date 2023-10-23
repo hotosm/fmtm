9615ffe95ce705c5229240344db253275bfd3cba
spwoodcock <sam.woodcock@protonmail.com>
2023-10-23 16:11:35 +0100

    ci: merge workflows from development

--- .github/workflows/tests/test_ci.sh
+++ .github/workflows/tests/test_ci.sh
@@ -11,20 +11,37 @@ set -e
 # GITHUB_TOKEN=input
 # Feed to act using -s flag: -s GITHUB_TOKEN=input_personal_access_token
 
-# PR
-act pull_request -W .github/workflows/pr.yml -e .github/workflows/tests/pr_payload.json
+# PR Test Backend
+act pull_request -W .github/workflows/pr_test_backend.yml \
+    -e .github/workflows/tests/pr_payload.json \
+    --var-file=.env --secret-file=.env
+
+# PR Test Frontend
+act pull_request -W .github/workflows/pr_test_frontend.yml \
+    -e .github/workflows/tests/pr_payload.json \
+    --var-file=.env --secret-file=.env
 
 # Build and deploy
-act push -W .github/workflows/build_and_deploy.yml -e .github/workflows/tests/push_payload.json
+act push -W .github/workflows/build_and_deploy.yml \
+    -e .github/workflows/tests/push_payload.json \
+    --var-file=.env --secret-file=.env
 
 # CI Img Build
-act push -W .github/workflows/build_ci_img.yml -e .github/workflows/tests/push_payload.json
+act push -W .github/workflows/build_ci_img.yml \
+    -e .github/workflows/tests/push_payload.json \
+    --var-file=.env --secret-file=.env
 
 # ODK Img Build
-act push -W .github/workflows/build_odk_imgs.yml -e .github/workflows/tests/push_payload.json
+act push -W .github/workflows/build_odk_imgs.yml \
+    -e .github/workflows/tests/push_payload.json \
+    --var-file=.env --secret-file=.env
 
 # Docs
-act push -W .github/workflows/docs.yml -e .github/workflows/tests/push_payload.json
+act push -W .github/workflows/docs.yml \
+    -e .github/workflows/tests/push_payload.json \
+    --var-file=.env --secret-file=.env
 
 # Wiki
-act push -W .github/workflows/wiki.yml -e .github/workflows/tests/push_payload.json
+act push -W .github/workflows/wiki.yml \
+    -e .github/workflows/tests/push_payload.json \
+    --var-file=.env --secret-file=.env
