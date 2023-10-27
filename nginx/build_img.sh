ca528377e151fd59a1ec756953cf9c84405f094d
spwoodcock <sam.woodcock@protonmail.com>
2023-10-27 09:59:59 +0100

    build: add PUSH_IMGS option to certs-init build

--- nginx/build_img.sh
+++ nginx/build_img.sh
@@ -5,7 +5,15 @@ docker build nginx \
     --target api-only-cert-init \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
 
+if [[ -n "$PUSH_IMGS" ]]; then
+    docker push "ghcr.io/hotosm/fmtm/certs-init:latest"
+fi
+
 docker build nginx \
     --tag "ghcr.io/hotosm/fmtm/certs-init-all:latest" \
     --target all-cert-init \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
+
+if [[ -n "$PUSH_IMGS" ]]; then
+    docker push "ghcr.io/hotosm/fmtm/certs-init-all:latest"
+fi
