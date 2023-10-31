95c39dacc5fbfcb73afb8b1a431da867d5870b83
spwoodcock <sam.woodcock@protonmail.com>
2023-10-31 06:29:17 +0000

    build: don't cache local cert-init img builds

--- nginx/build_img.sh
+++ nginx/build_img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build nginx \
+docker build nginx --no-cache \
     --tag "ghcr.io/hotosm/fmtm/certs-init:latest" \
     --target certs-init \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
@@ -9,7 +9,7 @@ if [[ -n "$PUSH_IMGS" ]]; then
     docker push "ghcr.io/hotosm/fmtm/certs-init:latest"
 fi
 
-docker build nginx \
+docker build nginx --no-cache \
     --tag "ghcr.io/hotosm/fmtm/certs-init-all:latest" \
     --target certs-init-all \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
