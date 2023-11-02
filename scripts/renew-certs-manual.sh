909e967781ab397702ea6bafb564aab8313c6fd5
spwoodcock <sam.woodcock@protonmail.com>
2023-11-02 12:30:32 +0000

    build: fix renew_certs_manual script

--- scripts/renew-certs-manual.sh
+++ scripts/renew-certs-manual.sh
@@ -2,4 +2,34 @@
 
 set -euo pipefail
 
-docker compose exec proxy certbot renew
+# Prompt the user for input and set the BRANCH_NAME variable
+read -p "Enter the environment (dev/staging/prod): " ENVIRONMENT
+
+case "$ENVIRONMENT" in
+    dev)
+        BRANCH_NAME="development"
+        ;;
+    staging)
+        BRANCH_NAME="staging"
+        ;;
+    prod)
+        BRANCH_NAME="main"
+        ;;
+    *)
+        echo "Invalid environment. Please enter dev, staging, or prod."
+        exit 1
+        ;;
+esac
+
+# Check if any containers using the 'ghcr.io/hotosm/fmtm/proxy:${BRANCH_NAME}' image are running
+if [[ -z $(docker ps -q -f "ancestor=ghcr.io/hotosm/fmtm/proxy:${BRANCH_NAME}") ]]; then
+    echo "No containers using the 'ghcr.io/hotosm/fmtm/proxy:${BRANCH_NAME}' image are running."
+    echo "You must first start the containers using:"
+    echo
+    echo "docker-compose -f docker-compose.${BRANCH_NAME}.yml up -d"
+    echo
+    exit 1
+fi
+
+# Execute the Docker Compose command with the determined BRANCH_NAME
+docker compose exec "fmtm-$BRANCH_NAME" certbot --non-interactive renew
