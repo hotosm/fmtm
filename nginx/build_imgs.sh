00c36c138efd93fb237c3458019767a09ac3ba67
spwoodcock <sam.woodcock@protonmail.com>
2023-11-05 09:50:50 +0000

    build: use cert init images specific to branches

--- nginx/build_imgs.sh
+++ nginx/build_imgs.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-# Certs init image
+# Dev certs init
+echo "Building proxy:certs-init-development"
 docker build nginx \
-    --tag "ghcr.io/hotosm/fmtm/proxy:certs-init" \
-    --target certs-init \
+    --tag "ghcr.io/hotosm/fmtm/proxy:certs-init-development" \
+    --target certs-init-development \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
 
 if [[ -n "$PUSH_IMGS" ]]; then
-    docker push "ghcr.io/hotosm/fmtm/proxy:certs-init"
+    docker push "ghcr.io/hotosm/fmtm/proxy:certs-init-development"
 fi
 
-# Development proxy
+# Staging certs init
+echo "Tagging proxy:certs-init-staging"
+docker tag "ghcr.io/hotosm/fmtm/proxy:certs-init-development" \
+    "ghcr.io/hotosm/fmtm/proxy:certs-init-staging"
+
+if [[ -n "$PUSH_IMGS" ]]; then
+    docker push "ghcr.io/hotosm/fmtm/proxy:certs-init-staging"
+fi
+
+# Main certs init
+echo "Building proxy:certs-init-main"
+docker build nginx \
+    --tag "ghcr.io/hotosm/fmtm/proxy:certs-init-main" \
+    --target certs-init-main \
+    --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
+
+if [[ -n "$PUSH_IMGS" ]]; then
+    docker push "ghcr.io/hotosm/fmtm/proxy:certs-init-main"
+fi
+
+# Dev proxy
+echo "Building proxy:development"
 docker build nginx \
     --tag "ghcr.io/hotosm/fmtm/proxy:development" \
     --target development \
@@ -21,6 +43,7 @@ if [[ -n "$PUSH_IMGS" ]]; then
 fi
 
 # Staging proxy
+echo "Tagging proxy:staging"
 docker tag "ghcr.io/hotosm/fmtm/proxy:development" \
     "ghcr.io/hotosm/fmtm/proxy:staging"
 
@@ -29,6 +52,7 @@ if [[ -n "$PUSH_IMGS" ]]; then
 fi
 
 # Main proxy
+echo "Building proxy:main"
 docker build nginx \
     --tag "ghcr.io/hotosm/fmtm/proxy:main" \
     --target main \
