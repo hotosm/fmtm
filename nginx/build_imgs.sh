69218fe17973f19caaa8a2ffb06f4fb4b3b9e9de
spwoodcock <sam.woodcock@protonmail.com>
2023-11-05 07:01:13 +0000

    build: rename certs-init image to proxy:certs-init tag

--- nginx/build_imgs.sh
+++ nginx/build_imgs.sh
@@ -2,18 +2,18 @@
 
 # Certs init image
 docker build nginx \
-    --tag "ghcr.io/hotosm/fmtm/certs-init:latest" \
+    --tag "ghcr.io/hotosm/fmtm/proxy:certs-init" \
     --target certs-init \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
 
 if [[ -n "$PUSH_IMGS" ]]; then
-    docker push "ghcr.io/hotosm/fmtm/certs-init:latest"
+    docker push "ghcr.io/hotosm/fmtm/proxy:certs-init"
 fi
 
 # Development proxy
 docker build nginx \
     --tag "ghcr.io/hotosm/fmtm/proxy:development" \
-    --target all \
+    --target development \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
 
 if [[ -n "$PUSH_IMGS" ]]; then
@@ -31,7 +31,7 @@ fi
 # Main proxy
 docker build nginx \
     --tag "ghcr.io/hotosm/fmtm/proxy:main" \
-    --target api-only \
+    --target main \
     --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
 
 if [[ -n "$PUSH_IMGS" ]]; then
