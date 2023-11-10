9a9c422329f0cc4260869ce51c5d35454496ca57
spwoodcock <sam.woodcock@protonmail.com>
2023-11-10 09:59:34 +0000

    build: remove default script domain init (only if var set)

--- nginx/build_imgs.sh
+++ nginx/build_imgs.sh
@@ -61,3 +61,14 @@ docker build nginx \
 if [[ -n "$PUSH_IMGS" ]]; then
     docker push "ghcr.io/hotosm/fmtm/proxy:main"
 fi
+
+# Main plus script proxy
+echo "Building proxy:main-plus-script"
+docker build nginx \
+    --tag "ghcr.io/hotosm/fmtm/proxy:main-plus-script" \
+    --target main-plus-script \
+    --build-arg NGINX_TAG="${NGINX_TAG:-1.25.2}"
+
+if [[ -n "$PUSH_IMGS" ]]; then
+    docker push "ghcr.io/hotosm/fmtm/proxy:main-plus-script"
+fi
