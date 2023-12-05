65afbfe6633e8b04bb5bcf2d3771a773774ddfd8
NSUWAL123 <suwalnishit@gmail.com>
2023-12-05 11:15:09 +0545

    fix (taskPriorityStr): task_priority_str enums added

--- src/frontend/src/types/enums.ts
+++ src/frontend/src/types/enums.ts
@@ -3,3 +3,15 @@ export enum task_split_type {
   choose_area_as_task = 1,
   task_splitting_algorithm = 2,
 }
+
+export enum task_priority_str {
+  READY = 0,
+  LOCKED_FOR_MAPPING = 1,
+  MAPPED = 2,
+  LOCKED_FOR_VALIDATION = 3,
+  VALIDATED = 4,
+  INVALIDATED = 5,
+  BAD = 6,
+  SPLIT = 7,
+  ARCHIVED = 8,
+}
