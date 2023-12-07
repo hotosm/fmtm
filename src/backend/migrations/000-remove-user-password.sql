2da7b50e76bc1dcd808f86b831ab3ed4dafaa1ba
spwoodcock <sam.woodcock@protonmail.com>
2023-12-07 05:22:34 +0000

    refactor: separate migration reversal scripts to dir

--- src/backend/migrations/000-remove-user-password.sql
+++ src/backend/migrations/000-remove-user-password.sql
@@ -1,7 +1,5 @@
 -- ## Migration to remove password field from public.users (replaced with OSM OAuth)
 
-
--- ## Apply Migration
 -- Start a transaction
 BEGIN;
 -- Drop the 'password' column if it exists
@@ -9,13 +7,3 @@ ALTER TABLE IF EXISTS public.users
 DROP COLUMN IF EXISTS password;
 -- Commit the transaction
 COMMIT;
-
-
--- ## Revert Migration (comment above, uncomment below)
--- -- Start a transaction
--- BEGIN;
--- -- Add the 'password' column back if it doesn't exist
--- ALTER TABLE public.users
--- ADD COLUMN IF NOT EXISTS password character varying;
--- -- Commit the transaction
--- COMMIT;
