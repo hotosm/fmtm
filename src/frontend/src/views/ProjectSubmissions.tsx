96cfbde6352ec3979a4f2b05edf701669929ca32
NSUWAL123 <suwalnishit@gmail.com>
2023-12-08 12:55:41 +0545

    fix (projectSubmissions): styling adjustments

--- src/frontend/src/views/ProjectSubmissions.tsx
+++ src/frontend/src/views/ProjectSubmissions.tsx
@@ -7,9 +7,9 @@ import SubmissionsTable from '../components/ProjectSubmissions/SubmissionsTable.
 const ProjectSubmissions = () => {
   const [viewBy, setViewBy] = useState<'infographics' | 'table'>('infographics');
   return (
-    <div className="fmtm-bg-[#F5F5F5]">
-      <div className="fmtm-flex">
-        <div className="fmtm-bg-white fmtm-flex fmtm-flex-col fmtm-gap-4 fmtm-mx-4 fmtm-my-4 fmtm-p-2">
+    <div className="fmtm-bg-[#F5F5F5] fmtm-px-5 sm:fmtm-px-0 sm:fmtm-pr-10 2xl:fmtm-pr-20">
+      <div className="fmtm-flex fmtm-flex-col sm:fmtm-flex-row fmtm-my-4 fmtm-w-full">
+        <div className="sm:fmtm-bg-white fmtm-flex sm:fmtm-flex-col fmtm-gap-4 sm:fmtm-mx-6 sm:fmtm-p-2 fmtm-justify-end sm:fmtm-justify-start">
           <AssetModules.GridViewIcon
             style={{ fontSize: '30px' }}
             className={`${
@@ -25,11 +25,11 @@ const ProjectSubmissions = () => {
             onClick={() => setViewBy('table')}
           />
         </div>
-        <div>
+        <div className="fmtm-flex fmtm-flex-grow ">
           <ProjectInfo />
         </div>
       </div>
-      <div>{viewBy === 'infographics' ? <SubmissionsInfographics /> : <SubmissionsTable />}</div>
+      <div className="fmtm-w-ful">{viewBy === 'infographics' ? <SubmissionsInfographics /> : <SubmissionsTable />}</div>
     </div>
   );
 };
