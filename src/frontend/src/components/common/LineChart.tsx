e0c66200ed1cdc5f742649ad9d62d1fae9cbcae5
NSUWAL123 <suwalnishit@gmail.com>
2023-12-13 11:35:30 +0545

    fix (lineChart): line color changed

--- src/frontend/src/components/common/LineChart.tsx
+++ src/frontend/src/components/common/LineChart.tsx
@@ -4,44 +4,44 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 const data = [
   {
     name: 'Page A',
-    uv: 4000,
-    pv: 2400,
+    Actual: 4000,
+    Planned: 2400,
     amt: 2400,
   },
   {
     name: 'Page B',
-    uv: 3000,
-    pv: 1398,
+    Actual: 3000,
+    Planned: 1398,
     amt: 2210,
   },
   {
     name: 'Page C',
-    uv: 2000,
-    pv: 9800,
+    Actual: 2000,
+    Planned: 9800,
     amt: 2290,
   },
   {
     name: 'Page D',
-    uv: 2780,
-    pv: 3908,
+    Actual: 2780,
+    Planned: 3908,
     amt: 2000,
   },
   {
     name: 'Page E',
-    uv: 1890,
-    pv: 4800,
+    Actual: 1890,
+    Planned: 4800,
     amt: 2181,
   },
   {
     name: 'Page F',
-    uv: 2390,
-    pv: 3800,
+    Actual: 2390,
+    Planned: 3800,
     amt: 2500,
   },
   {
     name: 'Page G',
-    uv: 3490,
-    pv: 4300,
+    Actual: 3490,
+    Planned: 4300,
     amt: 2100,
   },
 ];
@@ -54,19 +54,17 @@ const CustomLineChart = () => {
         height={300}
         data={data}
         margin={{
-          top: 5,
           right: 30,
           left: 20,
-          bottom: 5,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Legend />
-        <Line type="linear" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="linear" dataKey="uv" stroke="#82ca9d" />
+        <Legend iconSize={10} iconType="circle" align="left" verticalAlign="top" height={45} />
+        <Line type="linear" dataKey="Planned" stroke="#854C1F" activeDot={{ r: 5 }} />
+        <Line type="linear" dataKey="Actual" stroke="#0A8A40" activeDot={{ r: 5 }} />
       </LineChart>
     </ResponsiveContainer>
   );
