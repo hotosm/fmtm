2c0ee483fca99d1f27fa2f964dbbaf3f9adac357
NSUWAL123 <suwalnishit@gmail.com>
2023-12-20 10:10:22 +0545

    feat (projectSubmissions): task api call

--- src/frontend/src/views/ProjectSubmissions.tsx
+++ src/frontend/src/views/ProjectSubmissions.tsx
@@ -1,11 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AssetModules from '../shared/AssetModules.js';
 import ProjectInfo from '../components/ProjectSubmissions/ProjectInfo.js';
 import SubmissionsInfographics from '../components/ProjectSubmissions/SubmissionsInfographics.js';
 import SubmissionsTable from '../components/ProjectSubmissions/SubmissionsTable.js';
+import CoreModules from '../shared/CoreModules';
+import { ProjectActions } from '../store/slices/ProjectSlice';
+import { ProjectById } from '../api/Project';
+import environment from '../environment';
+import { fetchInfoTask } from '../api/task';
 
 const ProjectSubmissions = () => {
+  const dispatch = CoreModules.useAppDispatch();
+  const params = CoreModules.useParams();
+
+  const encodedId = params.projectId;
+  const decodedId = environment.decode(encodedId);
+
   const [viewBy, setViewBy] = useState<'infographics' | 'table'>('infographics');
+  const state = CoreModules.useAppSelector((state) => state.project);
+  const projectInfo = CoreModules.useAppSelector((state) => state.project.projectInfo);
+
+  //Fetch project for the first time
+  useEffect(() => {
+    dispatch(ProjectActions.SetNewProjectTrigger());
+    if (state.projectTaskBoundries.findIndex((project) => project.id == environment.decode(encodedId)) == -1) {
+      dispatch(ProjectActions.SetProjectTaskBoundries([]));
+      dispatch(ProjectById(state.projectTaskBoundries, environment.decode(encodedId)));
+    } else {
+      dispatch(ProjectActions.SetProjectTaskBoundries([]));
+      dispatch(ProjectById(state.projectTaskBoundries, environment.decode(encodedId)));
+    }
+    if (Object.keys(state.projectInfo).length == 0) {
+      dispatch(ProjectActions.SetProjectInfo(projectInfo));
+    } else {
+      if (state.projectInfo.id != environment.decode(encodedId)) {
+        dispatch(ProjectActions.SetProjectInfo(projectInfo));
+      }
+    }
+  }, [params.id]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      dispatch(fetchInfoTask(`${import.meta.env.VITE_API_URL}/tasks/tasks-features/?project_id=${decodedId}`));
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="fmtm-bg-[#F5F5F5] fmtm-px-5 sm:fmtm-px-5 fmtm-pb-5">
       <div className="fmtm-flex fmtm-flex-col sm:fmtm-flex-row fmtm-my-4 fmtm-w-full">
