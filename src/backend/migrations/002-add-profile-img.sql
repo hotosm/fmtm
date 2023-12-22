f12fbe108f604cd262623bc2e2b93c9770ebfa63
sujanadh <sujanadh07@gmail.com>
2023-12-22 10:09:15 +0545

    Revert migration inside revert folder

--- src/backend/migrations/002-add-profile-img.sql
+++ src/backend/migrations/002-add-profile-img.sql
@@ -8,15 +8,3 @@ ALTER TABLE IF EXISTS public.users
     ADD COLUMN IF NOT EXISTS profile_img VARCHAR;
 -- Commit the transaction
 COMMIT;
-
--- -- Revert migration for removing the profile_img field
-
--- -- Start a transaction
--- BEGIN;
-
--- -- Remove the profile_img column from the public.users table
--- ALTER TABLE IF EXISTS public.users
---     DROP COLUMN IF EXISTS profile_img;
-
--- -- Commit the transaction
--- COMMIT;
