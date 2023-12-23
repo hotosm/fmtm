6b2847cc73824754df5830579c6ddca81591c5ba
Sujan Adhikari <109404840+Sujanadh@users.noreply.github.com>
2023-12-23 16:37:58 +0545

    fix: add image to user profile (#1053)
    * feat: added new field to user model to store user profile_img
    
    * feat: updated schema to add updated task history
    
    * updated response of update_task_status
    
    * revert migration
    
    * Revert migration inside revert folder
    
    ---------
    
    Co-authored-by: sujanadh <sujanadh07@gmail.com>

---
+++ src/backend/migrations/revert/002-add-profile-img.sql
@@ -0,0 +1,9 @@
+-- Start a transaction
+BEGIN;
+
+-- Remove the profile_img column from the public.users table
+ALTER TABLE IF EXISTS public.users
+    DROP COLUMN IF EXISTS profile_img;
+
+-- Commit the transaction
+COMMIT;
