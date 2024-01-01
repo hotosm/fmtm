d728fa7aa6ff6033aa5c444b1f7f1b07b143a4be
Sujan Adhikari <109404840+Sujanadh@users.noreply.github.com>
2024-01-02 02:31:37 +0545

    refactor: pydantic v2 deprecations (#1074)
    Co-authored-by: sujanadh <sujanadh07@gmail.com>

--- src/backend/app/projects/project_schemas.py
+++ src/backend/app/projects/project_schemas.py
@@ -22,7 +22,7 @@ from dateutil import parser
 from typing import List, Optional
 
 from geojson_pydantic import Feature as GeojsonFeature
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
 
 from app.db import db_models
 from app.models.enums import ProjectPriority, ProjectStatus, TaskSplitType
@@ -161,12 +161,12 @@ class ProjectDashboard(BaseModel):
     created: datetime
     last_active: Optional[str] = None
 
-    @validator("created", pre=False, always=True)
+    @field_validator("created", mode="before")
     def get_created(cls, value, values):
         date = value.strftime("%d %b %Y")
         return date
     
-    @validator("last_active", pre=False, always=True)
+    @field_validator("last_active", mode="before")
     def get_last_active(cls, value, values):
         if value is None:
             return None
