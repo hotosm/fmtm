7bde9b594dbbe290aaa1cac6d33c2847c3a10df1
Sujan Adhikari <109404840+Sujanadh@users.noreply.github.com>
2024-01-05 23:20:28 +0545

    refactor: serialization for task_status and created project dashboard fields (#1076)
    * fix: Convert task_status to string in TaskBase schema
    
    * fix: Convert created date to string in ProjectDashboard schema
    
    * fix: datetime type and task status type
    
    * fix: use field_serializer where appropriate in models
    
    * fix: remove validators for task_status & created fields
    
    ---------
    
    Co-authored-by: sujanadh <sujanadh07@gmail.com>
    Co-authored-by: spwoodcock <sam.woodcock@protonmail.com>

--- src/backend/app/projects/project_schemas.py
+++ src/backend/app/projects/project_schemas.py
@@ -18,11 +18,11 @@
 
 import uuid
 from datetime import datetime
-from dateutil import parser
 from typing import List, Optional
 
+from dateutil import parser
 from geojson_pydantic import Feature as GeojsonFeature
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 
 from app.db import db_models
 from app.models.enums import ProjectPriority, ProjectStatus, TaskSplitType
@@ -142,6 +142,7 @@ class ProjectBase(BaseModel):
 class ProjectOut(ProjectBase):
     project_uuid: uuid.UUID = uuid.uuid4()
 
+
 class ReadProject(ProjectBase):
     project_uuid: uuid.UUID = uuid.uuid4()
     location_str: str
@@ -162,11 +163,6 @@ class ProjectDashboard(BaseModel):
     total_contributors: Optional[int] = None
     last_active: Optional[str] = None
 
-    @field_validator("created", mode="before")
-    def get_created(cls, value, values):
-        date = value.strftime("%d %b %Y")
-        return date
-    
     @field_validator("last_active", mode="before")
     def get_last_active(cls, value, values):
         if value is None:
@@ -180,9 +176,9 @@ class ProjectDashboard(BaseModel):
         days_difference = time_difference.days
 
         if days_difference == 0:
-            return 'today'
+            return "today"
         elif days_difference == 1:
-            return 'yesterday'
+            return "yesterday"
         elif days_difference < 7:
             return f'{days_difference} day{"s" if days_difference > 1 else ""} ago'
         else:
