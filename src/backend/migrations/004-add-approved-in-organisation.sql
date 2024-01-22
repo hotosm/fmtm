ffaafd551231b9e5205f09e8249c055725137df7
Niraj Adhikari <nrjadkry@gmail.com>
2024-01-22 10:31:02 +0545

    fix: table name in migration file for organisations

--- src/backend/migrations/004-add-approved-in-organisation.sql
+++ src/backend/migrations/004-add-approved-in-organisation.sql
@@ -2,8 +2,8 @@
 BEGIN;
 
 -- Add the 'approved' column with default value false to the 'organisation' table
-ALTER TABLE organisation
-ADD COLUMN approved BOOLEAN DEFAULT false;
+ALTER TABLE organisations
+    ADD COLUMN approved BOOLEAN DEFAULT false;
 
 -- Commit the transaction
 COMMIT;
--- src/backend/migrations/revert/004-remove-approved-in-organisation.sql
+++ src/backend/migrations/revert/004-remove-approved-in-organisation.sql
@@ -2,7 +2,7 @@
 BEGIN;
 
 -- Revert changes to the 'organisation' table
-ALTER TABLE IF EXISTS organisation 
+ALTER TABLE IF EXISTS organisations
     DROP COLUMN IF EXISTS approved;
 
 -- Commit the transaction
