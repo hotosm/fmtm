69337a4267e0e41c64e6f2f78a79648d6526417c
spwoodcock <sam.woodcock@protonmail.com>
2024-01-28 16:27:26 +0000

    fix: case when project_log.json does not exist

--- src/backend/app/projects/project_routes.py
+++ src/backend/app/projects/project_routes.py
@@ -216,10 +216,12 @@ async def read_project(project_id: int, db: Session = Depends(database.get_db)):
 async def delete_project(
     project: db_models.DbProject = Depends(project_deps.get_project_by_id),
     db: Session = Depends(database.get_db),
-    user_data: AuthUser = Depends(login_required),
+    current_user: AuthUser = Depends(login_required),
 ):
     """Delete a project from both ODK Central and the local database."""
-    log.info(f"User {user_data.username} attempting deletion of project {project.id}")
+    log.info(
+        f"User {current_user.username} attempting deletion of project {project.id}"
+    )
     # Odk crendentials
     odk_credentials = project_schemas.ODKCentral(
         odk_central_url=project.odk_central_url,
@@ -549,6 +551,7 @@ async def generate_files(
     xls_form_config_file: Optional[UploadFile] = File(None),
     data_extracts: Optional[UploadFile] = File(None),
     db: Session = Depends(database.get_db),
+    # current_user: AuthUser = Depends(login_required),
 ):
     """Generate additional content to initialise the project.
 
@@ -556,7 +559,7 @@ async def generate_files(
 
     Accepts a project ID, category, custom form flag, and an uploaded file as inputs.
     The generated files are associated with the project ID and stored in the database.
-    This api generates qr_code, forms. This api also creates an app user for
+    This api generates odk appuser tokens, forms. This api also creates an app user for
     each task and provides the required roles.
     Some of the other functionality of this api includes converting a xls file
     provided by the user to the xform, generates osm data extracts and uploads
@@ -727,7 +730,9 @@ async def generate_log(
             .first()
         ).extract_completed_count
 
-        with open("/opt/logs/create_project.json", "r") as log_file:
+        project_log_file = Path("/opt/logs/create_project.json")
+        project_log_file.touch(exist_ok=True)
+        with open(project_log_file, "r") as log_file:
             logs = [json.loads(line) for line in log_file]
 
             filtered_logs = [
