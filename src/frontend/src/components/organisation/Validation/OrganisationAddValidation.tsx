24c6e8a959ec43043a2bc71bc0dd487b04b22edb
spwoodcock <sam.woodcock@protonmail.com>
2024-01-31 16:37:41 +0000

    feat(frontend): option to add default odk creds for organisation

--- src/frontend/src/components/organisation/Validation/OrganisationAddValidation.tsx
+++ src/frontend/src/components/organisation/Validation/OrganisationAddValidation.tsx
@@ -4,6 +4,9 @@ interface OrganisationValues {
   description: string;
   url: string;
   type: number;
+  odk_central_url: string;
+  odk_central_user: string;
+  odk_central_password: string;
 }
 interface ValidationErrors {
   logo?: string;
@@ -11,6 +14,9 @@ interface ValidationErrors {
   description?: string;
   url?: string;
   type?: string;
+  odk_central_url?: string;
+  odk_central_user?: string;
+  odk_central_password?: string;
 }
 
 function isValidUrl(url: string) {
@@ -25,20 +31,27 @@ function isValidUrl(url: string) {
 function OrganisationAddValidation(values: OrganisationValues) {
   const errors: ValidationErrors = {};
 
-  // if (!values?.logo) {
-  //   errors.logo = 'Logo is Required.';
-  // }
   if (!values?.name) {
     errors.name = 'Name is Required.';
   }
+
   if (!values?.description) {
     errors.description = 'Description is Required.';
   }
+
   if (!values?.url) {
     errors.url = 'Organization Url is Required.';
   } else if (!isValidUrl(values.url)) {
     errors.url = 'Invalid URL.';
   }
+
+  if (values?.odk_central_url && !isValidUrl(values.odk_central_url)) {
+    errors.odk_central_url = 'Invalid URL.';
+  }
+
+  // if (!values?.logo) {
+  //   errors.logo = 'Logo is Required.';
+  // }
 
   return errors;
 }
