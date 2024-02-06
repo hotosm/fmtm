35e94f3084b7481086e6f8cf91a05d87cce6bb8f
NSUWAL123 <suwalnishit@gmail.com>
2024-02-06 09:40:06 +0545

    fix createEditOrganizationHeader: component renamed

--- src/frontend/src/components/CreateEditOrganization/CreateEditOrganizationHeader.tsx
+++ src/frontend/src/components/CreateEditOrganization/CreateEditOrganizationHeader.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import AssetModules from '@/shared/AssetModules.js';
 import { useNavigate } from 'react-router-dom';
 
-const CreateProjectHeader = ({ projectId }) => {
+const CreateEditOrganizationHeader = ({ projectId }) => {
   const navigate = useNavigate();
   return (
-    <div className="fmtm-px-5 fmtm-border-b-white fmtm-border-[1px] fmtm-bg-gray-100">
+    <div className="fmtm-px-5 fmtm-border-b-white fmtm-border-[1px] fmtm-bg-[#F5F5F5]">
       <div className="fmtm-flex fmtm-justify-between fmtm-items-center">
         <div className="fmtm-pt-3 fmtm-pb-1">
           <h1 className="sm:fmtm-text-xl md:fmtm-text-2xl">
@@ -23,4 +23,4 @@ const CreateProjectHeader = ({ projectId }) => {
   );
 };
 
-export default CreateProjectHeader;
+export default CreateEditOrganizationHeader;
