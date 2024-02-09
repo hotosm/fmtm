63fd35e49ce194dfd916c6e0bd21d6f785216133
NSUWAL123 <suwalnishit@gmail.com>
2024-02-09 13:36:27 +0545

    fix button: UI change, id prop inclusion

--- src/frontend/src/components/common/Button.tsx
+++ src/frontend/src/components/common/Button.tsx
@@ -2,6 +2,7 @@ import { Loader2 } from 'lucide-react';
 import React from 'react';
 
 interface IButton {
+  id?: string;
   btnText: string;
   btnType: 'primary' | 'secondary' | 'other' | 'disabled';
   type?: 'submit' | 'button';
@@ -18,20 +19,21 @@ interface IButton {
 const btnStyle = (btnType, className) => {
   switch (btnType) {
     case 'primary':
-      return `${className} hover:fmtm-bg-red-700 fmtm-flex fmtm-px-4 fmtm-py-1 fmtm-bg-primaryRed fmtm-text-white fmtm-rounded-[8px] fmtm-w-full`;
+      return `${className} hover:fmtm-bg-red-700 fmtm-flex fmtm-px-4 fmtm-py-1 fmtm-bg-primaryRed fmtm-text-white fmtm-rounded-[8px] fmtm-w-fit`;
     case 'secondary':
-      return `hover:fmtm-bg-gray-100 fmtm-flex fmtm-bg-white  fmtm-px-4 fmtm-py-1 fmtm-border border-[#E0E0E0] fmtm-rounded-[8px] ${className}`;
+      return `hover:fmtm-bg-gray-100 fmtm-flex fmtm-bg-white  fmtm-px-4 fmtm-py-1 fmtm-border border-[#E0E0E0] fmtm-rounded-[8px] fmtm-w-fit ${className}`;
 
     case 'other':
-      return `fmtm-py-1 fmtm-px-4 fmtm-text-red-600 fmtm-rounded-lg fmtm-border-[1px] fmtm-border-red-600 hover:fmtm-text-red-700 hover:fmtm-border-red-700 ${className}`;
+      return `fmtm-py-1 fmtm-px-4 fmtm-text-red-600 fmtm-rounded-lg fmtm-border-[1px] fmtm-border-red-600 hover:fmtm-text-red-700 hover:fmtm-border-red-700 fmtm-w-fit ${className}`;
     case 'disabled':
-      return `fmtm-py-1 fmtm-px-4 fmtm-text-white fmtm-rounded-lg fmtm-bg-gray-400 fmtm-cursor-not-allowed ${className}`;
+      return `fmtm-py-1 fmtm-px-4 fmtm-text-white fmtm-rounded-lg fmtm-bg-gray-400 fmtm-cursor-not-allowed fmtm-w-fit ${className}`;
 
     default:
       return 'fmtm-primary';
   }
 };
 const Button = ({
+  id,
   btnText,
   btnType,
   type,
@@ -44,35 +46,34 @@ const Button = ({
   isLoading,
   loadingText,
 }: IButton) => (
-  <div className="fmtm-w-fit">
-    <button
-      type={type === 'submit' ? 'submit' : 'button'}
-      onClick={onClick}
-      className={`fmtm-text-lg fmtm-group fmtm-flex fmtm-items-center fmtm-gap-2 ${btnStyle(
-        isLoading || disabled ? 'disabled' : btnType,
-        className,
-      )}`}
-      disabled={disabled || isLoading}
-      title={dataTip}
-    >
-      {isLoading ? (
-        <>
-          {loadingText ? loadingText : type === 'submit' ? 'Submitting...' : 'Loading...'}
-          <Loader2 className="fmtm-mr-2 fmtm-h-6 fmtm-w-6 fmtm-animate-spin" />
-        </>
-      ) : (
-        <>
-          <p className="fmtm-whitespace-nowrap">{btnText}</p>
-          {count && (
-            <p className="fmtm-flex fmtm-justify-center fmtm-items-center fmtm-text-overline fmtm-ml-2 fmtm-rounded-[40px] fmtm-bg-active_text fmtm-text-white fmtm-w-6 fmtm-h-6">
-              {count}
-            </p>
-          )}
-          {icon && <div>{icon}</div>}
-        </>
-      )}
-    </button>
-  </div>
+  <button
+    id={id}
+    type={type === 'submit' ? 'submit' : 'button'}
+    onClick={onClick}
+    className={`fmtm-text-lg fmtm-group fmtm-flex fmtm-items-center fmtm-gap-2 ${btnStyle(
+      isLoading || disabled ? 'disabled' : btnType,
+      className,
+    )}`}
+    disabled={disabled || isLoading}
+    title={dataTip}
+  >
+    {isLoading ? (
+      <>
+        {loadingText ? loadingText : type === 'submit' ? 'Submitting...' : 'Loading...'}
+        <Loader2 className="fmtm-mr-2 fmtm-h-6 fmtm-w-6 fmtm-animate-spin" />
+      </>
+    ) : (
+      <>
+        <p className="fmtm-whitespace-nowrap">{btnText}</p>
+        {count && (
+          <p className="fmtm-flex fmtm-justify-center fmtm-items-center fmtm-text-overline fmtm-ml-2 fmtm-rounded-[40px] fmtm-bg-active_text fmtm-text-white fmtm-w-6 fmtm-h-6">
+            {count}
+          </p>
+        )}
+        {icon && <div>{icon}</div>}
+      </>
+    )}
+  </button>
 );
 
 export default Button;
