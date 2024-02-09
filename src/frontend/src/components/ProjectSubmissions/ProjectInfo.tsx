fe83450d3afc249e24b529522ba13163c5835f4f
NSUWAL123 <suwalnishit@gmail.com>
2024-02-09 17:00:31 +0545

    fix projectSubmissions: projectDataCard UI enhancement

--- src/frontend/src/components/ProjectSubmissions/ProjectInfo.tsx
+++ src/frontend/src/components/ProjectSubmissions/ProjectInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CoreModules from '@/shared/CoreModules';
+import AssetModules from '@/shared/AssetModules';
 
 const ProjectInfo = () => {
   const projectInfo = CoreModules.useAppSelector((state) => state.project.projectInfo);
@@ -7,31 +8,43 @@ const ProjectInfo = () => {
   const projectDashboardLoading = CoreModules.useAppSelector((state) => state.project.projectDashboardLoading);
 
   const dataCard = [
-    { title: 'Tasks', count: projectDashboardDetail?.total_tasks },
-    { title: 'Contributors', count: projectDashboardDetail?.total_contributors },
-    { title: 'Submissions', count: projectDashboardDetail?.total_submission },
+    {
+      title: 'Tasks',
+      count: projectDashboardDetail?.total_tasks,
+      icon: <AssetModules.TaskIcon sx={{ color: 'white', fontSize: { xs: '35px', xl: '40px' } }} />,
+    },
+    {
+      title: 'Contributors',
+      count: projectDashboardDetail?.total_contributors,
+      icon: <AssetModules.PeopleAltIcon sx={{ color: 'white', fontSize: { xs: '35px', xl: '40px' } }} />,
+    },
+    {
+      title: 'Submissions',
+      count: projectDashboardDetail?.total_submission,
+      icon: <AssetModules.SubmissionIcon sx={{ color: 'white', fontSize: { xs: '35px', xl: '40px' } }} />,
+    },
   ];
 
   const ProjectDataCard = ({ data }) => (
-    <div className="fmtm-border-[1px] fmtm-border-primaryRed fmtm-bg-white fmtm-rounded-2xl fmtm-min-w-[7.5rem] fmtm-w-[7.5rem] sm:fmtm-w-[8.5rem] 2xl:fmtm-w-[10rem] fmtm-flex fmtm-flex-col fmtm-items-center fmtm-p-2 md:fmtm-p-4 fmtm-gap-2 fmtm-shadow-md fmtm-shadow-red-300">
-      {projectDashboardLoading ? (
-        <CoreModules.Skeleton className="!fmtm-w-[100px] fmtm-h-[30px]" />
-      ) : (
-        <h2 className="fmtm-font-archivo fmtm-text-xl sm:fmtm-text-2xl md:fmtm-text-[1.7rem] 2xl:fmtm-text-[2rem] fmtm-font-bold fmtm-text-primaryRed">
+    <div className="fmtm-bg-white fmtm-rounded-2xl fmtm-min-w-[12.5rem] fmtm-p-2 fmtm-px-4 md:fmtm-p-4 md:fmtm-shadow-[0px_10px_20px_0px_rgba(96,96,96,0.1)] fmtm-flex fmtm-items-center fmtm-gap-4">
+      <div className="fmtm-from-[#f79292] fmtm-to-primaryRed fmtm-rounded-full fmtm-p-2 fmtm-bg-gradient-to-br">
+        {data.icon}
+      </div>
+      <div className="fmtm-flex fmtm-flex-col fmtm-items-start">
+        <h2 className="fmtm-text-lg sm:fmtm-text-xl md:fmtm-text-xl fmtm-text-primaryRed fmtm-font-bold">
+          {data.title}
+        </h2>
+        <h2 className="fmtm-text-xl sm:fmtm-text-2xl md:fmtm-text-2xl fmtm-font-bold fmtm-text-primaryRed">
           {data.count}
         </h2>
-      )}
-      {projectDashboardLoading ? (
-        <CoreModules.Skeleton className="!fmtm-w-[100px] fmtm-h-[20px]" />
-      ) : (
-        <h4 className="fmtm-font-archivo fmtm-text-lg sm:fmtm-text-xl md::fmtm-text-[1.2rem] 2xl:fmtm-text-[1.5rem] fmtm-text-[#7A7676]">
-          {data.title}
-        </h4>
-      )}
+      </div>
     </div>
   );
+
+  const widthStyle = window.innerWidth >= 640 ? { width: 'calc(100% - 80px)' } : { width: '100%' };
+
   return (
-    <div className="fmtm-w-full sm:fmtm-ml-2 fmtm-border-b-[1px] fmtm-border-gray-300 fmtm-pb-10">
+    <div style={widthStyle} className="sm:fmtm-ml-2 fmtm-border-b-[1px] fmtm-border-gray-300 fmtm-pb-10">
       <div className="fmtm-pb-4">
         <p className="fmtm-text-[#706E6E] fmtm-font-archivo fmtm-text-sm">
           <span>Projects </span>
@@ -39,9 +52,9 @@ const ProjectInfo = () => {
           <span>Dashboard</span>
         </p>
       </div>
-      <div className=" fmtm-flex fmtm-flex-col lg:fmtm-flex-row fmtm-w-full sm:fmtm-items-center fmtm-gap-10 fmtm-mt-3">
+      <div className=" fmtm-flex fmtm-flex-col xl:fmtm-flex-row fmtm-w-full sm:fmtm-items-center fmtm-gap-10 fmtm-mt-3">
         <div className="fmtm-w-full fmtm-min-w-0 sm:fmtm-max-w-[37rem] fmtm-bg-primary-100 fmtm-rounded-2xl fmtm-p-5 fmtm-flex fmtm-flex-col fmtm-gap-5">
-          {false ? (
+          {projectDashboardLoading ? (
             <div>
               <CoreModules.Skeleton className="!fmtm-w-full" />
               <CoreModules.Skeleton className="!fmtm-w-[200px]" />
@@ -74,12 +87,20 @@ const ProjectInfo = () => {
             </p>
           </div>
         </div>
-        <div className="fmtm-w-full fmtm-overflow-x-scroll scrollbar fmtm-pb-1 sm:fmtm-pb-0 sm:fmtm-overflow-x-visible">
-          <div className="fmtm-w-full fmtm-flex sm:fmtm-justify-center fmtm-gap-5">
-            {dataCard.map((data, i) => (
-              <ProjectDataCard key={i} data={data} />
-            ))}
-          </div>
+        <div className="fmtm-w-full fmtm-overflow-x-scroll scrollbar fmtm-pb-1 md:fmtm-pb-0 md:fmtm-overflow-x-visible">
+          {projectDashboardLoading ? (
+            <div className="fmtm-w-full fmtm-flex sm:fmtm-justify-center fmtm-gap-5">
+              {Array.from({ length: 3 }).map((i) => (
+                <CoreModules.Skeleton key={i} className="!fmtm-w-[8.5rem] fmtm-h-[6.25rem]" />
+              ))}
+            </div>
+          ) : (
+            <div className="fmtm-w-full fmtm-flex md:fmtm-justify-center fmtm-gap-5">
+              {dataCard.map((data, i) => (
+                <ProjectDataCard key={i} data={data} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
