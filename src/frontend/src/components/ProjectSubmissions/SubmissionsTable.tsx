85dcb4788a08e80ee7c85db7c7a921a24fbefb0b
NSUWAL123 <suwalnishit@gmail.com>
2024-02-12 16:42:01 +0545

    fix submissionTable: submitted by textField add

--- src/frontend/src/components/ProjectSubmissions/SubmissionsTable.tsx
+++ src/frontend/src/components/ProjectSubmissions/SubmissionsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import AssetModules from '@/shared/AssetModules.js';
 import { CustomSelect } from '@/components/common/Select.js';
 import windowDimention from '@/hooks/WindowDimension';
@@ -10,10 +10,11 @@ import { SubmissionsTableSkeletonLoader } from '@/components/ProjectSubmissions/
 import { Loader2 } from 'lucide-react';
 import { SubmissionActions } from '@/store/slices/SubmissionSlice';
 import { reviewStateData } from '@/constants/projectSubmissionsConstants';
+import InputTextField from '@/components/common/InputTextField';
 
 type filterType = {
   task_id: number | null;
-  submitted_by: string | null;
+  submitted_by: string;
   review_state: string | null;
   submitted_date: Date | null;
 };
@@ -21,7 +22,7 @@ type filterType = {
 const SubmissionsTable = () => {
   const initialFilterState = {
     task_id: null,
-    submitted_by: null,
+    submitted_by: '',
     review_state: null,
     submitted_date: null,
   };
@@ -42,6 +43,7 @@ const SubmissionsTable = () => {
   const taskInfo = CoreModules.useAppSelector((state) => state.task.taskInfo);
   const [numberOfFilters, setNumberOfFilters] = useState<number>(0);
   const [paginationPage, setPaginationPage] = useState<number>(1);
+  const [submittedBy, setSubmittedBy] = useState<string>('');
 
   useEffect(() => {
     let count = 0;
@@ -126,6 +128,13 @@ const SubmissionsTable = () => {
     }
   };
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setFilter((prev) => ({ ...prev, submitted_by: submittedBy }));
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [submittedBy, 500]);
+
   const handleChangePage = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | React.KeyboardEvent<HTMLInputElement>,
     newPage: number,
@@ -140,142 +149,6 @@ const SubmissionsTable = () => {
   const clearFilters = () => {
     setFilter(initialFilterState);
   };
-
-  const TableFilter = () => (
-    <div className="fmtm-flex fmtm-items-center fmtm-justify-between fmtm-flex-col sm:fmtm-flex-row fmtm-gap-4">
-      <div
-        className={`fmtm-bg-white ${
-          windowSize.width < 2000 ? 'fmtm-w-full md:fmtm-w-fit' : 'fmtm-w-fit'
-        } fmtm-flex xl:fmtm-items-end fmtm-gap-2 xl:fmtm-gap-8 fmtm-px-4 fmtm-rounded-lg fmtm-relative fmtm-pr-6 fmtm-flex-col xl:fmtm-flex-row fmtm-py-2 fmtm-pt-3 xl:fmtm-py-2  fmtm-my-2`}
-      >
-        <div className="fmtm-flex fmtm-justify-between fmtm-items-center fmtm-gap-8">
-          <div className="fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-mb-1 fmtm-relative fmtm-w-fit">
-            <div>
-              <AssetModules.FilterAltOutlinedIcon className="fmtm-text-grey-700" />
-            </div>
-            <p className="fmtm-text-sm fmtm-mt-1 fmtm-text-grey-700 fmtm-font-bold">FILTER</p>
-            <div className="fmtm-absolute -fmtm-right-3 -fmtm-top-2 fmtm-w-4 fmtm-h-4 fmtm-rounded-full  fmtm-bg-primaryRed fmtm-flex fmtm-justify-center fmtm-items-center">
-              <p className=" fmtm-text-xs fmtm-text-white">{numberOfFilters}</p>
-            </div>
-          </div>
-          <button
-            className={`fmtm-w-fit fmtm-text-sm fmtm-text-grey-700 fmtm-font-bold fmtm-duration-150 fmtm-truncate fmtm-block xl:fmtm-hidden ${
-              !filter.task_id && !filter.review_state && !filter.submitted_by && !filter.submitted_date
-                ? 'fmtm-hidden'
-                : 'fmtm-block'
-            } ${
-              submissionTableDataLoading || submissionFormFieldsLoading
-                ? 'fmtm-cursor-not-allowed'
-                : 'hover:fmtm-text-red-700'
-            }`}
-            onClick={clearFilters}
-            disabled={submissionTableDataLoading || submissionFormFieldsLoading}
-          >
-            CLEAR ALL
-          </button>
-        </div>
-        {showFilter && (
-          <div
-            className={`fmtm-grid ${
-              windowSize.width < 500 ? 'fmtm-grid-cols-1' : 'fmtm-grid-cols-2 lg:fmtm-grid-cols-4'
-            } fmtm-gap-4`}
-          >
-            <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
-              <CustomSelect
-                title="Task Id"
-                placeholder="Select"
-                data={taskInfo}
-                dataKey="value"
-                value={filter?.task_id?.toString()}
-                valueKey="task_id"
-                label="task_id"
-                onValueChange={(value) => value && setFilter((prev) => ({ ...prev, task_id: +value }))}
-                className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
-              />
-            </div>
-            <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
-              <CustomSelect
-                title="Submitted By"
-                placeholder="Select"
-                data={[]}
-                dataKey="value"
-                value={''}
-                valueKey="value"
-                label="label"
-                onValueChange={() => {}}
-                errorMsg=""
-                className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
-              />
-            </div>
-            <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
-              <CustomSelect
-                title="Review State"
-                placeholder="Select"
-                data={reviewStateData}
-                dataKey="value"
-                value={filter?.review_state}
-                valueKey="value"
-                label="label"
-                onValueChange={(value) => value && setFilter((prev) => ({ ...prev, review_state: value.toString() }))}
-                errorMsg=""
-                className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
-              />
-            </div>
-            <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
-              <CustomSelect
-                title="Submitted Date"
-                placeholder="Select"
-                data={[]}
-                dataKey="value"
-                value={''}
-                valueKey="value"
-                label="label"
-                onValueChange={() => {}}
-                errorMsg=""
-                className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
-              />
-            </div>
-          </div>
-        )}
-        <button
-          className={`fmtm-w-fit fmtm-text-sm fmtm-text-grey-700 fmtm-font-bold fmtm-mb-1  hover:fmtm-text-red-700 fmtm-duration-150 fmtm-truncate fmtm-hidden xl:fmtm-block`}
-        >
-          CLEAR ALL
-        </button>
-        <div
-          className={`fmtm-group fmtm-absolute -fmtm-right-3 ${
-            showFilter
-              ? 'fmtm-top-[45%] sm:fmtm-top-20 lg:fmtm-top-9 xl:fmtm-top-5'
-              : 'fmtm-top-2 xl:fmtm-top-1 fmtm-rotate-180'
-          } fmtm-rounded-full  fmtm-w-7 fmtm-h-7 fmtm-bg-white fmtm-border-[1px] fmtm-border-gray-300 fmtm-flex fmtm-justify-center fmtm-items-center fmtm-pl-2 hover:fmtm-border-primaryRed fmtm-duration-150`}
-          onClick={() => setShowFilter(!showFilter)}
-        >
-          <AssetModules.ArrowBackIosIcon
-            className="fmtm-text-grey-700 group-hover:fmtm-text-primaryRed fmtm-duration-150"
-            style={{ fontSize: '18px' }}
-          />
-        </div>
-      </div>
-      <div className="fmtm-w-full fmtm-flex fmtm-justify-end sm:fmtm-w-fit">
-        <button
-          className={`fmtm-px-4 fmtm-py-1 fmtm-flex fmtm-items-center fmtm-w-fit fmtm-rounded-lg fmtm-gap-2 fmtm-duration-150 ${
-            submissionTableDataLoading || submissionFormFieldsLoading
-              ? 'fmtm-bg-gray-400 fmtm-cursor-not-allowed'
-              : 'fmtm-bg-primaryRed hover:fmtm-bg-red-700'
-          }`}
-          onClick={refreshTable}
-          disabled={submissionTableDataLoading || submissionFormFieldsLoading}
-        >
-          {(submissionTableDataLoading || submissionFormFieldsLoading) && submissionTableRefreshing ? (
-            <Loader2 className="fmtm-h-4 fmtm-w-4 fmtm-animate-spin fmtm-text-white" />
-          ) : (
-            <AssetModules.ReplayIcon className="fmtm-text-white" style={{ fontSize: '18px' }} />
-          )}
-          <p className="fmtm-text-white fmtm-pt-1">Refresh</p>
-        </button>
-      </div>
-    </div>
-  );
 
   function getValueByPath(obj: any, path: string) {
     let value = obj;
@@ -293,7 +166,136 @@ const SubmissionsTable = () => {
 
   return (
     <div className="fmtm-font-archivo">
-      <TableFilter />
+      <div className="fmtm-flex fmtm-items-center fmtm-justify-between fmtm-flex-col sm:fmtm-flex-row fmtm-gap-4">
+        <div
+          className={`fmtm-bg-white ${
+            windowSize.width < 2000 ? 'fmtm-w-full md:fmtm-w-fit' : 'fmtm-w-fit'
+          } fmtm-flex xl:fmtm-items-end fmtm-gap-2 xl:fmtm-gap-8 fmtm-px-4 fmtm-rounded-lg fmtm-relative fmtm-pr-6 fmtm-flex-col xl:fmtm-flex-row fmtm-py-2 fmtm-pt-3 xl:fmtm-py-2  fmtm-my-2`}
+        >
+          <div className="fmtm-flex fmtm-justify-between fmtm-items-center fmtm-gap-8">
+            <div className="fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-mb-1 fmtm-relative fmtm-w-fit">
+              <div>
+                <AssetModules.FilterAltOutlinedIcon className="fmtm-text-grey-700" />
+              </div>
+              <p className="fmtm-text-sm fmtm-mt-1 fmtm-text-grey-700 fmtm-font-bold">FILTER</p>
+              <div className="fmtm-absolute -fmtm-right-3 -fmtm-top-2 fmtm-w-4 fmtm-h-4 fmtm-rounded-full  fmtm-bg-primaryRed fmtm-flex fmtm-justify-center fmtm-items-center">
+                <p className=" fmtm-text-xs fmtm-text-white">{numberOfFilters}</p>
+              </div>
+            </div>
+            <button
+              className={`fmtm-w-fit fmtm-text-sm fmtm-text-grey-700 fmtm-font-bold fmtm-duration-150 fmtm-truncate fmtm-block xl:fmtm-hidden ${
+                !filter.task_id && !filter.review_state && !filter.submitted_by && !filter.submitted_date
+                  ? 'fmtm-hidden'
+                  : 'fmtm-block'
+              } ${
+                submissionTableDataLoading || submissionFormFieldsLoading
+                  ? 'fmtm-cursor-not-allowed'
+                  : 'hover:fmtm-text-red-700'
+              }`}
+              onClick={clearFilters}
+              disabled={submissionTableDataLoading || submissionFormFieldsLoading}
+            >
+              CLEAR ALL
+            </button>
+          </div>
+          {showFilter && (
+            <div
+              className={`fmtm-grid ${
+                windowSize.width < 500 ? 'fmtm-grid-cols-1' : 'fmtm-grid-cols-2 lg:fmtm-grid-cols-4'
+              } fmtm-gap-4`}
+            >
+              <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
+                <CustomSelect
+                  title="Task Id"
+                  placeholder="Select"
+                  data={taskInfo}
+                  dataKey="value"
+                  value={filter?.task_id?.toString()}
+                  valueKey="task_id"
+                  label="task_id"
+                  onValueChange={(value) => value && setFilter((prev) => ({ ...prev, task_id: +value }))}
+                  className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
+                />
+              </div>
+              <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
+                <CustomSelect
+                  title="Review State"
+                  placeholder="Select"
+                  data={reviewStateData}
+                  dataKey="value"
+                  value={filter?.review_state}
+                  valueKey="value"
+                  label="label"
+                  onValueChange={(value) => value && setFilter((prev) => ({ ...prev, review_state: value.toString() }))}
+                  errorMsg=""
+                  className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
+                />
+              </div>
+              <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
+                <CustomSelect
+                  title="Submitted Date"
+                  placeholder="Select"
+                  data={[]}
+                  dataKey="value"
+                  value={''}
+                  valueKey="value"
+                  label="label"
+                  onValueChange={() => {}}
+                  errorMsg=""
+                  className="fmtm-text-grey-700 fmtm-text-sm !fmtm-mb-0"
+                />
+              </div>
+              <div className={`${windowSize.width < 500 ? 'fmtm-w-full' : 'fmtm-w-[11rem]'}`}>
+                <p className={`fmtm-text-grey-700 fmtm-text-sm fmtm-font-semibold !fmtm-bg-transparent`}>
+                  Submitted By
+                </p>
+                <InputTextField
+                  fieldType="text"
+                  placeholder=""
+                  value={submittedBy}
+                  onChange={(e) => setSubmittedBy(e.target.value)}
+                />
+              </div>
+            </div>
+          )}
+          <button
+            className={`fmtm-w-fit fmtm-text-sm fmtm-text-grey-700 fmtm-font-bold fmtm-mb-1  hover:fmtm-text-red-700 fmtm-duration-150 fmtm-truncate fmtm-hidden xl:fmtm-block`}
+          >
+            CLEAR ALL
+          </button>
+          <div
+            className={`fmtm-group fmtm-absolute -fmtm-right-3 ${
+              showFilter
+                ? 'fmtm-top-[45%] sm:fmtm-top-20 lg:fmtm-top-9 xl:fmtm-top-5'
+                : 'fmtm-top-2 xl:fmtm-top-1 fmtm-rotate-180'
+            } fmtm-rounded-full  fmtm-w-7 fmtm-h-7 fmtm-bg-white fmtm-border-[1px] fmtm-border-gray-300 fmtm-flex fmtm-justify-center fmtm-items-center fmtm-pl-2 hover:fmtm-border-primaryRed fmtm-duration-150`}
+            onClick={() => setShowFilter(!showFilter)}
+          >
+            <AssetModules.ArrowBackIosIcon
+              className="fmtm-text-grey-700 group-hover:fmtm-text-primaryRed fmtm-duration-150"
+              style={{ fontSize: '18px' }}
+            />
+          </div>
+        </div>
+        <div className="fmtm-w-full fmtm-flex fmtm-justify-end sm:fmtm-w-fit">
+          <button
+            className={`fmtm-px-4 fmtm-py-1 fmtm-flex fmtm-items-center fmtm-w-fit fmtm-rounded-lg fmtm-gap-2 fmtm-duration-150 ${
+              submissionTableDataLoading || submissionFormFieldsLoading
+                ? 'fmtm-bg-gray-400 fmtm-cursor-not-allowed'
+                : 'fmtm-bg-primaryRed hover:fmtm-bg-red-700'
+            }`}
+            onClick={refreshTable}
+            disabled={submissionTableDataLoading || submissionFormFieldsLoading}
+          >
+            {(submissionTableDataLoading || submissionFormFieldsLoading) && submissionTableRefreshing ? (
+              <Loader2 className="fmtm-h-4 fmtm-w-4 fmtm-animate-spin fmtm-text-white" />
+            ) : (
+              <AssetModules.ReplayIcon className="fmtm-text-white" style={{ fontSize: '18px' }} />
+            )}
+            <p className="fmtm-text-white fmtm-pt-1">Refresh</p>
+          </button>
+        </div>
+      </div>
       {submissionTableDataLoading || submissionFormFieldsLoading ? (
         <SubmissionsTableSkeletonLoader />
       ) : (
