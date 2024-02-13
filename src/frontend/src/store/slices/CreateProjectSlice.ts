72e64c76f3c39ae58f9e40185bc8204a8582f278
NSUWAL123 <suwalnishit@gmail.com>
2024-02-13 18:02:48 +0545

    fix projectDetailsForm: default FMTM tag removal

--- src/frontend/src/store/slices/CreateProjectSlice.ts
+++ src/frontend/src/store/slices/CreateProjectSlice.ts
@@ -7,7 +7,7 @@ export const initialState: CreateProjectStateTypes = {
   projectDetails: {
     dimension: 10,
     no_of_buildings: 5,
-    hashtags: '#FMTM',
+    hashtags: '',
     name: '',
     short_description: '',
     odk_central_url: '',
