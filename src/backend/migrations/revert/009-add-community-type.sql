27ead826581a7adaaa4158da377ab1388e5b08ab
spwoodcock <sam.woodcock@protonmail.com>
2024-02-13 15:26:58 +0000

    build: update community_type migration number & logic

--- src/backend/migrations/revert/009-add-community-type.sql
+++ src/backend/migrations/revert/009-add-community-type.sql
@@ -4,10 +4,6 @@ BEGIN;
 ALTER TABLE public.organisations
     DROP COLUMN IF EXISTS community_type;
 
--- Remove the email column from organisations table
-ALTER TABLE public.organisations
-    DROP COLUMN IF EXISTS email;
-
 -- Drop the communitytype enum
 DROP TYPE IF EXISTS public.communitytype;
 
