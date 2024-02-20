4bb942eaea848557a87e7c32ab7efe74517e4d03
spwoodcock <sam.woodcock@protonmail.com>
2024-02-20 12:05:16 +0000

    build: update migrations to match org db model

---
+++ src/backend/migrations/008-add-user-in-org.sql
@@ -0,0 +1,13 @@
+-- ## Migration to:
+-- Add the created_by column to the organisations table
+
+-- Start a transaction
+
+BEGIN;
+
+ALTER TABLE organisations
+ADD COLUMN IF NOT EXISTS created_by INTEGER;
+
+-- Commit the transaction
+COMMIT;
+
