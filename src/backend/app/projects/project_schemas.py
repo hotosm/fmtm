063efc1c13cea4138ea71c3be5ce5e51f2604a4d
spwoodcock <sam.woodcock@protonmail.com>
2024-02-20 23:03:20 +0000

    refactor: add data_extract_url to ProjectOut schema

--- src/backend/app/projects/project_schemas.py
+++ src/backend/app/projects/project_schemas.py
@@ -322,6 +322,7 @@ class ReadProject(ProjectBase):
 
     project_uuid: uuid.UUID = uuid.uuid4()
     location_str: Optional[str] = None
+    data_extract_url: str
 
 
 class BackgroundTaskStatus(BaseModel):
