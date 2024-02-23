f9449c7887dd5a7ecf467d0a731ab7cb7fab963b
spwoodcock <sam.woodcock@protonmail.com>
2024-02-23 15:59:05 +0000

    build: make initial_feature_count migration idempotent

--- src/backend/migrations/011-task-features-count.sql
+++ src/backend/migrations/011-task-features-count.sql
@@ -4,8 +4,12 @@
 -- Start a transaction
 BEGIN;
 
-ALTER TABLE IF EXISTS public.tasks
-    RENAME COLUMN initial_feature_count TO feature_count;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'tasks' AND column_name = 'initial_feature_count') THEN
+        ALTER TABLE public.tasks RENAME COLUMN initial_feature_count TO feature_count;
+    END IF;
+END $$;
 
 -- Commit the transaction
 COMMIT;
