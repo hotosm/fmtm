71c19a401919367aaf7facd7873df17a71a34246
NSUWAL123 <suwalnishit@gmail.com>
2024-02-27 12:36:35 +0545

    feat commonSlice: ts types add

---
+++ src/frontend/src/store/types/ICommon.ts
@@ -0,0 +1,17 @@
+export type CommonType = {
+  snackbar: snackbarTypes;
+  loading: boolean;
+  postOrganisationLoading: boolean;
+  currentStepFormStep: {
+    create_project: {
+      step: number;
+    };
+  };
+};
+
+type snackbarTypes = {
+  open: boolean;
+  message: string;
+  variant: 'info' | 'success' | 'error' | 'warning';
+  duration: number;
+};
