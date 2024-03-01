388ae369a4210fc7e56416723cb22b90e05e2dd7
NSUWAL123 <suwalnishit@gmail.com>
2024-03-01 16:29:23 +0545

    feat richTextEditor: tiptap richTextEditor add

---
+++ src/frontend/src/components/common/Editor/Editor.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useEditor, EditorContent, FloatingMenu, BubbleMenu } from '@tiptap/react';
+import { Toolbar } from '@/components/common/Editor/Toolbar';
+import StarterKit from '@tiptap/starter-kit';
+import ListItem from '@tiptap/extension-list-item';
+import BulletList from '@tiptap/extension-bullet-list';
+import OrderedList from '@tiptap/extension-ordered-list';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+import Heading from '@tiptap/extension-heading';
+import Document from '@tiptap/extension-document';
+import Link from '@tiptap/extension-link';
+import './editorStyles.scss';
+
+// define your extension array
+const extensions = [
+  StarterKit,
+  Document,
+  Paragraph,
+  Text,
+  ListItem,
+  BulletList,
+  OrderedList,
+  Heading,
+  Link.configure({
+    validate: (href) => /^https?:\/\//.test(href),
+  }),
+];
+
+const content = '<p>Hello World!</p>';
+
+const Tiptap = () => {
+  const [editorHtmlContent, setEditorHtmlContent] = useState('');
+
+  const editor = useEditor({
+    extensions,
+    content,
+    onUpdate: ({ editor }) => {
+      setEditorHtmlContent(editor.getHTML());
+    },
+  });
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div className="no-tailwindcss fmtm-remove-all fmtm-border-[1px] fmtm-border-gray-300 fmtm-rounded-md">
+      <Toolbar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
+
+export default Tiptap;
---
+++ src/frontend/src/components/common/Editor/Toolbar.tsx
@@ -0,0 +1,192 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Editor } from '@tiptap/react';
+import AssetModules from '@/shared/AssetModules';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '../Dropdown';
+import { Tooltip } from '@mui/material';
+import InputTextField from '@/components/common/InputTextField';
+import Button from '@/components/common/Button';
+
+type ToolbarProps = {
+  editor: Editor | null;
+};
+
+const iconClassName = 'fmtm-duration-300 hover:fmtm-bg-red-100 fmtm-rounded fmtm-cursor-pointer fmtm-p-[2px]';
+
+export const Toolbar = ({ editor }: ToolbarProps) => {
+  const [linkText, setLinkText] = useState('');
+  const [linkDropdownOpen, setLinkDropdownOpen] = useState(false);
+
+  const isEditorActive = (editorItem) => {
+    if (editor?.isActive(editorItem)) {
+      return 'fmtm-text-primaryRed fmtm-bg-red-100';
+    }
+    return '';
+  };
+
+  const previousUrl = editor?.getAttributes('link').href;
+  useEffect(() => {
+    if (previousUrl) {
+      setLinkText(previousUrl);
+    }
+  }, [previousUrl]);
+
+  const setLink = useCallback(() => {
+    const url = linkText;
+    // cancelled
+    if (url === null) {
+      return;
+    }
+    // empty
+    if (url === '') {
+      editor?.chain().focus().extendMarkRange('link').unsetLink().run();
+      return;
+    }
+    // update link
+    editor?.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
+    setLinkText('');
+    setLinkDropdownOpen(false);
+  }, [editor, linkText]);
+
+  return (
+    <div className="fmtm-flex fmtm-justify-between fmtm-px-2 fmtm-border-b-[1px] fmtm-border-gray-300">
+      <div className="fmtm-flex fmtm-gap-2 fmtm-py-1 fmtm-items-center">
+        <Tooltip title="Bold">
+          <AssetModules.FormatBoldIcon
+            onClick={() => editor?.chain().focus().toggleBold().run()}
+            className={`${iconClassName} ${isEditorActive('bold')}`}
+          />
+        </Tooltip>
+        <Tooltip title="Italic">
+          <AssetModules.FormatItalicIcon
+            onClick={() => editor?.chain().focus().toggleItalic().run()}
+            className={`${iconClassName} ${isEditorActive('italic')}`}
+          />
+        </Tooltip>
+        <Tooltip title="Strike">
+          <AssetModules.StrikethroughSIcon
+            onClick={() => editor?.chain().focus().toggleStrike().run()}
+            className={`${iconClassName} ${isEditorActive('strike')}`}
+          />
+        </Tooltip>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="fmtm-outline-none">
+            <Tooltip title="Heading">
+              <p
+                className={`fmtm-font-bold fmtm-font-archivo fmtm-scale-110 ${iconClassName} !fmtm-py-0 fmtm-px-2 ${isEditorActive(
+                  'heading',
+                )}`}
+              >
+                h
+              </p>
+            </Tooltip>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="fmtm-flex fmtm-gap-1 fmtm-bg-white fmtm-px-1">
+            <Tooltip title="Heading 1">
+              <p
+                onClick={() => editor?.chain().focus().toggleHeading({ level: 1 }).run()}
+                className={`${iconClassName} fmtm-px-2 ${
+                  editor?.isActive('heading', { level: 1 }) ? 'fmtm-text-primaryRed fmtm-bg-red-100' : ''
+                }`}
+              >
+                H<sub>1</sub>
+              </p>
+            </Tooltip>
+
+            <Tooltip title="Heading 2">
+              <p
+                onClick={() => editor?.chain().focus().toggleHeading({ level: 2 }).run()}
+                className={`${iconClassName} fmtm-px-2 ${
+                  editor?.isActive('heading', { level: 2 }) ? 'fmtm-text-primaryRed fmtm-bg-red-100' : ''
+                }`}
+              >
+                H<sub>2</sub>
+              </p>
+            </Tooltip>
+
+            <Tooltip title="Heading 3">
+              <p
+                onClick={() => editor?.chain().focus().toggleHeading({ level: 3 }).run()}
+                className={`${iconClassName} fmtm-px-2 ${
+                  editor?.isActive('heading', { level: 3 }) ? 'fmtm-text-primaryRed fmtm-bg-red-100' : ''
+                }`}
+              >
+                H<sub>3</sub>
+              </p>
+            </Tooltip>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Tooltip title="Unordered List">
+          <AssetModules.ListViewIcon
+            onClick={() => editor?.chain().focus().toggleBulletList().run()}
+            className={`${iconClassName} ${isEditorActive('bulletList')}`}
+          />
+        </Tooltip>
+        <Tooltip title="Numbered List">
+          <AssetModules.FormatListNumberedIcon
+            onClick={() => editor?.chain().focus().toggleOrderedList().run()}
+            className={`${iconClassName} ${isEditorActive('orderedList')}`}
+          />
+        </Tooltip>
+        <Tooltip title="Code">
+          <AssetModules.CodeIcon
+            onClick={() => editor?.chain().focus().toggleCodeBlock().run()}
+            className={`${iconClassName} ${isEditorActive('codeBlock')}`}
+          />
+        </Tooltip>
+        <DropdownMenu
+          open={linkDropdownOpen}
+          onOpenChange={(state) => {
+            setLinkDropdownOpen(state);
+            if (!state) {
+              setLinkText('');
+            }
+          }}
+        >
+          <DropdownMenuTrigger className="fmtm-outline-none">
+            <Tooltip title="Link">
+              <AssetModules.LinkIcon className={`${iconClassName} ${isEditorActive('link')}`} />
+            </Tooltip>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="fmtm-flex fmtm-items-center fmtm-gap-2">
+            <InputTextField
+              onChange={(e) => {
+                setLinkText(e.target.value);
+              }}
+              value={linkText}
+              fieldType="text"
+            />
+            <Button
+              btnText="Add Link"
+              btnType="other"
+              className="fmtm-text-sm !fmtm-px-2 !fmtm-py-1 fmtm-rounded-[6px] !fmtm-bg-primaryRed !fmtm-text-white"
+              onClick={setLink}
+            />
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Tooltip title="Quote">
+          <AssetModules.FormatQuoteIcon
+            onClick={() => editor?.chain().focus().toggleBlockquote().run()}
+            className={`${iconClassName} ${isEditorActive('blockquote')}`}
+          />
+        </Tooltip>
+        <Tooltip title="Horizontal Ruler">
+          <AssetModules.HorizontalRuleIcon
+            onClick={() => editor?.chain().focus().setHorizontalRule().run()}
+            className={`${iconClassName}`}
+          />
+        </Tooltip>
+        <Tooltip title="Image">
+          <AssetModules.ImageAddIcon className={`${iconClassName}`} />
+        </Tooltip>
+      </div>
+      <div className="fmtm-flex fmtm-gap-2 fmtm-py-1">
+        <Tooltip title="Undo">
+          <AssetModules.UndoIcon onClick={() => editor?.chain().focus().undo().run()} className={`${iconClassName}`} />
+        </Tooltip>
+        <Tooltip title="Redo">
+          <AssetModules.RedoIcon onClick={() => editor?.chain().focus().redo().run()} className={`${iconClassName}`} />
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
