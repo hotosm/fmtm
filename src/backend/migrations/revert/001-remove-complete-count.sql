0329bb213fa87fdd0818948c39a86191bf390a72
spwoodcock <sam.woodcock@protonmail.com>
2024-03-02 21:30:59 +0000

    build: add xform_category, remove extract_completed_count

---
+++ src/backend/migrations/revert/001-remove-complete-count.sql
@@ -0,0 +1,32 @@
+-- Start a transaction
+BEGIN;
+
+-- Add the 'extract_complete_count' column if it doesn't exist
+ALTER TABLE IF EXISTS public.projects
+ADD COLUMN IF NOT EXISTS extract_complete_count integer;
+
+-- Remove the 'xform_category' column if it exists
+ALTER TABLE IF EXISTS public.projects
+DROP COLUMN IF EXISTS xform_category;
+
+-- Check if the 'xform_title' foreign key constraint exists
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM information_schema.constraint_column_usage 
+        WHERE constraint_name = 'fk_xform'
+    ) THEN
+        RAISE NOTICE 'Foreign key constraint "fk_xform" already exists.';
+    ELSE
+        -- Add the 'xform_title' foreign key constraint
+        ALTER TABLE ONLY public.projects
+        ADD CONSTRAINT fk_xform
+        FOREIGN KEY (xform_title) REFERENCES public.xlsforms(title);
+        
+        RAISE NOTICE 'Foreign key constraint "fk_xform" successfully added.';
+    END IF;
+END $$;
+
+-- Commit the transaction
+COMMIT;
