e40f01900a4237ca97338828e0f5522d1b9c4155
NSUWAL123 <suwalnishit@gmail.com>
2024-03-22 12:20:00 +0545

    fix activitiesPanel: css fix & sort code remove

--- src/frontend/src/components/ProjectDetailsV2/ActivitiesPanel.tsx
+++ src/frontend/src/components/ProjectDetailsV2/ActivitiesPanel.tsx
@@ -8,13 +8,6 @@ import { ActivitiesCardSkeletonLoader, ShowingCountSkeletonLoader } from '@/comp
 import { taskHistoryListType } from '@/models/project/projectModel';
 import { useAppSelector } from '@/types/reduxTypes';
 
-const sortByList = [
-  { id: 'activities', name: 'Activities' },
-  { id: 'date', name: 'Date' },
-  { id: 'users', name: 'Users' },
-  { id: 'taskid', name: 'Task ID' },
-];
-
 const ActivitiesPanel = ({ defaultTheme, state, params, map, view, mapDivPostion, states }) => {
   const displayLimit = 10;
   const [searchText, setSearchText] = useState<string>('');
@@ -129,56 +122,16 @@ const ActivitiesPanel = ({ defaultTheme, state, params, map, view, mapDivPostion
   };
 
   return (
-    <div className="fmtm-w-full fmtm-px-2 sm:fmtm-px-0 fmtm-relative">
-      <div className="fmtm-sticky sm:fmtm-relative -fmtm-top-[1px] fmtm-bg-white sm:fmtm-bg-[#F5F5F5]">
+    <div className="fmtm-w-full fmtm-px-2 sm:fmtm-px-0 fmtm-relative fmtm-overflow-y-scroll scrollbar">
+      <div className="fmtm-sticky fmtm-top-[0px] fmtm-bg-white sm:fmtm-bg-[#F5F5F5]">
         <div className="fmtm-flex fmtm-items-center fmtm-w-full fmtm-justify-between fmtm-gap-4">
           <input
             type="text"
             onChange={handleOnchange}
             value={searchText}
             placeholder="Search by task id or username"
-            className="fmtm-w-full fmtm-text-md fmtm-px-1 fmtm-py-[0.18rem] fmtm-outline-none fmtm-border-[1px] fmtm-border-[#E7E2E2]"
+            className="fmtm-w-full fmtm-text-md fmtm-px-2 fmtm-py-[0.18rem] fmtm-outline-none fmtm-border-[1px] fmtm-border-[#E7E2E2] fmtm-mr-2"
           />
-          <div>
-            <div className="fmtm-relative">
-              <div
-                className="fmtm-flex fmtm-items-center fmtm-gap-1 fmtm-justify-end fmtm-cursor-pointer"
-                onClick={() => setShowSortBy(!showShortBy)}
-              >
-                <AssetModules.ImportExportIcon
-                  className={`${sortBy ? 'fmtm-text-primaryRed' : 'fmtm-text-gray-500'}`}
-                />
-                <p className="fmtm-text-gray-500">Sort</p>
-              </div>
-              {showShortBy && (
-                <div className="fmtm-w-[10rem] fmtm-absolute fmtm-top-6 fmtm-right-0 sm:fmtm-left-0 fmtm-bg-[#fafafa] sm:fmtm-bg-white fmtm-z-[999] fmtm-rounded-sm fmtm-overflow-hidden">
-                  {/* <p className="fmtm-border-b-[1px] fmtm-px-2 fmtm-py-1">Sort By:</p> */}
-                  {sortByList.map((item, i) => (
-                    <div
-                      className={`fmtm-flex fmtm-items-center fmtm-gap-4 fmtm-px-2 fmtm-py-1 hover:fmtm-bg-red-50 hover:fmtm-cursor-pointer fmtm-duration-150 ${
-                        sortByList.length - 1 > i &&
-                        'fmtm-border-b-[1px] fmtm-border-b-slate-200 sm:fmtm-border-gray-100'
-                      }`}
-                      onClick={() => {
-                        if (item.name === sortBy) {
-                          setSortBy(null);
-                        } else {
-                          setSortBy(item.name);
-                        }
-                        setShowSortBy(false);
-                      }}
-                    >
-                      {/* {sortBy === item.name &&} */}
-                      <AssetModules.CheckIcon
-                        className={`${sortBy === item.name ? 'fmtm-visible' : 'fmtm-invisible'} fmtm-text-primaryRed`}
-                      />
-                      <div>{item.name}</div>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
         </div>
         {projectDetailsLoading ? (
           <ShowingCountSkeletonLoader />
@@ -188,7 +141,7 @@ const ActivitiesPanel = ({ defaultTheme, state, params, map, view, mapDivPostion
           </p>
         )}
       </div>
-      <div className="sm:fmtm-h-[52vh] fmtm-overflow-y-scroll scrollbar">
+      <div>
         {projectDetailsLoading ? (
           <div>
             {Array.from({ length: 10 }).map((_, i) => (
