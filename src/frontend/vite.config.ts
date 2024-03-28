fa105fba467ab6d4e1cd40640c14713a763334f6
Deepak Pradhan <deepak.pradhan048@gmail.com>
2024-03-28 11:49:13 +0545

    fix: vite test

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -2,7 +2,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
