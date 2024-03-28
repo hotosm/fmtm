95f7ae55219023a6b1cfa3f81a9e12b667882aff
NSUWAL123 <suwalnishit@gmail.com>
2024-03-28 14:28:47 +0545

    feat formUpdateTab: form validation api add

--- src/frontend/src/api/CreateProjectService.ts
+++ src/frontend/src/api/CreateProjectService.ts
@@ -463,7 +463,7 @@ const ValidateCustomForm: Function = (url: string, formUpload: any) => {
         dispatch(
           CommonActions.SetSnackBar({
             open: true,
-            message: JSON.stringify(error.response.data.detail) || 'Something Went Wrong',
+            message: JSON.stringify(error) || 'Something Went Wrong',
             variant: 'error',
             duration: 5000,
           }),
