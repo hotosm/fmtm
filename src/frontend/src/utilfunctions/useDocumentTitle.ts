a5c96bad8e10cd961669924fcbec359092da2fd1
NSUWAL123 <suwalnishit@gmail.com>
2024-04-02 14:47:49 +0545

    feat useDocumentTitle - hook to dynamically set document title

---
+++ src/frontend/src/utilfunctions/useDocumentTitle.ts
@@ -0,0 +1,20 @@
+import { useRef, useEffect } from 'react';
+
+function useDocumentTitle(titleSuffix: string, prevailOnUnmount: boolean = false) {
+  const defaultTitle = useRef(document.title);
+
+  useEffect(() => {
+    document.title = `${titleSuffix} - HOT Field Mapping Tasking Manager`;
+  }, [titleSuffix]);
+
+  useEffect(
+    () => () => {
+      if (!prevailOnUnmount) {
+        document.title = defaultTitle.current;
+      }
+    },
+    [],
+  );
+}
+
+export default useDocumentTitle;
