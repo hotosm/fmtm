833d686da511cc0586496fd4ae8aa9dee8807acf
NSUWAL123 <suwalnishit@gmail.com>
2024-04-02 11:59:04 +0545

    feat qrCodeComponent: download qr btn add to mobile UI popup

--- src/frontend/src/components/QrcodeComponent.jsx
+++ src/frontend/src/components/QrcodeComponent.jsx
@@ -3,6 +3,12 @@ import CoreModules from '@/shared/CoreModules';
 import AssetModules from '@/shared/AssetModules';
 
 const TasksComponent = ({ qrcode, taskId }) => {
+  const downloadQR = () => {
+    const downloadLink = document.createElement('a');
+    downloadLink.href = qrcode;
+    downloadLink.download = `Task_${taskId}`;
+    downloadLink.click();
+  };
   return (
     <div className="fmtm-flex fmtm-justify-center sm:fmtm-py-5 fmtm-border-t-[1px]">
       <div className="fmtm-p-5 fmtm-border-[1px] fmtm-rounded-lg fmtm-relative fmtm-hidden sm:fmtm-block">
@@ -13,12 +19,7 @@ const TasksComponent = ({ qrcode, taskId }) => {
         )}
         <div className="fmtm-rounded-full fmtm-w-10 fmtm-h-10 fmtm-flex fmtm-justify-center fmtm-items-center fmtm-shadow-xl fmtm-absolute fmtm-bottom-0 -fmtm-right-5 fmtm-bg-white ">
           <button
-            onClick={() => {
-              const downloadLink = document.createElement('a');
-              downloadLink.href = qrcode;
-              downloadLink.download = `Task_${taskId}`;
-              downloadLink.click();
-            }}
+            onClick={downloadQR}
             disabled={qrcode == '' ? true : false}
             aria-label="download qrcode"
             className={` ${qrcode === '' ? 'fmtm-cursor-not-allowed fmtm-opacity-50' : 'fmtm-cursor-pointer'}`}
@@ -27,6 +28,20 @@ const TasksComponent = ({ qrcode, taskId }) => {
             <AssetModules.FileDownloadIcon />
           </button>
         </div>
+      </div>
+      <div className="fmtm-block sm:fmtm-hidden">
+        <button
+          className={`fmtm-border-primaryRed fmtm-border-[1px] fmtm-text-primaryRed fmtm-px-2 fmtm-py-1 fmtm-rounded fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-my-2 hover:fmtm-bg-red-50 ${
+            qrcode == '' ? 'fmtm-cursor-not-allowed fmtm-opacity-50' : 'fmtm-cursor-pointer'
+          }`}
+          aria-label="download qrcode"
+          disabled={!qrcode}
+          onClick={downloadQR}
+        >
+          <AssetModules.FileDownloadOutlinedIcon />
+          <p>Download QR</p>
+          <AssetModules.QrCode2OutlinedIcon />
+        </button>
       </div>
     </div>
   );
