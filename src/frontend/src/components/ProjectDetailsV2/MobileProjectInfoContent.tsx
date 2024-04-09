87903a6b4d4572d74f6287dcf8f81cecebe3d2f7
NSUWAL123 <suwalnishit@gmail.com>
2024-04-09 10:24:19 +0545

    feat mobileProjectInfoContent: manage-project btn add

--- src/frontend/src/components/ProjectDetailsV2/MobileProjectInfoContent.tsx
+++ src/frontend/src/components/ProjectDetailsV2/MobileProjectInfoContent.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import AssetModules from '@/shared/AssetModules';
 import { projectInfoType } from '@/models/project/projectModel';
+import Button from '@/components/common/Button';
+import { useNavigate } from 'react-router-dom';
 
 const MobileProjectInfoContent = ({ projectInfo }: { projectInfo: projectInfoType }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="fmtm-flex fmtm-flex-col fmtm-gap-3 fmtm-mb-[10vh]">
       <div className="fmtm-flex fmtm-gap-3 fmtm-border-b-[1px] fmtm-pb-2">
         <AssetModules.InfoIcon className=" fmtm-text-primaryRed" sx={{ fontSize: '35px' }} />
         <p className="fmtm-text-2xl">Project Information</p>
       </div>
-      <div className="fmtm-flex fmtm-gap-3">
+      <div className="fmtm-flex fmtm-gap-3 fmtm-justify-between">
         <p className="fmtm-text-2xl fmtm-text-primaryRed">#{projectInfo?.id}</p>
+        <Button
+          btnText="Manage Project"
+          icon={<AssetModules.SettingsIcon />}
+          onClick={() => {
+            navigate('/manage-project/${projectId}');
+          }}
+          btnType="other"
+          className="!fmtm-text-base !fmtm-pr-2"
+        />
       </div>
       <div className="fmtm-flex fmtm-gap-3">
         <p className="fmtm-text-xl">Name: </p>
