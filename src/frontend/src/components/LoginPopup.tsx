8187c0fe2fbb4253c7fa765a229311199cb0775f
NSUWAL123 <suwalnishit@gmail.com>
2024-04-18 15:49:17 +0545

    feat(LoginPopup): image & description add to loginOptions

--- src/frontend/src/components/LoginPopup.tsx
+++ src/frontend/src/components/LoginPopup.tsx
@@ -6,15 +6,30 @@ import { LoginActions } from '@/store/slices/LoginSlice';
 import Button from './common/Button';
 import { createLoginWindow } from '@/utilfunctions/login';
 import { TemporaryLoginService } from '@/api/Login';
+import AssetModules from '@/shared/AssetModules';
+import OSMImg from '@/assets/images/osm-logo.png';
 
 type loginOptionsType = {
   id: string;
   name: string;
+  icon?: React.ReactNode;
+  image?: string;
+  description: string;
 };
 
 const loginOptions: loginOptionsType[] = [
-  { id: 'osm_account', name: 'OSM Account' },
-  { id: 'temp_account', name: 'Temporary Account' },
+  {
+    id: 'osm_account',
+    name: 'Personal OSM Account',
+    image: OSMImg,
+    description: 'If you want to Log in using your individual OSM account.',
+  },
+  {
+    id: 'temp_account',
+    name: 'Temporary Account',
+    icon: <AssetModules.PersonIcon color="" sx={{ fontSize: '40px' }} className="fmtm-w-10 fmtm-h-10" />,
+    description: "If you're not an OSM user or prefer not to create an OSM account.",
+  },
 ];
 
 const LoginPopup = () => {
@@ -44,7 +59,7 @@ const LoginPopup = () => {
       <div className="fmtm-flex fmtm-items-start fmtm-flex-col">
         <h2 className="fmtm-text-2xl fmtm-font-bold fmtm-mb-1">Sign In</h2>
         <p className="fmtm-text-base fmtm-mb-5 fmtm-text-gray-700">Sign In with any of the accounts</p>
-        <div className="fmtm-grid fmtm-grid-cols-1 sm:fmtm-grid-cols-2 fmtm-w-full fmtm-gap-6 fmtm-justify-items-center">
+        <div className="fmtm-w-full fmtm-flex fmtm-flex-col fmtm-gap-4 fmtm-justify-items-center">
           {loginOptions?.map((option) => (
             <div
               key={option.id}
@@ -52,26 +67,21 @@ const LoginPopup = () => {
               className={`${
                 activeLogin === option.id
                   ? 'fmtm-bg-red-50 fmtm-text-primaryRed fmtm-border-primaryRed'
-                  : 'fmtm-bg-white fmtm-border-gray-300 fmtm-text-gray-700'
-              } fmtm-col-span-1 fmtm-w-full fmtm-max-w-[15rem] fmtm-py-3 fmtm-px-4 fmtm-rounded-md fmtm-duration-300 hover:fmtm-border-primaryRed hover:fmtm-text-primaryRed fmtm-border-[1px] fmtm-cursor-pointer fmtm-text-sm fmtm-flex fmtm-items-center fmtm-gap-3 fmtm-group`}
+                  : 'fmtm-bg-[#F5F5F5] fmtm-border-gray-300 fmtm-text-gray-700'
+              } fmtm-w-full fmtm-py-3 fmtm-px-4 fmtm-rounded-md fmtm-duration-300 hover:fmtm-border-primaryRed hover:fmtm-text-primaryRed fmtm-border-[1px] fmtm-cursor-pointer fmtm-text-sm fmtm-flex fmtm-items-start fmtm-gap-3 fmtm-group`}
             >
-              <div
-                className={`fmtm-border-[1px] ${
-                  activeLogin === option.id ? 'fmtm-border-primaryRed' : 'fmtm-border-gray-400'
-                } fmtm-rounded-full fmtm-w-3 fmtm-h-3 fmtm-flex fmtm-justify-center fmtm-items-center group-hover:fmtm-border-primaryRed fmtm-duration-300`}
-              >
-                <div
-                  className={`fmtm-h-[8px] fmtm-w-[8px] fmtm-rounded-full fmtm-duration-300 group-hover:fmtm-bg-primaryRed group-hover:fmtm-border-primaryRed ${
-                    activeLogin === option.id ? 'fmtm-bg-primaryRed' : 'fmtm-bg-gray-400'
-                  }`}
-                ></div>
+              <div className="fmtm-w-10 fmtm-max-w-10 fmtm-min-w-10">
+                {option?.image ? <img src={option?.image} className="fmtm-w-full" /> : option?.icon}
               </div>
-              <p className="fmtm-text-base">{option.name}</p>
+              <div className="fmtm-flex fmtm-flex-col">
+                <p className="fmtm-text-lg">{option.name}</p>
+                <p className="">{option.description}</p>
+              </div>
             </div>
           ))}
         </div>
         {activeLogin && (
-          <div className="fmtm-w-full fmtm-flex fmtm-justify-center fmtm-mt-10">
+          <div className="fmtm-w-full fmtm-flex fmtm-justify-center fmtm-mt-6">
             <Button
               btnText="Sign In"
               btnType="primary"
@@ -92,7 +102,7 @@ const LoginPopup = () => {
         onOpenChange={(openStatus) => {
           dispatch(LoginActions.setLoginModalOpen(openStatus));
         }}
-        className=""
+        className="!fmtm-max-w-[35rem]"
       />
     </div>
   );
