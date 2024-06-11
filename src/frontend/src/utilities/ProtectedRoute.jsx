1a0e63aca202fb06f86d4f0ee769a775b3aab938
NSUWAL123 <suwalnishit@gmail.com>
2024-06-11 11:52:07 +0545

    feat(protectedRoute): pass current pathname state to navigate

--- src/frontend/src/utilities/ProtectedRoute.jsx
+++ src/frontend/src/utilities/ProtectedRoute.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import CoreModules from '@/shared/CoreModules';
 import { LoginActions } from '@/store/slices/LoginSlice';
 
 const ProtectedRoute = ({ children, next }) => {
   const dispatch = CoreModules.useAppDispatch();
   const authDetails = CoreModules.useAppSelector((state) => state.login.authDetails);
+  const { pathname } = useLocation();
 
   if (authDetails == null) {
     dispatch(LoginActions.setLoginModalOpen(true));
-    return <Navigate to="/" replace />;
+    return <Navigate to="/" replace state={{ from: pathname }} />;
   }
 
   return children;
