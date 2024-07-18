bd2cd2641286e5a46c291994fca0ed150508c507
NSUWAL123 <suwalnishit@gmail.com>
2024-07-18 12:54:59 +0545

    fix(createproject): add assertions & delete project after creation

--- src/frontend/e2e/createproject.spec.ts
+++ src/frontend/e2e/createproject.spec.ts
@@ -5,17 +5,17 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByText('Temporary Account').click();
   await page.getByRole('button', { name: '+ Create New Project' }).click();
-  await page.getByRole('button', { name: 'NEXT' }).click();
 
   // 1. Project Details Step
+  await page.getByRole('button', { name: 'NEXT' }).click();
   await expect(page.getByText('Project Name is Required.')).toBeVisible();
-  await expect(page.getByText('Short Description is Required.')).toBeVisible();
-  await expect(page.getByText('Description is Required.')).toBeVisible();
+  await expect(page.getByText('Short Description is Required.', { exact: true })).toBeVisible();
+  await expect(page.getByText('Description is Required.', { exact: true })).toBeVisible();
   await expect(page.getByText('Organization is Required.')).toBeVisible();
   await expect(page.getByText('ODK URL is Required.')).toBeVisible();
 
   await page.locator('#name').click();
-  await page.locator('#name').fill('playwright test');
+  await page.locator('#name').fill('Playwright Test');
   await page.locator('#short_description').click();
   await page.locator('#short_description').fill('short');
   await page.locator('#description').click();
@@ -38,8 +38,9 @@ test('test', async ({ page }) => {
   );
 
   await page.locator('#data-extract-custom-file').setInputFiles(`${__dirname}/files/invalid-aoi.geojson`);
-  // await page.getByRole('alert').locator('div').filter({ hasText: 'The project area exceeded 200' }).click();
-  // await page.getByRole('heading', { name: 'The project area exceeded 200' }).click();
+  await expect(page.getByText('The project area exceeded 200')).toBeVisible();
+
+  await page.locator('#data-extract-custom-file').setInputFiles([]);
 
   await page.locator('#data-extract-custom-file').setInputFiles(`${__dirname}/files/valid-aoi.geojson`);
   // Reapply the hidden class to the input element
@@ -67,4 +68,19 @@ test('test', async ({ page }) => {
   await page.getByRole('spinbutton').fill('200');
   await page.getByRole('button', { name: 'Click to generate task' }).click();
   await page.getByRole('button', { name: 'SUBMIT' }).click();
+  await expect(page.getByText('Project Generation Completed. Redirecting...')).toBeVisible();
+
+  // 6. Delete Project after Creation
+  await page.getByRole('button', { name: 'MANAGE PROJECT' }).click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^DELETE$/ })
+    .click();
+  await page.getByRole('button', { name: 'DELETE PROJECT' }).click();
+  await page.waitForSelector('textbox');
+  await page.getByRole('textbox').click();
+  await page.getByRole('textbox').fill('Playwright Test');
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'Confirm' }).click();
+  await expect(page.getByText('Project deleted. Redirecting...')).toBeVisible();
 });
