a9ff7f0f7f09c580de994ea7d27a1e61e528c415
NSUWAL123 <suwalnishit@gmail.com>
2024-07-25 11:26:17 +0545

    fix(editTab): remove unused useeffect

--- src/frontend/src/components/ManageProject/EditTab/index.tsx
+++ src/frontend/src/components/ManageProject/EditTab/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ProjectDescriptionTab from './ProjectDescriptionTab';
 import FormUpdateTab from './FormUpdateTab';
 
