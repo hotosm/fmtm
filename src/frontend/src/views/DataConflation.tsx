3a788af714b660f3cd69787950801adbedcf65ab
NSUWAL123 <suwalnishit@gmail.com>
2024-07-31 10:26:55 +0545

    fix(dataConflation): UI fix

--- src/frontend/src/views/DataConflation.tsx
+++ src/frontend/src/views/DataConflation.tsx
@@ -10,7 +10,7 @@ const DataConflation = () => {
   const [showSubmissionConflation, setShowSubmissionConflation] = useState(true);
 
   return (
-    <div className="fmtm-bg-[#F5F5F5] fmtm-p-6 md:fmtm-h-full fmtm-relative">
+    <div className="fmtm-bg-[#F5F5F5] md:fmtm-h-full fmtm-relative fmtm-p-5">
       <Modal
         title={<p className="fmtm-text-left">Merge Data With OSM</p>}
         description={
@@ -45,15 +45,15 @@ const DataConflation = () => {
         className=""
       />
 
-      <div className="fmtm-w-full fmtm-h-full fmtm-flex fmtm-gap-5 fmtm-flex-col lg:fmtm-flex-row">
+      <div className="fmtm-w-full fmtm-flex fmtm-gap-5 fmtm-flex-col lg:fmtm-flex-row md:fmtm-h-full">
         <div className="2xl:fmtm-w-[15%] lg:fmtm-h-full">
           <TaskInfo />
         </div>
-        <div className="fmtm-h-full fmtm-flex fmtm-flex-col md:fmtm-flex-row fmtm-gap-5 fmtm-w-full lg:fmtm-w-[85%]">
+        <div className="md:fmtm-h-[calc(100%-205px)] lg:fmtm-h-full fmtm-flex fmtm-flex-col md:fmtm-flex-row fmtm-gap-5 fmtm-w-full lg:fmtm-w-[85%]">
           <div className="fmtm-h-[50vh] fmtm-w-full md:fmtm-h-full md:fmtm-w-[60%]">
             <ConflationMap />
           </div>
-          <div className="fmtm-w-full fmtm-h-[60vh] md:fmtm-h-full md:fmtm-w-[40%]">
+          <div className="fmtm-w-full md:fmtm-h-full md:fmtm-w-[40%]">
             {showSubmissionConflation && <SubmissionConflation />}
           </div>
         </div>
