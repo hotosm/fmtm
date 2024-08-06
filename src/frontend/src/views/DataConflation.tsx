191327f849356a72690b2ac4f6446b5ab55566bc
NSUWAL123 <suwalnishit@gmail.com>
2024-08-06 13:01:23 +0545

    fix(dataConflation): UI style fix according to feature click

--- src/frontend/src/views/DataConflation.tsx
+++ src/frontend/src/views/DataConflation.tsx
@@ -13,9 +13,7 @@ const DataConflation = () => {
   const dispatch = useDispatch();
   const { projectId, taskId } = useParams();
   const [openModal, setOpenModal] = useState(true);
-  const [showSubmissionConflation, setShowSubmissionConflation] = useState(true);
-
-  const submissionConflationGeojson = useAppSelector((state) => state.dataconflation.submissionConflationGeojson);
+  const selectedFeatureOSMId = useAppSelector((state) => state.dataconflation.selectedFeatureOSMId);
 
   useEffect(() => {
     dispatch(
@@ -68,12 +66,18 @@ const DataConflation = () => {
           <TaskInfo />
         </div>
         <div className="md:fmtm-h-[calc(100%-205px)] lg:fmtm-h-full fmtm-flex fmtm-flex-col md:fmtm-flex-row fmtm-gap-5 fmtm-w-full lg:fmtm-w-[80%]">
-          <div className="fmtm-h-[50vh] fmtm-w-full md:fmtm-h-full md:fmtm-w-[65%]">
+          <div
+            className={`fmtm-h-[50vh] fmtm-w-full md:fmtm-h-full fmtm-duration-200 ${
+              selectedFeatureOSMId ? 'md:fmtm-w-[65%]' : 'md:fmtm-w-full'
+            }`}
+          >
             <ConflationMap />
           </div>
-          <div className="fmtm-w-full md:fmtm-h-full md:fmtm-w-[35%]">
-            {showSubmissionConflation && <SubmissionConflation />}
-          </div>
+          {selectedFeatureOSMId && (
+            <div className="fmtm-w-full md:fmtm-h-full md:fmtm-w-[35%] fmtm-duration-200">
+              <SubmissionConflation />
+            </div>
+          )}
         </div>
       </div>
     </div>
