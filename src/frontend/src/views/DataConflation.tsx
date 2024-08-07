71aef5782da2332cc98e7f3b0e1173a933ea9581
NSUWAL123 <suwalnishit@gmail.com>
2024-08-07 10:52:40 +0545

    fix(dataConflation): remove modal

--- src/frontend/src/views/DataConflation.tsx
+++ src/frontend/src/views/DataConflation.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import ConflationMap from '@/components/DataConflation/ConflationMap';
 import TaskInfo from '@/components/DataConflation/TaskInfo';
-import { Modal } from '@/components/common/Modal';
-import Button from '@/components/common/Button';
 import SubmissionConflation from '@/components/DataConflation/SubmissionConflation';
 import { useDispatch } from 'react-redux';
 import { SubmissionConflationGeojsonService } from '@/api/DataConflation';
@@ -12,7 +10,6 @@ import { useAppSelector } from '@/types/reduxTypes';
 const DataConflation = () => {
   const dispatch = useDispatch();
   const { projectId, taskId } = useParams();
-  const [openModal, setOpenModal] = useState(true);
   const selectedFeatureOSMId = useAppSelector((state) => state.dataconflation.selectedFeatureOSMId);
 
   useEffect(() => {
@@ -27,40 +24,6 @@ const DataConflation = () => {
 
   return (
     <div className="fmtm-bg-[#F5F5F5] md:fmtm-h-full fmtm-relative fmtm-p-5">
-      <Modal
-        title={<p className="fmtm-text-left">Merge Data With OSM</p>}
-        description={
-          <div className="fmtm-mt-1">
-            <p className="fmtm-text-sm fmtm-text-[#7A7676]">
-              This step compares FMTM submission with OSM tags/geometry, highlighting conflicts in different colors
-            </p>
-            <div className="fmtm-flex fmtm-gap-[0.625rem] fmtm-justify-end fmtm-mt-5">
-              <Button
-                btnText="Cancel"
-                btnType="other"
-                onClick={() => {
-                  setOpenModal(false);
-                }}
-                type="button"
-                className="!fmtm-rounded fmtm-text-sm !fmtm-py-2"
-              />
-              <Button
-                btnText="Merge Data"
-                btnType="primary"
-                onClick={() => {
-                  setOpenModal(false);
-                }}
-                type="button"
-                className="!fmtm-rounded fmtm-text-sm !fmtm-py-2"
-              />
-            </div>
-          </div>
-        }
-        open={false}
-        onOpenChange={(status) => setOpenModal(status)}
-        className=""
-      />
-
       <div className="fmtm-w-full fmtm-flex fmtm-gap-5 fmtm-flex-col lg:fmtm-flex-row md:fmtm-h-full">
         <div className="2xl:fmtm-w-[20%] lg:fmtm-h-full">
           <TaskInfo />
