bdfdc5186051e0466eab26811e27ef7164a0bf07
NSUWAL123 <suwalnishit@gmail.com>
2024-09-05 10:05:08 +0545

    feat(02-mapper-flow): comment section test add

--- src/frontend/e2e/02-mapper-flow.spec.ts
+++ src/frontend/e2e/02-mapper-flow.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('mapper flow', () => {
   test('mapper flow', async ({ page }) => {
     await page.goto('/');
+    // await page.goto('http://fmtm.localhost:7050/');
     await page.getByRole('button', { name: 'Sign in' }).click();
     await page
       .getByLabel('', { exact: true })
@@ -64,5 +65,60 @@ test.describe('mapper flow', () => {
     await expect(page.getByText('Status: VALIDATED')).toBeVisible();
   });
 
-  test('comment section', async ({ page }) => {});
+  test('comment section', async ({ page }) => {
+    await page.goto('/');
+    // await page.goto('http://fmtm.localhost:7050/');
+    await page.getByRole('button', { name: 'Sign in' }).click();
+    await page
+      .getByLabel('', { exact: true })
+      .locator('div')
+      .filter({ hasText: "Temporary AccountIf you're" })
+      .nth(3)
+      .click();
+
+    // click first project card on the home page
+    await page.locator('.MuiCardContent-root').first().click();
+
+    await page.waitForTimeout(4000);
+    await page.locator('canvas').click({
+      position: {
+        x: 475,
+        y: 127,
+      },
+    });
+
+    // Assert no comment is visible
+    await page.getByRole('button', { name: 'Comments' }).click();
+    await expect(page.getByText('No Comments!')).toBeVisible();
+
+    // Add comment
+    await page.getByTestId('FormatBoldIcon').click();
+    await page.locator('.fmtm-min-h-\\[150px\\] > .tiptap > p').click();
+    await page.locator('.fmtm-min-h-\\[150px\\] > .tiptap').fill('Test playwright');
+    await page.getByRole('button', { name: 'SAVE COMMENT' }).click();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /Test playwright/ })
+        .first(),
+    ).toBeVisible();
+
+    // Add comment
+    await page.locator('.fmtm-min-h-\\[150px\\] > .tiptap > p').click();
+    await page.locator('.fmtm-min-h-\\[150px\\] > .tiptap > p').click();
+    await page.locator('.fmtm-min-h-\\[150px\\] > .tiptap').fill('map features accurately');
+    await page.getByRole('button', { name: 'SAVE COMMENT' }).click();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /map features accurately/ })
+        .first(),
+    ).toBeVisible();
+
+    // Save empty comment
+    await page.locator('.fmtm-min-h-\\[150px\\] > .tiptap > p').click();
+    await page.getByRole('button', { name: 'SAVE COMMENT' }).click();
+    await page.getByRole('heading', { name: 'Empty comment field.' }).click();
+    await page.getByRole('alert').click();
+  });
 });
