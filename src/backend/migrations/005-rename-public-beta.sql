0cd71faa1f3cd9a914cd5026f3185921c497cb8e
spwoodcock <sam.woodcock@protonmail.com>
2024-09-20 21:58:59 +0100

    build: update public beta rename migration to be idempotent

--- src/backend/migrations/005-rename-public-beta.sql
+++ src/backend/migrations/005-rename-public-beta.sql
@@ -4,12 +4,21 @@
 -- Start a transaction
 BEGIN;
 
-UPDATE public.organisations
-SET
-    name = 'HOTOSM',
-    slug = 'hotosm',
-    description = 'Humanitarian OpenStreetMap Team'
-WHERE slug = 'fmtm-public-beta';
+-- Check if the organization with the new name 'HOTOSM' already exists
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM public.organisations WHERE slug = 'hotosm'
+    ) THEN
+        -- If 'HOTOSM' does not exist, update the organisation
+        UPDATE public.organisations
+        SET
+            name = 'HOTOSM',
+            slug = 'hotosm',
+            description = 'Humanitarian OpenStreetMap Team'
+        WHERE slug = 'fmtm-public-beta';
+    END IF;
+END $$;
 
 -- Commit the transaction
 COMMIT;
