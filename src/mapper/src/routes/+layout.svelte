ad8f7a08bd1d1ef48f2a712d39f0432cb73255aa
NSUWAL123 <suwalnishit@gmail.com>
2024-10-21 12:09:52 +0545

    feat(layout): add toast component to the main

--- src/mapper/src/routes/+layout.svelte
+++ src/mapper/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '@hotosm/ui/dist/style.css';
 	import '@hotosm/ui/dist/hotosm-ui';
 	import { setBasePath } from '@hotosm/ui/dist/hotosm-ui';
+	import Toast from '$lib/components/common/toast.svelte';
 
 	setBasePath('/mapnow');
 
@@ -10,5 +11,6 @@
 
 <main class="flex flex-col h-screen overflow-hidden">
 	<hot-header></hot-header>
+	<Toast />
 	<slot {data}></slot>
 </main>
