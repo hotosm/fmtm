87ff14b14ad9329ee05786d65801c5b3c39f5d5d
NSUWAL123 <suwalnishit@gmail.com>
2024-10-22 13:03:30 +0545

    fix(more): UI slicing for comment section

--- src/mapper/src/lib/components/page/more/index.svelte
+++ src/mapper/src/lib/components/page/more/index.svelte
@@ -25,7 +25,7 @@
 	{#if activeStack === ''}
 		{#each stackGroup as stack}
 			<div
-				class="group flex items-center justify-between hover:bg-red-50 rounded-md px-2 duration-200 cursor-pointer"
+				class="group flex items-center justify-between hover:bg-red-50 rounded-md p-2 duration-200 cursor-pointer"
 				on:click={() => (activeStack = stack.title)}
 			>
 				<div class="flex items-center gap-3">
@@ -39,21 +39,45 @@
 
 	<!-- header -->
 	{#if activeStack !== ''}
-		<div class="flex items-center gap-x-2 sticky top-0 bg-white">
+		<div class="flex items-center gap-x-2 sticky top-0 bg-white pb-2 z-50">
 			<hot-icon
 				name="chevron-left"
 				class="text-[1rem] hover:-translate-x-[2px] duration-200 cursor-pointer text-[1.125rem] text-black hover:text-red-600 duration-200"
 				on:click={() => (activeStack = '')}
 			></hot-icon>
-			<p class="text-[1.125rem] font-barlow-semibold leading-0">{activeStack}</p>
+			<p class="text-[1.125rem] font-barlow-semibold">{activeStack}</p>
 		</div>
 	{/if}
 	<!-- body -->
 	{#if activeStack === 'Comment'}
 		<div class="h-[calc(100%-2.25rem)] sm:h-[calc(100%-2.6rem)]">
-			<div class="h-[calc(100%-11.875rem)] overflow-y-scroll"></div>
+			<div class="h-[calc(100%-11.875rem)] overflow-y-scroll overflow-x-hidden flex flex-col gap-2">
+				{#each Array.from({ length: 5 }) as _, index}
+					<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md mr-1">
+						<div class="flex gap-4 px-3">
+							<hot-icon
+								name="person-fill"
+								style="border: 1px solid"
+								class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
+							></hot-icon>
+							<div class="flex flex-col gap-1 flex-1">
+								<p class="font-semibold">Localadmin</p>
+								<div class="flex items-center justify-between">
+									<p class="text-[#484848] text-sm">#2</p>
+									<div class="flex items-center gap-2">
+										<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"
+										></hot-icon>
+										<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<Editor editable={false} content={'<p>This is a comment</p>'} />
+					</div>
+				{/each}
+			</div>
 
-			<div class="">
+			<div class="mt-2">
 				<Editor
 					editable={true}
 					content=""
