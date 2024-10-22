a73492bd01fa178c26151f28fe50434c62fa74e3
NSUWAL123 <suwalnishit@gmail.com>
2024-10-22 14:14:08 +0545

    feat(more): instructions section UI slicing

--- src/mapper/src/lib/components/page/more/index.svelte
+++ src/mapper/src/lib/components/page/more/index.svelte
@@ -17,7 +17,7 @@
 		},
 	];
 
-	let activeStack: stackType = 'Comment';
+	let activeStack: stackType = '';
 	export let instructions;
 </script>
 
@@ -53,7 +53,7 @@
 		<div class="h-[calc(100%-2.25rem)] sm:h-[calc(100%-2.6rem)]">
 			<div class="h-[calc(100%-11.875rem)] overflow-y-scroll overflow-x-hidden flex flex-col gap-2">
 				{#each Array.from({ length: 5 }) as _, index}
-					<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md mr-1">
+					<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md">
 						<div class="flex gap-4 px-3">
 							<hot-icon
 								name="person-fill"
@@ -102,8 +102,36 @@
 		</div>
 	{/if}
 	{#if activeStack === 'Activities'}
-		<div>
-			<p>Activities</p>
+		<div class="overflow-y-scroll overflow-x-hidden flex flex-col gap-2 pb-2">
+			{#each Array.from({ length: 15 }) as _, index}
+				<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md mr-1">
+					<div class="flex gap-4 px-3">
+						<hot-icon
+							name="person-fill"
+							style="border: 1px solid"
+							class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
+						></hot-icon>
+						<div class="flex flex-col gap-1 flex-1">
+							<p class="font-semibold">Localadmin</p>
+							<div class="flex items-center justify-between">
+								<p class="text-[#484848] text-sm">#2</p>
+								<div class="flex items-center gap-2">
+									<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"
+									></hot-icon>
+									<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="px-3 flex items-center fmtm-justify-between gap-2">
+						<p class="font-normal text-[#484848] flex-1">
+							<span class="">svcfmtm</span> updated status to <span>MAPPED</span>
+						</p>
+						<hot-icon name="map" class="!text-[1rem] text-[#484848] hover:text-red-600 cursor-pointer duration-200"
+						></hot-icon>
+					</div>
+				</div>
+			{/each}
 		</div>
 	{/if}
 </div>
