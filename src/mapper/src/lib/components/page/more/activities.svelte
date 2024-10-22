c6442e67de872df1c812b72829e4ac4941f1329a
NSUWAL123 <suwalnishit@gmail.com>
2024-10-22 18:05:18 +0545

    feat(more): component broken down to smaller comment, activities components

---
+++ src/mapper/src/lib/components/page/more/activities.svelte
@@ -0,0 +1,65 @@
+<script>
+</script>
+
+<div class="overflow-y-scroll overflow-x-hidden flex flex-col gap-2 pb-2">
+	{#if false}
+		{#each Array.from({ length: 5 }) as _, index}
+			<div class="bg-[#F6F5F5] rounded-md p-3">
+				<div class="flex gap-4">
+					<hot-skeleton
+						style="--color: #e3e1e1; --sheen-color: #fafafa;"
+						effect="sheen"
+						class="w-[2.063rem] h-[2.063rem]"
+					></hot-skeleton>
+					<div class="flex-1">
+						<hot-skeleton style="--color: #e3e1e1; --sheen-color: #fafafa;" effect="sheen" class="w-[8rem] mb-2"
+						></hot-skeleton>
+						<div class="flex justify-between flex-1">
+							<hot-skeleton style="--color: #e3e1e1; --sheen-color: #fafafa;" effect="sheen" class="w-[1rem]"
+							></hot-skeleton>
+							<hot-skeleton style="--color: #e3e1e1; --sheen-color: #fafafa;" effect="sheen" class="w-[7.75rem]"
+							></hot-skeleton>
+						</div>
+					</div>
+				</div>
+				<div class="flex justify-between items-center mt-3">
+					<hot-skeleton style="--color: #e3e1e1; --sheen-color: #fafafa;" effect="sheen" class="w-[16rem]"
+					></hot-skeleton>
+					<hot-skeleton style="--color: #e3e1e1; --sheen-color: #fafafa;" effect="sheen" class="w-[1.5rem] h-[1.5rem]"
+					></hot-skeleton>
+				</div>
+			</div>
+		{/each}
+	{:else}
+		{#each Array.from({ length: 5 }) as _, index}
+			<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md mr-1">
+				<div class="flex gap-4 px-3">
+					<hot-icon
+						name="person-fill"
+						style="border: 1px solid"
+						class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
+					></hot-icon>
+					<div class="flex flex-col gap-1 flex-1">
+						<p class="font-semibold">Localadmin</p>
+						<div class="flex items-center justify-between">
+							<p class="text-[#484848] text-sm">#2</p>
+							<div class="flex items-center gap-2">
+								<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"></hot-icon>
+								<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="px-3 flex items-center fmtm-justify-between gap-2">
+					<p class="font-normal text-[#484848] flex-1">
+						<span class="">svcfmtm</span> updated status to <span>MAPPED</span>
+					</p>
+					<hot-icon name="map" class="!text-[1rem] text-[#484848] hover:text-red-600 cursor-pointer duration-200"
+					></hot-icon>
+				</div>
+			</div>
+		{/each}
+	{/if}
+</div>
+
+<style></style>
--- src/mapper/src/lib/components/page/more/index.svelte
+++ src/mapper/src/lib/components/page/more/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Editor from '$lib/components/common/Editor/editor.svelte';
+	import Comment from '$lib/components/page/more/comment.svelte';
+	import Activities from '$lib/components/page/more/activities.svelte';
 
 	type stackType = '' | 'Comment' | 'Instructions' | 'Activities';
 	const stackGroup: { icon: string; title: stackType }[] = [
@@ -17,7 +19,7 @@
 		},
 	];
 
-	let activeStack: stackType = '';
+	let activeStack: stackType = 'Activities';
 	export let instructions;
 </script>
 
@@ -50,90 +52,13 @@
 	{/if}
 	<!-- body -->
 	{#if activeStack === 'Comment'}
-		<div class="h-[calc(100%-2.25rem)] sm:h-[calc(100%-2.6rem)]">
-			<div class="h-[calc(100%-11.875rem)] overflow-y-scroll overflow-x-hidden flex flex-col gap-2">
-				{#each Array.from({ length: 5 }) as _, index}
-					<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md">
-						<div class="flex gap-4 px-3">
-							<hot-icon
-								name="person-fill"
-								style="border: 1px solid"
-								class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
-							></hot-icon>
-							<div class="flex flex-col gap-1 flex-1">
-								<p class="font-semibold">Localadmin</p>
-								<div class="flex items-center justify-between">
-									<p class="text-[#484848] text-sm">#2</p>
-									<div class="flex items-center gap-2">
-										<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"
-										></hot-icon>
-										<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<Editor editable={false} content={'<p>This is a comment</p>'} />
-					</div>
-				{/each}
-			</div>
-
-			<div class="mt-2">
-				<Editor
-					editable={true}
-					content=""
-					setEditorHtmlContent={(editorText) => {
-						// to-do: store state to post comment
-					}}
-				/>
-				<div class="w-full flex justify-end my-2 gap-2">
-					<sl-button variant="default" size="small" class="secondary col-span-2 sm:col-span-1"
-						><span class="font-barlow-medium text-sm">CLEAR</span></sl-button
-					>
-					<sl-button variant="default" size="small" class="primary col-span-2 sm:col-span-1"
-						><span class="font-barlow-medium text-sm">COMMENT</span></sl-button
-					>
-				</div>
-			</div>
-		</div>
+		<Comment />
 	{/if}
+
 	{#if activeStack === 'Instructions'}
-		<div>
-			<Editor editable={false} content={instructions} />
-		</div>
+		<Editor editable={false} content={instructions} />
 	{/if}
 	{#if activeStack === 'Activities'}
-		<div class="overflow-y-scroll overflow-x-hidden flex flex-col gap-2 pb-2">
-			{#each Array.from({ length: 15 }) as _, index}
-				<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md mr-1">
-					<div class="flex gap-4 px-3">
-						<hot-icon
-							name="person-fill"
-							style="border: 1px solid"
-							class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
-						></hot-icon>
-						<div class="flex flex-col gap-1 flex-1">
-							<p class="font-semibold">Localadmin</p>
-							<div class="flex items-center justify-between">
-								<p class="text-[#484848] text-sm">#2</p>
-								<div class="flex items-center gap-2">
-									<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"
-									></hot-icon>
-									<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="px-3 flex items-center fmtm-justify-between gap-2">
-						<p class="font-normal text-[#484848] flex-1">
-							<span class="">svcfmtm</span> updated status to <span>MAPPED</span>
-						</p>
-						<hot-icon name="map" class="!text-[1rem] text-[#484848] hover:text-red-600 cursor-pointer duration-200"
-						></hot-icon>
-					</div>
-				</div>
-			{/each}
-		</div>
+		<Activities />
 	{/if}
 </div>
-
-<style></style>
