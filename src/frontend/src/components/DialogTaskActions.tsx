d703213022691a957a4583d0ff85aaa60faa0397
NSUWAL123 <suwalnishit@gmail.com>
2024-10-29 10:27:23 +0545

    fix(dialogTaskActions): remove taskBoundary from args, pass geojsonStyles to as args

--- src/frontend/src/components/DialogTaskActions.tsx
+++ src/frontend/src/components/DialogTaskActions.tsx
@@ -92,7 +92,7 @@ export default function Dialog({ taskId, feature }: dialogPropType) {
     if (!btnId) return;
     const selectedAction = taskEventEnum[btnId];
     const authDetailsCopy = authDetails != null ? { ...authDetails } : {};
-    const geoStyle = geojsonStyles[btnId];
+
     if (btnId != undefined) {
       if (authDetailsCopy.hasOwnProperty('id')) {
         // if (btnId === 'MERGE_WITH_OSM') {
@@ -107,8 +107,7 @@ export default function Dialog({ taskId, feature }: dialogPropType) {
             taskId.toString(),
             authDetailsCopy,
             { project_id: currentProjectId },
-            geoStyle,
-            taskBoundaryData,
+            geojsonStyles,
             feature,
           ),
         );
