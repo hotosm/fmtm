7e9f044b67a17cee72bd914fbba293e7f566ef6e
spwoodcock <sam.woodcock@protonmail.com>
2024-11-05 18:04:56 +0000

    build: restructure / simplify frontend docker volumes

--- src/container-entrypoint.sh
+++ src/container-entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-# Copy frontend to attached volume
+# Copy frontend to attached volume, making it
+# accessible to the Nginx container
 echo "Syncing files from /app --> /frontend."
 rclone sync /app /frontend
 echo "Sync done."
