f6290873881e7569c363ae2a2b7f8d4038dd6428
Nishit Suwal <81785002+NSUWAL123@users.noreply.github.com>
2024-11-11 13:42:09 +0545

    feat(frontend): visualise submission photos via slider (#1857)
    * feat(submission): submission photos api call and slice add
    
    * feat(assetModules): icons add
    
    * fix(modal): fix close btn styling
    
    * feat(imageSlider): image slider component add
    
    * fix(submissionDetails): add imageSlider to visualize uploaded submission photos
    
    * fix(submissionDetails): add images heading

--- src/frontend/src/views/SubmissionDetails.tsx
+++ src/frontend/src/views/SubmissionDetails.tsx
@@ -1,6 +1,6 @@
 import CoreModules from '@/shared/CoreModules.js';
 import React, { useEffect } from 'react';
-import { SubmissionService } from '@/api/Submission';
+import { SubmissionService, GetSubmissionPhotosService } from '@/api/Submission';
 import SubmissionInstanceMap from '@/components/SubmissionMap/SubmissionInstanceMap';
 import { GetSubmissionDashboard } from '@/api/Project';
 import Button from '@/components/common/Button';
@@ -12,6 +12,7 @@ import useDocumentTitle from '@/utilfunctions/useDocumentTitle';
 import Accordion from '@/components/common/Accordion';
 import { GetProjectComments } from '@/api/Project';
 import SubmissionComments from '@/components/SubmissionInstance/SubmissionComments';
+import ImageSlider from '@/components/common/ImageSlider';
 
 const renderValue = (value: any, key: string = '') => {
   if (key === 'start' || key === 'end') {
@@ -95,6 +96,8 @@ const SubmissionDetails = () => {
   const submissionDetails = useAppSelector((state) => state.submission.submissionDetails);
   const submissionDetailsLoading = useAppSelector((state) => state.submission.submissionDetailsLoading);
   const taskId = submissionDetails?.task_id ? submissionDetails?.task_id : '-';
+  const submissionPhotosLoading = useAppSelector((state) => state.submission.submissionPhotosLoading);
+  const submissionPhotos = useAppSelector((state) => state.submission.submissionPhotos);
 
   const { start, end, today, deviceid, ...restSubmissionDetails } = submissionDetails || {};
   const dateDeviceDetails = { start, end, today, deviceid };
@@ -118,6 +121,12 @@ const SubmissionDetails = () => {
       ),
     );
   }, [taskUid]);
+
+  useEffect(() => {
+    if (paramsInstanceId) {
+      dispatch(GetSubmissionPhotosService(`${import.meta.env.VITE_API_URL}/submission/${paramsInstanceId}/photos`));
+    }
+  }, [paramsInstanceId]);
 
   const filteredData = restSubmissionDetails ? removeNullValues(restSubmissionDetails) : {};
 
@@ -263,7 +272,7 @@ const SubmissionDetails = () => {
             </div>
           </div>
         </div>
-        <div className="fmtm-grid fmtm-grid-cols-1 md:fmtm-grid-cols-2 fmtm-gap-x-8 fmtm-mt-10 fmtm-gap-y-10">
+        <div className="fmtm-grid fmtm-grid-cols-1 md:fmtm-grid-cols-2 fmtm-gap-x-8 fmtm-mt-10 fmtm-gap-y-10 fmtm-mb-5">
           {submissionDetailsLoading ? (
             <div className="fmtm-flex fmtm-flex-col fmtm-gap-3 fmtm-mt-5">
               {Array.from({ length: 8 }).map((_, i) => (
@@ -285,6 +294,23 @@ const SubmissionDetails = () => {
             <SubmissionComments />
           </div>
         </div>
+        {/* submission photos */}
+        {submissionPhotosLoading ? (
+          <div className="fmtm-flex fmtm-gap-x-3 fmtm-overflow-x-scroll scrollbar fmtm-bg-white fmtm-p-6 fmtm-rounded-xl">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <CoreModules.Skeleton
+                key={i}
+                style={{ width: '9.688rem', height: '10.313rem' }}
+                className="!fmtm-rounded-lg"
+              />
+            ))}
+          </div>
+        ) : submissionPhotos?.length > 0 ? (
+          <div className="fmtm-bg-white fmtm-rounded-xl fmtm-p-6">
+            <p className="fmtm-text-base fmtm-font-bold fmtm-text-[#555] fmtm-mb-2">Images</p>
+            <ImageSlider images={submissionPhotos || []} />
+          </div>
+        ) : null}
       </div>
     </>
   );
