874abced49a8f33e0d2337cdd524bcfe06a0f0ed
NSUWAL123 <suwalnishit@gmail.com>
2024-11-12 10:54:25 +0545

    fix(comment): replace dynamic data with static

--- src/mapper/src/lib/components/more/comment.svelte
+++ src/mapper/src/lib/components/more/comment.svelte
@@ -1,6 +1,16 @@
-<script>
+<script lang="ts">
 	import Editor from '$lib/components/editor/editor.svelte';
 	import CommentSkeleton from '$lib/components/more/skeleton/comment.svelte';
+	import { commentTask } from '$lib/db/events';
+	import { getTaskStore } from '$store/tasks.svelte.ts';
+
+	interface Props {comments: any, projectId: any}
+
+	let { comments, projectId }: Props = $props();
+
+	let currentComment: string = $state('');
+
+	const taskStore = getTaskStore();
 </script>
 
 <div class="h-[calc(100%-2.25rem)] sm:h-[calc(100%-2.6rem)]">
@@ -10,7 +20,7 @@
 				<CommentSkeleton />
 			{/each}
 		{:else}
-			{#each Array.from({ length: 5 }) as _, index}
+			{#each comments as comment}
 				<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md">
 					<div class="flex gap-4 px-3">
 						<hot-icon
@@ -19,18 +29,25 @@
 							class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
 						></hot-icon>
 						<div class="flex flex-col gap-1 flex-1">
-							<p class="font-semibold">Localadmin</p>
+							<p class="font-semibold capitalize">{comment?.username}</p>
 							<div class="flex items-center justify-between">
-								<p class="text-[#484848] text-sm">#2</p>
+								<p class="text-[#484848] text-sm">#{comment?.task_id}</p>
 								<div class="flex items-center gap-2">
 									<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"
 									></hot-icon>
-									<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
+									<p class="text-[#484848] text-sm">
+										<span>
+											{comment?.created_at?.split(' ')[0]}
+										</span>
+										<span>
+											{comment?.created_at?.split(' ')[1]?.split('.')[0]}
+										</span>
+									</p>
 								</div>
 							</div>
 						</div>
 					</div>
-					<Editor editable={false} content={'<p>This is a comment</p>'} />
+					<Editor editable={false} content={comment?.comment} />
 				</div>
 			{/each}
 		{/if}
@@ -40,16 +57,22 @@
 		<Editor
 			editable={true}
 			content=""
-			setEditorHtmlContent={(editorText) => {
-				// to-do: store state to post comment
+			setEditorHtmlContent={(editorText: string) => {
+				currentComment = editorText;
 			}}
 		/>
 		<div class="w-full flex justify-end my-2 gap-2">
-			<sl-button variant="default" size="small" class="secondary col-span-2 sm:col-span-1"
+			<sl-button onclick={() => {}} onkeydown={() => {}} role='button' tabindex="0" variant="default" size="small" class="secondary col-span-2 sm:col-span-1"
 				><span class="font-barlow-medium text-sm">CLEAR</span></sl-button
 			>
-			<sl-button variant="default" size="small" class="primary col-span-2 sm:col-span-1"
-				><span class="font-barlow-medium text-sm">COMMENT</span></sl-button
+			<sl-button
+				variant="default"
+				size="small"
+				class="primary col-span-2 sm:col-span-1"
+				onclick={() => {commentTask(projectId, taskStore.selectedTaskId, currentComment)}} 
+				onkeydown={() => {}} 
+				role='button' tabindex="0"><span class="font-barlow-medium text-sm">COMMENT</span></sl-button
+				
 			>
 		</div>
 	</div>
