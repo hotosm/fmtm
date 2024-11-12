5fffad422328925cb063a97dd042422b59ee7fa3
NSUWAL123 <suwalnishit@gmail.com>
2024-11-12 10:54:01 +0545

    fix(activities): replace dynamic data with static

--- src/mapper/src/lib/components/more/activities.svelte
+++ src/mapper/src/lib/components/more/activities.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import ActivitiesSkeleton from '$lib/components/more/skeleton/activities.svelte';
+
+	interface Props {
+		taskEvents: any;
+	}
+
+	let { taskEvents }: Props = $props();
 </script>
 
 <div class="overflow-y-scroll overflow-x-hidden flex flex-col gap-2 pb-2">
@@ -8,7 +14,7 @@
 			<ActivitiesSkeleton />
 		{/each}
 	{:else}
-		{#each Array.from({ length: 5 }) as _, index}
+		{#each taskEvents as event}
 			<div class="flex flex-col gap-2 py-3 bg-[#F6F5F5] rounded-md mr-1">
 				<div class="flex gap-4 px-3">
 					<hot-icon
@@ -17,19 +23,26 @@
 						class="!text-[1.7rem] text-red-600 cursor-pointer duration-200 rounded-full p-[2px] bg-white"
 					></hot-icon>
 					<div class="flex flex-col gap-1 flex-1">
-						<p class="font-semibold">Localadmin</p>
+						<p class="font-semibold capitalize">{event?.username}</p>
 						<div class="flex items-center justify-between">
-							<p class="text-[#484848] text-sm">#2</p>
+							<p class="text-[#484848] text-sm">#{event?.task_id}</p>
 							<div class="flex items-center gap-2">
 								<hot-icon name="clock-history" class="!text-[1rem] text-red-600 cursor-pointer duration-200"></hot-icon>
-								<p class="text-[#484848] text-sm">2024-10-21 11:42</p>
+								<p class="text-[#484848] text-sm">
+									<span>
+										{event?.created_at?.split(' ')[0]}
+									</span>
+									<span>
+										{event?.created_at?.split(' ')[1]?.split('.')[0]}
+									</span>
+								</p>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="px-3 flex items-center fmtm-justify-between gap-2">
 					<p class="font-normal text-[#484848] flex-1">
-						<span class="">svcfmtm</span> updated status to <span>MAPPED</span>
+						<span class="capitalize">{event?.username}</span> updated status to <span>{event?.state}</span>
 					</p>
 					<hot-icon name="map" class="!text-[1rem] text-[#484848] hover:text-red-600 cursor-pointer duration-200"
 					></hot-icon>
