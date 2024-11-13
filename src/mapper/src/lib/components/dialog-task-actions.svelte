042b458e16c17f38769fea10cdbc7a9a64d85a94
NSUWAL123 <suwalnishit@gmail.com>
2024-11-13 13:59:56 +0545

    feat(dialog-task-actions): mapNewFeature btn add to open ODK

--- src/mapper/src/lib/components/dialog-task-actions.svelte
+++ src/mapper/src/lib/components/dialog-task-actions.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
 	import { mapTask, finishTask, resetTask } from '$lib/db/events';
+	import type { ProjectData } from '$lib/types';
 	import { getTaskStore } from '$store/tasks.svelte.ts';
+	import { getAlertStore } from '$store/common.svelte.ts';
 
 	type Props = {
 		isTaskActionModalOpen: boolean;
 		toggleTaskActionModal: (value: boolean) => void;
 		selectedTab: string;
-		projectId: number;
+		projectData: ProjectData;
 	};
 
 	const taskStore = getTaskStore();
-	let { isTaskActionModalOpen, toggleTaskActionModal, selectedTab, projectId }: Props = $props();
+	const alertStore = getAlertStore();
+	let { isTaskActionModalOpen, toggleTaskActionModal, selectedTab, projectData }: Props = $props();
+
+	function mapNewFeature() {
+		const xformId = projectData?.odk_form_id;
+		if (!xformId) {
+			return;
+		}
+
+		const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+		if (isMobile) {
+			document.location.href = `odkcollect://form/${xformId}`;
+		} else {
+			alertStore.setAlert({
+				variant: 'warning',
+				message: 'Requires a mobile phone with ODK Collect.',
+			});
+		}
+	}
 </script>
 
 {#if taskStore.selectedTaskId && selectedTab === 'map' && isTaskActionModalOpen && (taskStore.selectedTaskState === 'UNLOCKED_TO_MAP' || taskStore.selectedTaskState === 'LOCKED_FOR_MAPPING')}
@@ -18,8 +39,7 @@
 		<div
 			class="bg-white w-full font-barlow-regular md:max-w-[580px] pointer-events-auto px-4 py-3 sm:py-4 rounded-t-3xl"
 		>
-			<div class="flex justify-between items-center">
-				<p class="text-[#333] text-xl font-barlow-semibold">Task #{taskStore.selectedTaskId}</p>
+			<div class="flex justify-end">
 				<hot-icon
 					name="close"
 					class="!text-[1.5rem] text-[#52525B] cursor-pointer hover:text-red-600 duration-200"
@@ -32,6 +52,28 @@
 					role="button"
 					tabindex="0"
 				></hot-icon>
+			</div>
+			<div class="flex justify-between items-center">
+				<p class="text-[#333] text-xl font-barlow-semibold">Task #{taskStore.selectedTaskId}</p>
+				<div
+					onclick={() => {
+						mapNewFeature();
+					}}
+					onkeydown={(e: KeyboardEvent) => {
+						if (e.key === 'Enter') {
+							mapNewFeature();
+						}
+					}}
+					role="button"
+					tabindex="0"
+					class="flex items-center gap-1 cursor-pointer group"
+				>
+					<hot-icon
+						name="new-window"
+						class="!text-[1.25rem] duration-200 text-[#333333] font-light group-hover:text-black"
+					></hot-icon>
+					<p class="uppercase text-[0.813rem] text-red-600 group-hover:text-red-700">map new feature</p>
+				</div>
 			</div>
 
 			{#if taskStore.selectedTaskState === 'UNLOCKED_TO_MAP'}
@@ -57,10 +99,10 @@
 						variant="default"
 						size="small"
 						class="primary"
-						onclick={() => mapTask(projectId, taskStore.selectedTaskId)}
+						onclick={() => mapTask(projectData?.id, taskStore.selectedTaskId)}
 						onkeydown={(e: KeyboardEvent) => {
 							if (e.key === 'Enter') {
-								mapTask(projectId, taskStore.selectedTaskId);
+								mapTask(projectData?.id, taskStore.selectedTaskId);
 							}
 						}}
 						role="button"
@@ -75,14 +117,14 @@
 				<p class="my-4 sm:my-6">Task #{taskStore.selectedTaskId} has been locked. Is the task completely mapped?</p>
 				<div class="grid grid-cols-2 sm:grid-cols-3 gap-2">
 					<sl-button
-						onclick={() => resetTask(projectId, taskStore.selectedTaskId)}
+						onclick={() => resetTask(projectData?.id, taskStore.selectedTaskId)}
 						variant="default"
 						outline
 						size="small"
 						class="secondary"
 						onkeydown={(e: KeyboardEvent) => {
 							if (e.key === 'Enter') {
-								resetTask(projectId, taskStore.selectedTaskId);
+								resetTask(projectData?.id, taskStore.selectedTaskId);
 							}
 						}}
 						role="button"
@@ -96,13 +138,13 @@
 						<span class="font-barlow-medium text-sm">CANCEL MAPPING</span>
 					</sl-button>
 					<sl-button
-						onclick={() => finishTask(projectId, taskStore.selectedTaskId)}
+						onclick={() => finishTask(projectData?.id, taskStore.selectedTaskId)}
 						variant="default"
 						size="small"
 						class="green"
 						onkeydown={(e: KeyboardEvent) => {
 							if (e.key === 'Enter') {
-								finishTask(projectId, taskStore.selectedTaskId);
+								finishTask(projectData?.id, taskStore.selectedTaskId);
 							}
 						}}
 						role="button"
