e091db6c8219a501a446c7d1c5af4df3666b86ff
NSUWAL123 <suwalnishit@gmail.com>
2024-11-18 13:22:56 +0545

    fix(baseLayers): remove duplicate osm layer

--- src/mapper/src/constants/baseLayers.ts
+++ src/mapper/src/constants/baseLayers.ts
@@ -51,33 +51,6 @@ let esriStyle = {
 	],
 };
 
-let osm = {
-	id: 'OSM',
-	version: 8,
-	name: 'OSM',
-	sources: {
-		osm: {
-			type: 'raster',
-			tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
-			// minzoom: 0,
-			// maxzoom: 19,
-			tileSize: 256,
-			attribution:
-				'Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>, under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>',
-		},
-	},
-	layers: [
-		{
-			id: 'osm',
-			type: 'raster',
-			source: 'osm',
-			layout: {
-				visibility: 'visible',
-			},
-		},
-	],
-};
-
 let satellite = {
 	id: 'Satellite',
 	version: 8,
@@ -102,4 +75,4 @@ let satellite = {
 	],
 };
 
-export const baseLayers = [stamenStyle, esriStyle, osm, satellite];
+export const baseLayers = [stamenStyle, esriStyle, satellite];
