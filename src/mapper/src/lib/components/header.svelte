bd07a722d5eaf64932bae410e203806b2ab4a65c
NSUWAL123 <suwalnishit@gmail.com>
2024-11-20 18:01:07 +0545

    fix(header): header component ui slice

---
+++ src/mapper/src/lib/components/header.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import HotLogo from '$assets/images/hot-logo.svg';
+	import HotLogoText from '$assets/images/hot-logo-text.svg';
+	import Login from '$lib/components/login.svelte';
+	import { getLoginStore } from '$store/login.svelte.ts';
+
+	const loginStore = getLoginStore();
+</script>
+
+<div class="p-3 flex items-center justify-between">
+	<div class="flex items-center gap-1">
+		<img src={HotLogo} alt="hot-logo" />
+		<img src={HotLogoText} alt="hot-logo" />
+	</div>
+	<div class="flex items-center gap-4">
+		<sl-button
+			class="hover:bg-gray-50 rounded"
+			variant="text"
+			size="small"
+			onclick={() => {
+				loginStore.toggleLoginModal(true);
+			}}
+			onkeydown={(e: KeyboardEvent) => {
+				if (e.key === 'Enter') {
+					loginStore.toggleLoginModal(true);
+				}
+			}}
+			role="button"
+			tabindex="0"
+		>
+			<span class="font-barlow-medium text-base">SIGN IN</span>
+		</sl-button>
+		<hot-icon
+			name="list"
+			class="!text-[1.8rem] text-[#52525B] leading-0 cursor-pointer hover:text-red-600 duration-200"
+			onclick={() => {}}
+			onkeydown={() => {}}
+			role="button"
+			tabindex="0"
+		></hot-icon>
+	</div>
+</div>
+<Login />
