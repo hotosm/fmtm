347f368c76e676d110b97723824e9d43f50aeecc
spwoodcock <sam.woodcock@protonmail.com>
2024-11-27 18:02:44 +0000

    fix: allow providing manual thumbnail for base layers

--- src/mapper/src/lib/components/map/layer-switcher.svelte
+++ src/mapper/src/lib/components/map/layer-switcher.svelte
@@ -84,7 +84,7 @@ map = new Map({
 	 * Process the style to add metadata and return it.
 	 */
 	function processStyle(style: maplibregl.StyleSpecification): MapLibreStylePlusMetadata {
-		const thumbnailUrl = getRasterThumbnailUrl(style);
+		const thumbnailUrl = style?.metadata?.thumbnail || getRasterThumbnailUrl(style);
 		return {
 			...style,
 			metadata: {
