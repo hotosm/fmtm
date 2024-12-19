f4217ca5d362a88a44c7481f378a3365949b51d4
NSUWAL123 <suwalnishit@gmail.com>
2024-12-19 13:38:01 +0545

    fix(header): update shoelace component reference types

--- src/mapper/src/lib/components/header.svelte
+++ src/mapper/src/lib/components/header.svelte
@@ -8,20 +8,23 @@
 	import { getAlertStore } from '$store/common.svelte';
 	import { getProjectSetupStepStore } from '$store/common.svelte.ts';
 	import { projectSetupStep as projectSetupStepEnum } from '$constants/enums.ts';
+	import type { SlDrawer, SlTooltip } from '@shoelace-style/shoelace';
 
-	let drawerRef: any = $state();
-	let drawerOpenButtonRef: any = $state();
+	let drawerRef: SlDrawer | undefined = $state();
+	let drawerOpenButtonRef: SlTooltip | undefined = $state();
 	const loginStore = getLoginStore();
 	const alertStore = getAlertStore();
 	const projectSetupStepStore = getProjectSetupStepStore();
 
-	let isFirstLoad = $derived(+projectSetupStepStore.projectSetupStep === projectSetupStepEnum['odk_project_load']);
+	let isFirstLoad = $derived(
+		+(projectSetupStepStore.projectSetupStep || 0) === projectSetupStepEnum['odk_project_load'],
+	);
 
 	const handleSignOut = async () => {
 		try {
 			await revokeCookies();
 			loginStore.signOut();
-			drawerRef.hide();
+			drawerRef?.hide();
 			// window.location.href = window.location.origin;
 		} catch (error) {
 			alertStore.setAlert({ variant: 'danger', message: 'Sign Out Failed' });
@@ -87,30 +90,30 @@
 				class="!text-[1.8rem] text-[#52525B] leading-0 cursor-pointer hover:text-red-600 duration-200"
 				style={isFirstLoad ? 'background-color: var(--sl-color-yellow-300);' : ''}
 				onclick={() => {
-					drawerRef.show();
+					drawerRef?.show();
 				}}
 				onkeydown={() => {
-					drawerRef.show();
+					drawerRef?.show();
 				}}
 				role="button"
 				tabindex="0"
 			></hot-icon>
-	  	{/snippet}
+		{/snippet}
 		<!-- add tooltip on first load -->
 		{#if isFirstLoad}
-		<hot-tooltip
-			bind:this={drawerOpenButtonRef}
-			content="First download the custom ODK Collect app here"
-			open={true}
-			placement="bottom"
-			onsl-after-hide={() => {
-				// Always keep tooltip open
-				drawerOpenButtonRef.show();
-			}}
-		>
-			{@render drawerOpenButton()}
-		</hot-tooltip>
-		<!-- else render with no tooltip -->
+			<hot-tooltip
+				bind:this={drawerOpenButtonRef}
+				content="First download the custom ODK Collect app here"
+				open={true}
+				placement="bottom"
+				onsl-after-hide={() => {
+					// Always keep tooltip open
+					drawerOpenButtonRef?.show();
+				}}
+			>
+				{@render drawerOpenButton()}
+			</hot-tooltip>
+			<!-- else render with no tooltip -->
 		{:else}
 			{@render drawerOpenButton()}
 		{/if}
