a48a9d357f78c8be69aca02a3f4ddea38688ca7b
Sujan Adhikari <sujanadh10@gmail.com>
2024-12-31 15:53:53 +0545

    feat: create geometrylog table to store bad and new geoms

---
+++ src/backend/migrations/002-create-geometry-log.sql
@@ -0,0 +1,19 @@
+-- ## Migration to create a table to store new and bad geometries.
+
+-- Start a transaction
+
+BEGIN;
+
+CREATE TABLE geometrylog (
+    id SERIAL PRIMARY KEY,
+    geom GEOMETRY NOT NULL,
+    status geomstatus,
+    project_id int,
+    task_id int
+);
+
+-- Indexes for efficient querying
+CREATE INDEX idx_geometrylog ON geometrylog USING gist (geom);
+
+-- Commit the transaction
+COMMIT;
