bff368337d4328f7c94a9cac57f991898ef57756
Sujan Adhikari <sujanadh10@gmail.com>
2025-01-02 12:55:29 +0545

    feat: add migration of geomstatus enum

--- src/backend/migrations/002-create-geometry-log.sql
+++ src/backend/migrations/002-create-geometry-log.sql
@@ -4,15 +4,20 @@
 
 BEGIN;
 
-CREATE TABLE geometrylog (
+CREATE TYPE public.geomstatus AS ENUM (
+    'BAD',
+    'NEW'
+);
+ALTER TYPE public.geomstatus OWNER TO fmtm;
+
+CREATE TABLE public.geometrylog (
     id SERIAL PRIMARY KEY,
     geom GEOMETRY NOT NULL,
     status geomstatus,
     project_id int,
     task_id int
 );
-
-ALTER TABLE geometrylog OWNER TO fmtm;
+ALTER TABLE public.geometrylog OWNER TO fmtm;
 
 -- Indexes for efficient querying
 CREATE INDEX idx_geometrylog ON geometrylog USING gist (geom);
