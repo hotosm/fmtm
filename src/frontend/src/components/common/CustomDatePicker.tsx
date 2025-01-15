c2a4a074c7aed39ae687da3161d2f176269b1fee
NSUWAL123 <suwalnishit@gmail.com>
2025-01-15 17:09:25 +0545

    fix(customDatePicker): add start and end date range

--- src/frontend/src/components/common/CustomDatePicker.tsx
+++ src/frontend/src/components/common/CustomDatePicker.tsx
@@ -5,19 +5,29 @@ import 'react-datepicker/dist/react-datepicker.css';
 type CustomDatePickerType = {
   title: string;
   className: string;
-  selectedDate: string | null;
-  setSelectedDate: (date: Date) => void;
+  setStartDate: (date: Date | null) => void;
+  setEndDate: (date: Date | null) => void;
+  startDate: Date | null;
+  endDate: Date | null;
 };
 
-const CustomDatePicker = ({ title, className, selectedDate, setSelectedDate }: CustomDatePickerType) => {
+const CustomDatePicker = ({ title, className, setStartDate, setEndDate, startDate, endDate }: CustomDatePickerType) => {
+  const onChange = (dates: [Date | null, Date | null]) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
   return (
     <div className="fmtm-z-[10000] fmtm-w-full">
       {title && (
         <p className={`fmtm-text-[1rem] fmtm-mb-2 fmtm-font-semibold !fmtm-bg-transparent ${className}`}>{title}</p>
       )}
       <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
         className="fmtm-outline-none fmtm-border-[1px] fmtm-border-gray-300 fmtm-h-[2rem] fmtm-w-full fmtm-z-50 fmtm-px-2 fmtm-text-base fmtm-pt-1 hover"
         placeholderText="YYYY/MM/DD"
         dateFormat="yyyy/MM/dd"
