d22f08c9af60137007edf1b4b4a8e664f7813fab
NSUWAL123 <suwalnishit@gmail.com>
2025-01-21 17:40:06 +0545

    fix(legend/layer-switcher): update stylings

--- src/mapper/src/lib/components/map/legend.svelte
+++ src/mapper/src/lib/components/map/legend.svelte
@@ -34,11 +34,11 @@
 		<hot-icon
 			style="border: 1px solid #D7D7D7;"
 			name="legend-toggle"
-			class={`!text-[1.7rem] text-[#333333] bg-white p-2 rounded-full group-hover:text-red-600 duration-200 ${isOpen && 'text-red-600'}`}
+			class={`!text-[1.7rem] text-[#333333] bg-white p-1 rounded-full group-hover:text-red-600 duration-200 ${isOpen && 'text-red-600'}`}
 		></hot-icon>
 	</div>
 	<div
-		class={`absolute bottom-0 right-14 bg-white rounded-md p-4 duration-200 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'} overflow-hidden flex flex-col gap-2`}
+		class={`absolute bottom-0 right-11 bg-white rounded-md p-4 duration-200 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'} overflow-hidden flex flex-col gap-2`}
 	>
 		<p class="font-semibold leading-0 text-lg mb-3">Legend</p>
 		{#each taskStatuses as taskStatus}
