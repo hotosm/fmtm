798e1dcf728cbd149f9eacedd62aa43623d91a3d
NSUWAL123 <suwalnishit@gmail.com>
2025-01-24 13:23:47 +0545

    feat(select2): show selected values in capsules on multi-select case

--- src/frontend/src/components/common/Select2.tsx
+++ src/frontend/src/components/common/Select2.tsx
@@ -106,126 +106,146 @@ function Select2({
   }, [options]);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild ref={triggerRef}>
-        <button
-          disabled={disabled}
-          className={cn(
-            'fmtm-group fmtm-flex fmtm-items-center fmtm-justify-between fmtm-gap-2 fmtm-border-[1px] fmtm-border-gray-300 fmtm-h-[2.3rem] fmtm-rounded-md disabled:!fmtm-cursor-not-allowed fmtm-px-3',
-            className,
-          )}
-          onClick={() => setOpen(true)}
-        >
-          <div className="fmtm-flex fmtm-items-center fmtm-justify-center fmtm-gap-2">
-            {multiple ? (
-              <div className="fmtm-flex fmtm-flex-wrap">
-                {Array.isArray(value) && value.length > 0 ? (
-                  value.length === 1 ? (
-                    <span className="fmtm-body-sm fmtm-line-clamp-1 fmtm-text-start">
-                      {options?.find((option) => option[choose as keyof selectOptionsType] === value[0])?.label ||
-                        '1 Selected'}
-                    </span>
+    <>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild ref={triggerRef}>
+          <button
+            disabled={disabled}
+            className={cn(
+              'fmtm-group fmtm-flex fmtm-items-center fmtm-justify-between fmtm-gap-2 fmtm-border-[1px] fmtm-border-gray-300 fmtm-h-[2.3rem] fmtm-rounded-md disabled:!fmtm-cursor-not-allowed fmtm-px-3',
+              className,
+            )}
+            onClick={() => setOpen(true)}
+          >
+            <div className="fmtm-flex fmtm-items-center fmtm-justify-center fmtm-gap-2">
+              {multiple ? (
+                <div className="fmtm-flex fmtm-flex-wrap">
+                  {Array.isArray(value) && value.length > 0 ? (
+                    value.length === 1 ? (
+                      <span className="fmtm-body-sm fmtm-line-clamp-1 fmtm-text-start">
+                        {options?.find((option) => option[choose as keyof selectOptionsType] === value[0])?.label ||
+                          '1 Selected'}
+                      </span>
+                    ) : (
+                      <span className="fmtm-line-clamp-1 fmtm-text-start fmtm-font-medium">
+                        {value.length} Selected
+                      </span>
+                    )
                   ) : (
-                    <span className="fmtm-line-clamp-1 fmtm-text-start fmtm-font-medium">{value.length} Selected</span>
-                  )
-                ) : (
-                  <p
-                    className={cn(
-                      'fmtm-text-base fmtm-line-clamp-1  fmtm-text-start fmtm-text-grey-500',
-                      placeholderClassName,
-                    )}
-                  >
-                    {placeholder || 'Choose'}
-                  </p>
-                )}
-              </div>
-            ) : (
-              <>
-                {value ? (
-                  <p className="fmtm-text-base fmtm-line-clamp-1 fmtm-text-start">
-                    {options.find((option) => {
-                      return option[choose as keyof selectOptionsType] === value;
-                    })?.label || 'No Name Found'}
-                  </p>
-                ) : (
-                  <p
-                    className={cn(
-                      'fmtm-text-base fmtm-line-clamp-1 fmtm-text-start fmtm-text-grey-500',
-                      placeholderClassName,
-                    )}
-                  >
-                    {placeholder || 'Choose'}
-                  </p>
-                )}
-              </>
-            )}
-          </div>
-          <AssetModules.ExpandMoreIcon className="fmtm-text-grey-600" />
-        </button>
-      </PopoverTrigger>
-      <PopoverContent className="fmtm-bg-white fmtm-p-[0px]" style={{ width: `${dropDownWidth}px`, ...style }}>
-        {enableSearchbar ? (
-          <Searchbar
-            value={searchTextData}
-            onChange={handleChangeData}
-            isSmall
-            className=" !fmtm-py-3 fmtm-border-t-0 fmtm-border-x-0 fmtm-rounded-b-none focus:!fmtm-border-grey-300"
-            wrapperStyle="!fmtm-h-auto"
-          />
-        ) : null}
-        <div className="scrollbar search-scrollbar fmtm-block fmtm-max-h-[10rem] fmtm-overflow-y-auto">
-          <Command className="fmtm-m-0 fmtm-p-0">
-            {isLoading && (
-              <div className="fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-px-4 fmtm-py-2">
-                <LoaderCircle className="fmtm-w-5 fmtm-animate-spin" /> <p>Loading ...</p>
-              </div>
-            )}
-            <CommandGroup className="">
-              {filteredOptionsData.length ? (
-                filteredOptionsData?.map((option) => (
-                  <CommandItem
-                    key={option.value?.toString()}
-                    onSelect={() => {
-                      handleSelect(option[choose as keyof selectPropType]);
-                    }}
-                    className="fmtm-flex fmtm-items-center fmtm-gap-[0.15rem] hover:fmtm-bg-red-50 fmtm-duration-150"
-                  >
-                    {!checkBox ? (
-                      <AssetModules.DoneIcon
-                        className={`!fmtm-text-[20px] fmtm-text-grey-600 ${
-                          !multiple
-                            ? value === option[choose as keyof selectPropType]
-                              ? 'fmtm-opacity-100'
-                              : 'fmtm-opacity-0'
-                            : Array.isArray(value) && value.includes(option[choose as keyof selectPropType])
-                              ? 'fmtm-opacity-100'
-                              : 'fmtm-opacity-0'
-                        }`}
-                      />
-                    ) : !multiple ? (
-                      value === option[choose as keyof selectPropType] ? (
+                    <p
+                      className={cn(
+                        'fmtm-text-base fmtm-line-clamp-1  fmtm-text-start fmtm-text-grey-500',
+                        placeholderClassName,
+                      )}
+                    >
+                      {placeholder || 'Choose'}
+                    </p>
+                  )}
+                </div>
+              ) : (
+                <>
+                  {value ? (
+                    <p className="fmtm-text-base fmtm-line-clamp-1 fmtm-text-start">
+                      {options.find((option) => {
+                        return option[choose as keyof selectOptionsType] === value;
+                      })?.label || 'No Name Found'}
+                    </p>
+                  ) : (
+                    <p
+                      className={cn(
+                        'fmtm-text-base fmtm-line-clamp-1 fmtm-text-start fmtm-text-grey-500',
+                        placeholderClassName,
+                      )}
+                    >
+                      {placeholder || 'Choose'}
+                    </p>
+                  )}
+                </>
+              )}
+            </div>
+            <AssetModules.ExpandMoreIcon className="fmtm-text-grey-600" />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent className="fmtm-bg-white fmtm-p-[0px]" style={{ width: `${dropDownWidth}px`, ...style }}>
+          {enableSearchbar ? (
+            <Searchbar
+              value={searchTextData}
+              onChange={handleChangeData}
+              isSmall
+              className=" !fmtm-py-3 fmtm-border-t-0 fmtm-border-x-0 fmtm-rounded-b-none focus:!fmtm-border-grey-300"
+              wrapperStyle="!fmtm-h-auto"
+            />
+          ) : null}
+          <div className="scrollbar search-scrollbar fmtm-block fmtm-max-h-[10rem] fmtm-overflow-y-auto">
+            <Command className="fmtm-m-0 fmtm-p-0">
+              {isLoading && (
+                <div className="fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-px-4 fmtm-py-2">
+                  <LoaderCircle className="fmtm-w-5 fmtm-animate-spin" /> <p>Loading ...</p>
+                </div>
+              )}
+              <CommandGroup className="">
+                {filteredOptionsData.length ? (
+                  filteredOptionsData?.map((option) => (
+                    <CommandItem
+                      key={option.value?.toString()}
+                      onSelect={() => {
+                        handleSelect(option[choose as keyof selectPropType]);
+                      }}
+                      className="fmtm-flex fmtm-items-center fmtm-gap-[0.15rem] hover:fmtm-bg-red-50 fmtm-duration-150"
+                    >
+                      {!checkBox ? (
+                        <AssetModules.DoneIcon
+                          className={`!fmtm-text-[20px] fmtm-text-grey-600 ${
+                            !multiple
+                              ? value === option[choose as keyof selectPropType]
+                                ? 'fmtm-opacity-100'
+                                : 'fmtm-opacity-0'
+                              : Array.isArray(value) && value.includes(option[choose as keyof selectPropType])
+                                ? 'fmtm-opacity-100'
+                                : 'fmtm-opacity-0'
+                          }`}
+                        />
+                      ) : !multiple ? (
+                        value === option[choose as keyof selectPropType] ? (
+                          <AssetModules.CheckBoxIcon className="!fmtm-text-[20px] fmtm-text-grey-600" />
+                        ) : (
+                          <AssetModules.CheckBoxOutlineBlankIcon className="!fmtm-text-[20px] fmtm-text-grey-600" />
+                        )
+                      ) : Array.isArray(value) && value.includes(option[choose as keyof selectPropType]) ? (
                         <AssetModules.CheckBoxIcon className="!fmtm-text-[20px] fmtm-text-grey-600" />
                       ) : (
                         <AssetModules.CheckBoxOutlineBlankIcon className="!fmtm-text-[20px] fmtm-text-grey-600" />
-                      )
-                    ) : Array.isArray(value) && value.includes(option[choose as keyof selectPropType]) ? (
-                      <AssetModules.CheckBoxIcon className="!fmtm-text-[20px] fmtm-text-grey-600" />
-                    ) : (
-                      <AssetModules.CheckBoxOutlineBlankIcon className="!fmtm-text-[20px] fmtm-text-grey-600" />
-                    )}
-                    <span className="fmtm-text-base fmtm-text-grey-800">{option.label || '-'}</span>
-                  </CommandItem>
-                ))
-              ) : (
-                <div className="fmtm-body-sm fmtm-line-clamp-1 fmtm-flex fmtm-h-[4.25rem] fmtm-items-center fmtm-justify-center fmtm-text-start">
-                  No Data Found.
-                </div>
-              )}
-            </CommandGroup>
-          </Command>
+                      )}
+                      <span className="fmtm-text-base fmtm-text-grey-800">{option.label || '-'}</span>
+                    </CommandItem>
+                  ))
+                ) : (
+                  <div className="fmtm-body-sm fmtm-line-clamp-1 fmtm-flex fmtm-h-[4.25rem] fmtm-items-center fmtm-justify-center fmtm-text-start">
+                    No Data Found.
+                  </div>
+                )}
+              </CommandGroup>
+            </Command>
+          </div>
+        </PopoverContent>
+      </Popover>
+      {multiple && Array.isArray(value) && (
+        <div className="fmtm-flex fmtm-flex-wrap fmtm-gap-2 fmtm-pt-2">
+          {value?.map((val) => (
+            <div
+              key={val}
+              className="fmtm-bg-[#F5F5F5] fmtm-rounded-full fmtm-px-2 fmtm-py-1 fmtm-border-[1px] fmtm-border-[#D7D7D7] fmtm-text-[#484848] fmtm-flex fmtm-items-center fmtm-gap-1"
+            >
+              <p>{options.find((option) => option.value === val)?.label}</p>
+              <AssetModules.CloseIcon
+                onClick={() => handleSelect(val)}
+                className="!fmtm-text-[1.125rem] fmtm-cursor-pointer hover:fmtm-text-red-600"
+              />
+            </div>
+          ))}
         </div>
-      </PopoverContent>
-    </Popover>
+      )}
+    </>
   );
 }
 
