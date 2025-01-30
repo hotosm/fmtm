cf658033a26d011ac37b8e685f67d8bd85e80831
NSUWAL123 <suwalnishit@gmail.com>
2025-01-30 10:42:11 +0545

    fix(user): update ts type

--- src/frontend/src/api/User.ts
+++ src/frontend/src/api/User.ts
@@ -2,14 +2,17 @@ import axios, { AxiosResponse } from 'axios';
 import { AppDispatch } from '@/store/Store';
 import { UserActions } from '@/store/slices/UserSlice';
 import { userType } from '@/models/user/userModel';
+import { paginationType } from '@/store/types/ICommon';
 
-export const GetUserListService = (url: string) => {
+export const GetUserListService = (url: string, params: { page: number; results_per_page: number; search: string }) => {
   return async (dispatch: AppDispatch) => {
     dispatch(UserActions.SetUserListLoading(true));
 
     const getUserList = async (url: string) => {
       try {
-        const response: AxiosResponse<userType[]> = await axios.get(url);
+        const response: AxiosResponse<{ results: userType[]; pagination: paginationType }> = await axios.get(url, {
+          params,
+        });
         dispatch(UserActions.SetUserList(response.data));
       } catch (error) {
       } finally {
