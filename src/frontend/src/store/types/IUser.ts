55b11f758ce951207cb9a404e5c3f2108daddfe1
NSUWAL123 <suwalnishit@gmail.com>
2025-01-31 15:38:41 +0545

    feat(user): slice add for user list select

--- src/frontend/src/store/types/IUser.ts
+++ src/frontend/src/store/types/IUser.ts
@@ -5,4 +5,6 @@ export type UserStateTypes = {
   userList: { results: userType[]; pagination: paginationType };
   userListLoading: boolean;
   updateUserRoleLoading: boolean;
+  userListForSelect: userType[];
+  userListForSelectLoading: boolean;
 };
