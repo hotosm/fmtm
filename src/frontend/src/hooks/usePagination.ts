9d1c28bf08449f52d664c47335d1d9f6cd7312c6
NSUWAL123 <suwalnishit@gmail.com>
2025-02-12 12:36:12 +0545

    fix(usePagination): change directory

--- src/frontend/src/hooks/usePagination.ts
+++ src/frontend/src/hooks/usePagination.ts
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 interface usePaginationIProps {
   totalCount: number;
   pageSize: number;
-  siblingCount: number;
+  siblingCount?: number;
   currentPage: number;
 }
 
