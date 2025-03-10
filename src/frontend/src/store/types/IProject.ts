dccd505041f77075ae37ae295e87266646cdfc25
NSUWAL123 <suwalnishit@gmail.com>
2025-03-10 16:46:29 +0545

    fix(IProject): update mobileFooterSelection type

--- src/frontend/src/store/types/IProject.ts
+++ src/frontend/src/store/types/IProject.ts
@@ -21,7 +21,7 @@ export type ProjectStateTypes = {
   downloadDataExtractLoading: boolean;
   taskModalStatus: boolean;
   toggleGenerateMbTilesModal: boolean;
-  mobileFooterSelection: string;
+  mobileFooterSelection: '' | 'projectInfo' | 'activities' | 'comment';
   projectDetailsLoading: boolean;
   projectDashboardDetail: projectDashboardDetailTypes | null;
   entityOsmMap: EntityOsmMap[];
