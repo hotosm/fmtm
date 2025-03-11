b92402a89e518d8f62db00b638266fd6dadfcc77
NSUWAL123 <suwalnishit@gmail.com>
2025-03-11 17:57:31 +0545

    feat(projectDetails): resizable project description and map section

--- src/frontend/src/views/ProjectDetails.tsx
+++ src/frontend/src/views/ProjectDetails.tsx
@@ -4,7 +4,6 @@ import '../styles/home.scss';
 import ActivitiesPanel from '@/components/ProjectDetails/ActivitiesPanel';
 import { ProjectById, GetEntityStatusList, GetGeometryLog } from '@/api/Project';
 import { ProjectActions } from '@/store/slices/ProjectSlice';
-import CoreModules from '@/shared/CoreModules';
 import AssetModules from '@/shared/AssetModules';
 import GenerateBasemap from '@/components/GenerateBasemap';
 import TaskSelectionPopup from '@/components/ProjectDetails/TaskSelectionPopup';
@@ -14,7 +13,7 @@ import MobileFooter from '@/components/ProjectDetails/MobileFooter';
 import MobileActivitiesContents from '@/components/ProjectDetails/MobileActivitiesContents';
 import BottomSheet from '@/components/common/BottomSheet';
 import MobileProjectInfoContent from '@/components/ProjectDetails/MobileProjectInfoContent';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import ProjectOptions from '@/components/ProjectDetails/ProjectOptions';
 import Button from '@/components/common/Button';
 import ProjectInfo from '@/components/ProjectDetails/ProjectInfo';
@@ -24,6 +23,8 @@ import Comments from '@/components/ProjectDetails/Comments';
 import Instructions from '@/components/ProjectDetails/Instructions';
 import useDocumentTitle from '@/utilfunctions/useDocumentTitle';
 import ProjectDetailsMap from '@/components/ProjectDetails/ProjectDetailsMap';
+import FolderManagedIcon from '@/assets/icons/folderManagedIcon.svg';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/RadixComponents/Resizable';
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
@@ -156,104 +157,108 @@ const ProjectDetails = () => {
   };
 
   return (
-    <div className="fmtm-bg-[#f5f5f5] !fmtm-h-[100dvh] sm:!fmtm-h-full">
+    <div className="fmtm-bg-grey-100 !fmtm-h-[100dvh] sm:!fmtm-h-full md:fmtm-overflow-hidden">
       {/* Customized Modal For Generate Tiles */}
       <GenerateBasemap projectInfo={projectInfo} />
 
-      <div className="fmtm-flex fmtm-h-full fmtm-gap-6">
-        <div className="fmtm-w-[22rem] fmtm-h-full fmtm-hidden md:fmtm-block">
-          <div className="fmtm-flex fmtm-justify-between fmtm-items-center fmtm-mb-4">
-            {projectDetailsLoading ? (
-              <div className="fmtm-flex fmtm-gap-1 fmtm-items-center">
-                <p className="fmtm-text-[#9B9999]">#</p>
-                <CoreModules.Skeleton className="!fmtm-w-[50px] fmtm-h-[20px]" />
-              </div>
-            ) : (
-              <p className="fmtm-text-lg fmtm-font-archivo fmtm-text-[#9B9999]">{`#${projectInfo.id}`}</p>
-            )}
-            <Button variant="secondary-red" onClick={() => navigate(`/manage/project/${params?.id}`)}>
-              MANAGE PROJECT <AssetModules.SettingsIcon />
-            </Button>
+      <div className="fmtm-h-full fmtm-w-full">
+        {/* upper div */}
+        <div className="fmtm-w-full fmtm-hidden md:fmtm-flex fmtm-items-center fmtm-justify-between fmtm-gap-2">
+          <div className="fmtm-flex fmtm-items-center">
+            <AssetModules.ChevronLeftIcon className="!fmtm-w-[1.125rem] fmtm-mx-1" />
+            <h5>{projectInfo.name}</h5>
           </div>
-          <div
-            className="fmtm-flex fmtm-flex-col fmtm-gap-4 fmtm-flex-auto"
-            style={{ height: `${selectedTab === 'comments' ? 'calc(100% - 63px)' : 'calc(100% - 103px)'}` }}
-          >
-            {projectDetailsLoading ? (
-              <CoreModules.Skeleton className="!fmtm-w-[250px] fmtm-h-[25px]" />
-            ) : (
-              <div className="fmtm-relative">
-                <p className="fmtm-text-xl fmtm-font-archivo fmtm-line-clamp-3 fmtm-mr-4" title={projectInfo.name}>
-                  {projectInfo.name}
-                </p>
-              </div>
-            )}
-            <div className="fmtm-w-full fmtm-h-1 fmtm-bg-white"></div>
-            <div className="fmtm-flex fmtm-w-full">
-              {tabList.map((tab) => (
-                <button
-                  key={tab.id}
-                  className={`fmtm-rounded-none fmtm-border-none fmtm-text-base fmtm-py-1 ${
-                    selectedTab === tab.id
-                      ? 'fmtm-bg-primaryRed fmtm-text-white hover:fmtm-bg-red-700'
-                      : 'fmtm-bg-white fmtm-text-[#706E6E] hover:fmtm-bg-grey-50'
-                  } ${(taskModalStatus && tab.id === 'project_info') || (!taskModalStatus && (tab.id === 'task_activity' || tab.id === 'comments')) ? 'fmtm-hidden' : ''}`}
-                  onClick={() => setSelectedTab(tab.id)}
-                >
-                  {tab.name}
-                </button>
-              ))}
-            </div>
-            {getTabContent(selectedTab)}
-          </div>
-          {selectedTab !== 'comments' && (
-            <div className="fmtm-flex fmtm-gap-4">
-              <Button
-                variant="secondary-red"
-                className="!fmtm-w-1/2"
-                onClick={() => navigate(`/project-submissions/${projectId}`)}
-              >
-                VIEW INFOGRAPHICS
-              </Button>
-              <div className="fmtm-relative fmtm-w-1/2" ref={divRef}>
-                <Button variant="secondary-red" className="fmtm-w-full" onClick={handleToggle}>
-                  DOWNLOAD
-                </Button>
-                <div
-                  className={`fmtm-flex fmtm-gap-4 fmtm-absolute fmtm-duration-200 fmtm-z-[1000] fmtm-bg-[#F5F5F5] fmtm-px-2 fmtm-py-[2px] fmtm-rounded ${
-                    toggle
-                      ? 'fmtm-left-0 fmtm-bottom-0 lg:fmtm-top-0'
-                      : '-fmtm-left-[65rem] fmtm-bottom-0 lg:fmtm-top-0'
-                  }`}
-                >
-                  <ProjectOptions projectName={projectInfo?.name as string} />
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-        {projectId && (
-          <div className="fmtm-relative sm:fmtm-static fmtm-flex-grow fmtm-h-full sm:fmtm-rounded-2xl fmtm-overflow-hidden">
-            <ProjectDetailsMap
-              setSelectedTaskArea={setSelectedTaskArea}
-              setSelectedTaskFeature={setSelectedTaskFeature}
-              setMap={setMap}
-            />
-            <div
-              className="fmtm-absolute fmtm-top-4 fmtm-left-4 fmtm-bg-white fmtm-rounded-full fmtm-p-1 hover:fmtm-bg-red-50 fmtm-duration-300 fmtm-border-[1px] md:fmtm-hidden fmtm-cursor-pointer"
-              onClick={() => navigate('/')}
+          <div className="fmtm-flex fmtm-items-center fmtm-gap-2">
+            <Button
+              variant="secondary-grey"
+              onClick={() => dispatch(ProjectActions.ToggleGenerateMbTilesModalStatus(true))}
             >
-              <AssetModules.ArrowBackIcon className="fmtm-text-grey-800" />
+              <AssetModules.BoltIcon className="!fmtm-text-[1.125rem]" />
+              Generate Basemap
+            </Button>
+            <div className="fmtm-relative fmtm-w-1/2" ref={divRef}>
+              <Button variant="secondary-grey" className="fmtm-w-full" onClick={handleToggle}>
+                <AssetModules.FileDownloadOutlinedIcon className="!fmtm-text-[1.125rem]" />
+                Download
+              </Button>
+              <div
+                className={`fmtm-flex fmtm-gap-4 fmtm-absolute fmtm-duration-200 fmtm-z-[1000] fmtm-bg-grey-100 fmtm-px-2 fmtm-py-[2px] fmtm-rounded ${
+                  toggle ? 'fmtm-right-0 fmtm-top-[3rem]' : '-fmtm-right-[15rem] fmtm-top-[3rem]'
+                }`}
+              >
+                <ProjectOptions projectName={projectInfo?.name as string} />
+              </div>
             </div>
-            {['projectInfo', 'activities', 'instructions', 'comment'].includes(mobileFooterSelection) && (
-              <BottomSheet
-                body={getMobileBottomSheetContent(mobileFooterSelection)}
-                onClose={() => dispatch(ProjectActions.SetMobileFooterSelection(''))}
-              />
-            )}
-            <MobileFooter />
           </div>
-        )}
+        </div>
+        <div className="fmtm-flex fmtm-h-[calc(100%-3rem)] fmtm-gap-6 fmtm-mt-0 sm:fmtm-mt-2">
+          <ResizablePanelGroup direction="horizontal" className="fmtm-gap-5">
+            <ResizablePanel className="fmtm-hidden md:fmtm-flex md:fmtm-min-w-[22rem]">
+              <div className="fmtm-w-full fmtm-h-full fmtm-hidden md:fmtm-flex fmtm-flex-col">
+                <div
+                  className="fmtm-flex fmtm-flex-col fmtm-gap-4 fmtm-flex-1"
+                  // style={{ height: `${selectedTab === 'comments' ? 'calc(100% - 63px)' : 'calc(100% - 103px)'}` }}
+                >
+                  <div className="fmtm-flex fmtm-border-b fmtm-border-grey-200 fmtm-gap-3">
+                    {tabList.map((tab) => (
+                      <button
+                        key={tab.id}
+                        className={`fmtm-body-md fmtm-rounded-none fmtm-border-b fmtm-py-1 fmtm-px-5 fmtm-duration-200 fmtm-w-fit fmtm-text-grey-900 ${
+                          selectedTab === tab.id ? 'fmtm-border-primaryRed fmtm-button' : 'fmtm-border-transparent'
+                        } ${(taskModalStatus && tab.id === 'project_info') || (!taskModalStatus && (tab.id === 'task_activity' || tab.id === 'comments')) ? 'fmtm-hidden' : ''}`}
+                        onClick={() => setSelectedTab(tab.id)}
+                      >
+                        {tab.name}
+                      </button>
+                    ))}
+                  </div>
+                  {getTabContent(selectedTab)}
+                </div>
+                {selectedTab !== 'comments' && (
+                  <div className="fmtm-flex fmtm-gap-[0.625rem]">
+                    <Link to={`/manage/project/${params?.id}`} className="!fmtm-w-1/2">
+                      <Button variant="secondary-grey" className="fmtm-w-full">
+                        <img src={FolderManagedIcon} alt="Manage Project" className="fmtm-h-5 fmtm-w-5" />
+                        Manage Project
+                      </Button>
+                    </Link>
+                    <Link to={`/project-submissions/${projectId}`} className="!fmtm-w-1/2">
+                      <Button variant="secondary-grey" className="fmtm-w-full">
+                        <AssetModules.BarChartOutlinedIcon className="!fmtm-text-[1.125rem]" />
+                        View Statistics
+                      </Button>
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </ResizablePanel>
+            <ResizableHandle withHandle className="fmtm-bg-grey-200 fmtm-hidden sm:fmtm-flex" />
+            <ResizablePanel className="md:fmtm-min-w-[22rem]">
+              {projectId && (
+                <div className="fmtm-relative sm:fmtm-static fmtm-flex-grow fmtm-h-full sm:fmtm-rounded-2xl fmtm-overflow-hidden">
+                  <ProjectDetailsMap
+                    setSelectedTaskArea={setSelectedTaskArea}
+                    setSelectedTaskFeature={setSelectedTaskFeature}
+                    setMap={setMap}
+                  />
+                  <div
+                    className="fmtm-absolute fmtm-top-4 fmtm-left-4 fmtm-bg-white fmtm-rounded-full fmtm-p-1 hover:fmtm-bg-red-50 fmtm-duration-300 fmtm-border-[1px] md:fmtm-hidden fmtm-cursor-pointer"
+                    onClick={() => navigate('/')}
+                  >
+                    <AssetModules.ArrowBackIcon className="fmtm-text-grey-800" />
+                  </div>
+                  {['projectInfo', 'activities', 'instructions', 'comment'].includes(mobileFooterSelection) && (
+                    <BottomSheet
+                      body={getMobileBottomSheetContent(mobileFooterSelection)}
+                      onClose={() => dispatch(ProjectActions.SetMobileFooterSelection(''))}
+                    />
+                  )}
+                  <MobileFooter />
+                </div>
+              )}
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </div>
       </div>
       {selectedTaskArea != undefined && selectedTaskFeature === undefined && selectedTask && (
         <TaskSelectionPopup
