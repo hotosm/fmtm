d0540a19558e265907f65b281215d5cde226f2dc
spwoodcock <sam.woodcock@protonmail.com>
2025-03-11 16:39:00 +0000

    fix(frontend): translations set on wrong frontend 😅

--- src/frontend/src/types/enums.ts
+++ src/frontend/src/types/enums.ts
@@ -1,5 +1,3 @@
-import { m } from '$translations/messages.js';
-
 export enum task_split_type {
   DIVIDE_ON_SQUARE = 'DIVIDE_ON_SQUARE',
   CHOOSE_AREA_AS_TASK = 'CHOOSE_AREA_AS_TASK',
@@ -27,11 +25,11 @@ export enum task_state {
 }
 
 export enum task_state_labels {
-  UNLOCKED_TO_MAP = m['task_states.UNLOCKED_TO_MAP'](),
-  LOCKED_FOR_MAPPING = m['task_states.LOCKED_FOR_MAPPING'](),
-  UNLOCKED_TO_VALIDATE = m['task_states.UNLOCKED_TO_VALIDATE'](),
-  LOCKED_FOR_VALIDATION = m['task_states.LOCKED_FOR_VALIDATION'](),
-  UNLOCKED_DONE = m['task_states.UNLOCKED_DONE'](),
+  UNLOCKED_TO_MAP = 'Ready',
+  LOCKED_FOR_MAPPING = 'Locked For Mapping',
+  UNLOCKED_TO_VALIDATE = 'Ready For Validation',
+  LOCKED_FOR_VALIDATION = 'Locked For Validation',
+  UNLOCKED_DONE = 'Validated',
 }
 
 export enum entity_state {
