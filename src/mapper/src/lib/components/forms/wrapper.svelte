6ba54dd3dd84a9547bb2279b4050f793e6d8c8e3
DanielJDufour <daniel.j.dufour@gmail.com>
2025-03-11 22:56:01 -0400

    fixed bug with webforms not showing

---
+++ src/mapper/src/lib/components/forms/wrapper.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+	import { onSetLanguageTag, languageTag } from '$translations/runtime.js';
+	import type { SlDrawer } from '@shoelace-style/shoelace';
+	const API_URL = import.meta.env.VITE_API_URL;
+	type Props = {
+		drawerRef: SlDrawer | undefined;
+		entityId: string | undefined;
+		projectId: number | undefined;
+		webFormsRef: HTMLElement | undefined;
+	};
+	let { drawerRef = $bindable(undefined), entityId, webFormsRef = $bindable(undefined), projectId }: Props = $props();
+	let iframeRef: HTMLIFrameElement;
+	let odkForm: any;
+	// to-do: hide drawer upon successful submission
+	function handleSubmit(payload: any) {
+		(async () => {
+			if (!payload.detail) return;
+			const url = `${API_URL}/submission?project_id=${projectId}`;
+			var data = new FormData();
+			data.append('submission_xml', await payload.detail[0].data.instanceFile.text());
+			await fetch(url, {
+				method: 'POST',
+				body: data,
+			});
+			drawerRef?.hide();
+		})();
+	}
+	function handleOdkForm(evt: any) {
+		if (evt?.detail?.[0]) {
+			odkForm = evt.detail[0];
+			console.log('set odkForm', odkForm);
+			// over-write default language
+			setFormLanguage(languageTag());
+			// select feature that you clicked on map
+			odkForm
+				.getChildren()
+				.find((it: any) => it.definition.nodeset === '/data/feature')
+				?.setValueState([entityId]);
+		}
+	}
+	const setFormLanguage = (newLocale: string) => {
+		if (odkForm) {
+			const target = `(${newLocale.toLowerCase()})`;
+			const match = odkForm.languages.find((it: any) => it?.language.toLowerCase().includes(target));
+			if (match) {
+				odkForm?.setLanguage(match);
+			}
+		}
+	};
+	$effect(() => {
+		// we want to rerun this $effect function whenever a new iframe is rendered
+		// because projectId and entityId are state keys that force a re-render below when they change
+		// we add them here inside the $effect function, so Svelte knows that the function depends on them
+		projectId;
+		entityId;
+		if (iframeRef) {
+			const intervalId = setInterval(() => {
+				webFormsRef = iframeRef.contentDocument?.querySelector('odk-web-form') || undefined;
+				if (webFormsRef) {
+					clearInterval(intervalId);
+					webFormsRef.addEventListener('submit', handleSubmit);
+					webFormsRef.addEventListener('odkForm', handleOdkForm);
+				}
+			}, 10);
+			// clear interval when re-run
+			return () => {
+				clearInterval(intervalId);
+			};
+		}
+	});
+	onSetLanguageTag((newLocale: string) => setFormLanguage(newLocale));
+	console.log({ entityId, projectId });
+</script>
+
+<hot-drawer
+	id="odk-web-forms-drawer"
+	bind:this={drawerRef}
+	contained
+	placement="start"
+	class="drawer-contained drawer-placement-start drawer-overview"
+	style="--size: 100vw; --header-spacing: 0px"
+>
+	{#if entityId}
+		{#key projectId}
+			{#key entityId}
+				<iframe
+					bind:this={iframeRef}
+					title="odk-web-forms-wrapper"
+					src={`./web-forms.html?projectId=${projectId}&entityId=${entityId}&api_url=${API_URL}&language=${languageTag()}`}
+					style="height: {window.outerHeight}px; width: 100%; z-index: 11;"
+				>
+				</iframe>
+			{/key}
+		{/key}
+	{/if}
+</hot-drawer>
+
+<style>
+	#odk-web-forms-drawer::part(panel) {
+		z-index: 11;
+	}
+	#odk-web-forms-drawer::part(body) {
+		padding: 0;
+	}
+</style>
