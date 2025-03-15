d67d20158126930855df235960b94a1d844c06bf
spwoodcock <sam.woodcock@protonmail.com>
2025-03-15 07:51:38 +0000

    build: add deploy compose for sub, plus sub and deploy script

---
+++ deploy/gen-and-deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+install_envsubst_if_missing() {
+    if ! command -v curl &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y curl
+    fi
+
+    echo
+    # Get a8m/envsubst (required for default vals syntax ${VAR:-default})
+    # Use local version, as envsubst may be installed on system already
+    if [ -f ./envsubst ]; then
+        echo "envsubst already exists. Continuing."
+    else
+        echo "Downloading a8m/envsubst"
+        echo
+        curl -L "https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-$(uname -s)-$(uname -m)" -o envsubst
+        chmod +x envsubst
+    fi
+}
+
+gen_current_env() {
+    # Generate a .env file from .env.example and export to the environment
+    while IFS= read -r line; do
+        echo "${line?}"
+    done < <(./envsubst -i .env.example | grep -vE '^\s*#|^\s*$')
+
+    # Override exported env vars with any in the .env file
+    if [ -f .env ]; then
+        while IFS= read -r line; do
+            # Remove surrounding quotes (single or double)
+            clean_line=$(echo "$line" | sed -E 's/^([^=]+)=(["'\'']?)(.*)\2$/\1=\3/')
+            export "${clean_line?}"
+        done < <(grep -vE '^\s*#|^\s*$' .env)
+    fi
+
+    # Generate compose file (note this is currently only for development branch)
+    export GIT_BRANCH=development
+    echo ""
+    echo "Generated compose file:"
+    echo ""
+    ./envsubst -i deploy/compose.sub.yaml
+}
+
+pull_and_deploy() {
+    # Pull images (must run in a subshell to be within 'deploy' dir for ../compose.yaml extend)
+    (cd deploy &&
+        ../envsubst -i compose.sub.yaml | \
+        docker compose -f - pull)
+
+    # Deploy
+    (cd deploy &&
+        ../envsubst -i compose.sub.yaml | \
+        docker compose -f - up -d)
+}
+
+install_envsubst_if_missing
+gen_current_env
+pull_and_deploy
