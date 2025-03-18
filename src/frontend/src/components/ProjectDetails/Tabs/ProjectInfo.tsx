3e976d3de7841056402597ae74284731ef7bf1ae
NSUWAL123 <suwalnishit@gmail.com>
2025-03-18 13:37:37 +0545

    refactor(projectInfo): redesign project info tab

--- src/frontend/src/components/ProjectDetails/Tabs/ProjectInfo.tsx
+++ src/frontend/src/components/ProjectDetails/Tabs/ProjectInfo.tsx
@@ -1,19 +1,52 @@
 import React, { useEffect, useRef, useState } from 'react';
-import AssetModules from '@/shared/AssetModules.js';
+import { useParams } from 'react-router-dom';
+import { Tooltip } from '@mui/material';
 import ProjectIcon from '@/assets/images/project_icon.png';
-import CoreModules from '@/shared/CoreModules';
 import { useAppSelector } from '@/types/reduxTypes';
 import { entity_state } from '@/types/enums';
 import { EntityOsmMap } from '@/models/project/projectModel';
+import { ProjectInfoSkeletonLoader } from '@/components/ProjectDetails/SkeletonLoader';
+import CoreModules from '@/shared/CoreModules';
 
 const ProjectInfo: React.FC = () => {
+  const params = useParams();
   const paraRef = useRef<HTMLParagraphElement>(null);
+
+  const projectId = params.id;
   const [seeMore, setSeeMore] = useState(false);
   const [descLines, setDescLines] = useState(1);
+
   const projectInfo = useAppSelector((state) => state?.project?.projectInfo);
+  const projectTaskBoundries = useAppSelector((state) => state.project.projectTaskBoundries);
   const projectDetailsLoading = useAppSelector((state) => state?.project?.projectDetailsLoading);
   const projectEntities = useAppSelector((state) => state?.project?.entityOsmMap);
   const projectEntitiesLoading = useAppSelector((state) => state?.project?.entityOsmMapLoading);
+
+  const projectTasks = projectTaskBoundries.find((project) => project.id.toString() === projectId)?.taskBoundries;
+
+  const taskProgress = projectTasks?.reduce(
+    (total, task) => {
+      const taskState = task.task_state;
+      if (taskState === 'UNLOCKED_TO_VALIDATE' || taskState === 'LOCKED_FOR_VALIDATION') {
+        total.mapped += 1;
+      }
+      if (taskState === 'UNLOCKED_DONE') {
+        total.validated += 1;
+      }
+      return total;
+    },
+    { mapped: 0, validated: 0 },
+  );
+
+  const projectLastActiveDate: Date | null = projectEntities.reduce((latestDate: Date | null, entity: EntityOsmMap) => {
+    const updatedAt = Date.parse(entity.updated_at);
+    return updatedAt && (!latestDate || updatedAt > latestDate.getTime()) ? new Date(updatedAt) : latestDate;
+  }, null);
+
+  const projectTotalFeatures: number = projectEntities.length;
+  const projectMappedFeatures: number = projectEntities.filter(
+    (entity: EntityOsmMap) => entity.status === entity_state.SURVEY_SUBMITTED,
+  ).length;
 
   useEffect(() => {
     if (paraRef.current) {
@@ -23,91 +56,98 @@ const ProjectInfo: React.FC = () => {
     }
   }, [projectInfo, paraRef.current]);
 
-  const projectLastActiveDate: Date | null = projectEntities.reduce((latestDate: Date | null, entity: EntityOsmMap) => {
-    const updatedAt = Date.parse(entity.updated_at);
-    return updatedAt && (!latestDate || updatedAt > latestDate.getTime()) ? new Date(updatedAt) : latestDate;
-  }, null);
-
-  const projectTotalFeatures: number = projectEntities.length;
-
-  const projectMappedFeatures: number = projectEntities.filter(
-    (entity: EntityOsmMap) => entity.status === entity_state.SURVEY_SUBMITTED,
-  ).length;
-
+  if (projectDetailsLoading) return <ProjectInfoSkeletonLoader />;
   return (
-    <div className="fmtm-flex fmtm-flex-col fmtm-gap-5 fmtm-mt-3  fmtm-overflow-y-scroll scrollbar fmtm-pr-1">
+    <div className="fmtm-flex fmtm-flex-col fmtm-gap-5 fmtm-overflow-y-scroll scrollbar fmtm-pr-1 fmtm-pb-6">
+      {/* <ProjectInfoSkeletonLoader /> */}
       <div>
-        <p className="fmtm-font-bold">Description</p>
-        {projectDetailsLoading ? (
-          <div>
-            {Array.from({ length: 7 }).map((_, index) => (
-              <CoreModules.Skeleton key={index} />
-            ))}
-            <CoreModules.Skeleton key="last" className="!fmtm-w-[80px]" />
-          </div>
-        ) : (
-          <div>
-            <p className={`${!seeMore ? 'fmtm-line-clamp-[7]' : ''} fmtm-text-[#706E6E]`} ref={paraRef}>
-              {projectInfo?.description}
-            </p>
-            {descLines >= 7 && (
-              <p
-                className="fmtm-text-primaryRed hover:fmtm-text-red-700 hover:fmtm-cursor-pointer"
-                onClick={() => setSeeMore(!seeMore)}
-              >
-                ... {!seeMore ? 'See More' : 'See Less'}
-              </p>
-            )}
-          </div>
-        )}
-      </div>
-      <div className="fmtm-flex fmtm-items-center fmtm-gap-2">
-        <AssetModules.FmdGoodIcon className="fmtm-text-primaryRed" />
-        {projectDetailsLoading ? (
-          <CoreModules.Skeleton className="!fmtm-w-[160px]" />
-        ) : (
-          <p>{projectInfo?.location_str ? projectInfo?.location_str : '-'}</p>
-        )}
-      </div>
-      <div className="fmtm-flex fmtm-justify-between fmtm-w-full">
+        <p className="fmtm-button fmtm-text-red-medium">Description</p>
         <div>
-          <p className="fmtm-font-bold">Mapped Features</p>
-          {projectEntitiesLoading ? (
-            <CoreModules.Skeleton className="!fmtm-w-[60px] fmtm-ml-[15%]" />
-          ) : (
-            <p className="fmtm-text-center fmtm-text-[#706E6E]">
-              {projectMappedFeatures} / {projectTotalFeatures}
-            </p>
-          )}
-        </div>
-        <div>
-          <p className="fmtm-font-bold">Last Contribution</p>
-          {projectEntitiesLoading ? (
-            <CoreModules.Skeleton className="!fmtm-w-[70px] fmtm-ml-[20%]" />
-          ) : (
-            <p className="fmtm-text-center fmtm-text-[#706E6E] fmtm-capitalize">
-              {projectLastActiveDate ? projectLastActiveDate.toLocaleString() : '-'}
+          <p className={`${!seeMore ? 'fmtm-line-clamp-[7]' : ''} fmtm-body-md fmtm-text-grey-900 `} ref={paraRef}>
+            {projectInfo?.description}
+          </p>
+          {descLines >= 7 && (
+            <p
+              className="fmtm-body-md fmtm-text-red-medium hover:fmtm-text-red-dark hover:fmtm-cursor-pointer fmtm-w-fit"
+              onClick={() => setSeeMore(!seeMore)}
+            >
+              ... {!seeMore ? 'See More' : 'See Less'}
             </p>
           )}
         </div>
       </div>
       <div>
-        <p className="fmtm-font-bold fmtm-mb-1">Organized By:</p>
-        {projectDetailsLoading ? (
-          <div className="fmtm-flex fmtm-items-center fmtm-gap-5">
-            <CoreModules.Skeleton className="!fmtm-w-[2.81rem] fmtm-h-[2.81rem] !fmtm-rounded-full fmtm-overflow-hidden" />
-            <CoreModules.Skeleton className="!fmtm-w-[180px]" />
-          </div>
+        <p className="fmtm-button fmtm-text-grey-900">Project ID</p>
+        <p className="fmtm-body-md fmtm-text-grey-800">#{projectInfo?.id}</p>
+      </div>
+      <div>
+        <p className="fmtm-button fmtm-text-grey-900">Project Area</p>
+        <p className="fmtm-body-md fmtm-text-grey-800">{projectInfo?.location_str || '-'}</p>
+      </div>
+      <div>
+        <p className="fmtm-button fmtm-text-grey-900">Last Contribution</p>
+        <p className="fmtm-body-md fmtm-text-grey-800">
+          {projectLastActiveDate ? projectLastActiveDate.toLocaleString() : '-'}
+        </p>
+      </div>
+      <div>
+        <p className="fmtm-button fmtm-text-grey-900">Organized By</p>
+        <img
+          src={projectInfo?.organisation_logo ? projectInfo?.organisation_logo : ProjectIcon}
+          alt="Organization Photo"
+          className="fmtm-max-w-[2.625rem]"
+        />
+      </div>
+      <div>
+        {projectEntitiesLoading ? (
+          <CoreModules.Skeleton width={150} height={14} />
         ) : (
-          <div className="fmtm-flex fmtm-items-center fmtm-gap-4">
-            <div className="fmtm-w-10 fmtm-h-10 fmtm-overflow-hidden fmtm-rounded-full fmtm-bg-white fmtm-flex fmtm-justify-center fmtm-items-center">
-              <img
-                src={projectInfo?.organisation_logo ? projectInfo?.organisation_logo : ProjectIcon}
-                alt="Organization Photo"
+          <p className="fmtm-body-md fmtm-mb-1">
+            <span className="fmtm-text-red-medium">{projectMappedFeatures}</span>{' '}
+            <span className="fmtm-text-grey-800">/{projectTotalFeatures} Features Mapped</span>
+          </p>
+        )}
+        {projectTasks && taskProgress && (
+          <Tooltip
+            title={
+              <div>
+                <p>{projectTasks?.length} Total Tasks</p>
+                <p>{taskProgress?.mapped} Tasks Mapped</p>
+                <p>{taskProgress?.validated} Tasks Validated</p>
+              </div>
+            }
+            placement="top"
+            arrow
+            componentsProps={{
+              tooltip: {
+                sx: {
+                  backgroundColor: '#333333',
+                  color: '#ffffff',
+                  fontSize: '12px',
+                },
+              },
+              arrow: {
+                sx: {
+                  color: '#333333',
+                },
+              },
+            }}
+          >
+            <div className="fmtm-h-[0.375rem] fmtm-w-full fmtm-bg-grey-300 fmtm-rounded-xl fmtm-overflow-hidden fmtm-flex fmtm-cursor-pointer">
+              <div
+                style={{
+                  width: `${(taskProgress?.mapped / projectTasks?.length) * 100}%`,
+                }}
+                className={`fmtm-h-full fmtm-bg-grey-800 fmtm-rounded-r-xl`}
+              />
+              <div
+                style={{
+                  width: `${(taskProgress?.validated / projectTasks?.length) * 100}%`,
+                }}
+                className={`fmtm-h-full fmtm-bg-grey-500 fmtm-rounded-r-xl`}
               />
             </div>
-            <p>{projectInfo?.organisation_name}</p>
-          </div>
+          </Tooltip>
         )}
       </div>
     </div>
