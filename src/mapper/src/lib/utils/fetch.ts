1b93b496298ffee6bf8fe38e73dbc9caf5359342
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2025-04-11 05:16:07 -0400

    feat(mapper): image file choice question image override by css (#2365)
    * cache form xml and ODK Web Forms js library
    
    * transform category select into images for web forms

--- src/mapper/src/lib/utils/fetch.ts
+++ src/mapper/src/lib/utils/fetch.ts
@@ -1,3 +1,5 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 /**
  * @name fetchBlobUrl
  * @param url - url to a web resource like a script or xml file
@@ -7,4 +9,19 @@ export async function fetchBlobUrl(url: string): Promise<string> {
 	const response = await fetch(url);
 	const blob = await response.blob();
 	return URL.createObjectURL(blob);
+}
+
+export async function fetchFormMediBlobUrls(projectId: number): Promise<{ [filename: string]: string }> {
+	if (projectId === undefined) return {};
+
+	const response = await fetch(`${API_URL}/central/get-form-media?project_id=${projectId}`, { method: 'POST' });
+	const data: { [filename: string]: string } = await response.json();
+
+	const formMediaBlobs: { [filename: string]: string } = {};
+	for (let filename in data) {
+		const url = data[filename];
+		formMediaBlobs[filename] = await fetchBlobUrl(url);
+	}
+
+	return formMediaBlobs;
 }
