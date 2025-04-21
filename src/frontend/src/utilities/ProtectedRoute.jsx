f3c1f289f379c466493c4c770a05a71f579cf2af
Nishit Suwal <suwalnishit@gmail.com>
2025-04-21 03:52:40 +0000

    feat(protectedRoute): use of axios interceptors to handle 403 and display forbidden page

--- src/frontend/src/utilities/ProtectedRoute.jsx
+++ src/frontend/src/utilities/ProtectedRoute.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import { Navigate, useLocation } from 'react-router-dom';
 import CoreModules from '@/shared/CoreModules';
 import { LoginActions } from '@/store/slices/LoginSlice';
+import Forbidden from '@/views/Forbidden';
 
 const ProtectedRoute = ({ children, next }) => {
   const dispatch = CoreModules.useAppDispatch();
   const authDetails = CoreModules.useAppSelector((state) => state.login.authDetails);
   const { pathname } = useLocation();
+  const [isForbidden, setIsForbidden] = React.useState(false);
+
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const status = error.response ? error.response.status : null;
+
+      if (status === 403) {
+        setIsForbidden(true);
+      }
+    },
+  );
+
+  if (isForbidden) return <Forbidden />;
 
   if (authDetails == null) {
     dispatch(LoginActions.setLoginModalOpen(true));
