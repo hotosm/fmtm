e8435745189f15bb4b5eb7b09cc7935b648ce1a1
Nishit Suwal <suwalnishit@gmail.com>
2025-04-22 05:18:32 +0000

    fix(user): manageproject-usertab: style fixes

--- src/frontend/src/components/ManageProject/User/index.tsx
+++ src/frontend/src/components/ManageProject/User/index.tsx
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
 import AssignTab from './Assign';
 import InviteTab from './Invite';
+import { project_roles } from '@/types/enums';
 
 const tabList: ['Assign', 'Invite'] = ['Assign', 'Invite'];
+const roleList = [
+  {
+    id: 'MAPPER',
+    label: 'Mapper',
+    value: project_roles.MAPPER,
+  },
+  {
+    id: 'PROJECT_MANAGER',
+    label: 'Project Manager',
+    value: project_roles.PROJECT_MANAGER,
+  },
+];
 
 const UserTab = () => {
-  const [tabView, setTabView] = useState<'Assign' | 'Invite'>('Assign');
+  const [tabView, setTabView] = useState<'Assign' | 'Invite'>('Invite');
 
   return (
-    <div className="fmtm-py-5 lg:fmtm-py-10 fmtm-px-5 lg:fmtm-px-9">
-      <div className="fmtm-flex fmtm-flex-col lg:fmtm-flex-row fmtm-gap-5 lg:fmtm-gap-10 fmtm-w-full">
-        <div className="fmtm-max-w-[18rem] lg:fmtm-w-[23%] lg:fmtm-min-w-[18rem]">
-          <div className="fmtm-flex fmtm-gap-3 fmtm-mb-5">
-            {tabList.map((tab) => (
-              <div
-                key={tab}
-                className={`fmtm-w-fit fmtm-text-[#9B9999] fmtm-px-2 fmtm-border-b-[1px] hover:fmtm-border-primaryRed fmtm-duration-300 fmtm-cursor-pointer ${
-                  tabView === tab ? 'fmtm-border-primaryRed' : 'fmtm-border-transparent'
-                }`}
-                onClick={() => setTabView(tab)}
-              >
-                {tab}
-              </div>
-            ))}
+    <div className="fmtm-flex fmtm-flex-col fmtm-w-full fmtm-h-full">
+      <div className="fmtm-flex fmtm-gap-3 fmtm-mb-2">
+        {tabList.map((tab) => (
+          <div
+            key={tab}
+            className={`fmtm-button fmtm-w-fit fmtm-text-grey-900 fmtm-px-2 fmtm-border-b-[1px] hover:fmtm-border-red-medium fmtm-duration-300 fmtm-cursor-pointer ${
+              tabView === tab ? 'fmtm-border-red-medium' : 'fmtm-border-transparent'
+            }`}
+            onClick={() => setTabView(tab)}
+          >
+            {tab}
           </div>
-          <div>{tabView === 'Assign' ? <AssignTab /> : <InviteTab />}</div>
-        </div>
+        ))}
       </div>
+      {tabView === 'Assign' ? <AssignTab roleList={roleList} /> : <InviteTab roleList={roleList} />}
     </div>
   );
 };
