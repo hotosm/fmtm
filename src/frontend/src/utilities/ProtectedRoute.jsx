c1406a5409432fdf63fc3ce23ce32353af162dc2
Nishit Suwal <suwalnishit@gmail.com>
2025-04-22 12:15:19 +0000

    refactor(protectedRoute): append search to pathname

--- src/frontend/src/utilities/ProtectedRoute.jsx
+++ src/frontend/src/utilities/ProtectedRoute.jsx
@@ -6,11 +6,11 @@ import { LoginActions } from '@/store/slices/LoginSlice';
 const ProtectedRoute = ({ children, next }) => {
   const dispatch = CoreModules.useAppDispatch();
   const authDetails = CoreModules.useAppSelector((state) => state.login.authDetails);
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   if (authDetails == null) {
     dispatch(LoginActions.setLoginModalOpen(true));
-    return <Navigate to="/" replace state={{ from: pathname }} />;
+    return <Navigate to="/" replace state={{ from: `${pathname}${search}` }} />;
   }
 
   return children;
