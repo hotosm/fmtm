16c516d8824c7c10a6c4d55c167960f83ffabfcc
spwoodcock <sam.woodcock@protonmail.com>
2025-04-23 16:34:07 +0100

    refactor(frontend): remove unused code on react frontend

--- src/frontend/src/utilfunctions/checkGeomTypeInGeojson.ts
+++
@@ -1,9 +0,0 @@
-export const checkGeomTypeInGeojson = (geojson: Record<string, any>, geomType: string): boolean => {
-  if (geojson?.type === 'FeatureCollection') {
-    const hasPreferredGeomType = geojson?.features?.some((feature: Record<string, any>) => {
-      return feature?.geometry?.type === geomType;
-    });
-    return hasPreferredGeomType;
-  }
-  return false;
-};
