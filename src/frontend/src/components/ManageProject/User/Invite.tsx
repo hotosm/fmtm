15a938901aa97db15120fca4aab3f95216b38c10
Nishit Suwal <suwalnishit@gmail.com>
2025-04-23 04:28:59 +0000

    feat(invite): show project user invite errors on dialog

--- src/frontend/src/components/ManageProject/User/Invite.tsx
+++ src/frontend/src/components/ManageProject/User/Invite.tsx
@@ -13,6 +13,9 @@ import InviteValidation from '@/components/ManageProject/User/validation/InviteV
 import RadioButton from '@/components/common/RadioButton';
 import { useAppDispatch, useAppSelector } from '@/types/reduxTypes';
 import InviteTable from './InviteTable';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/RadixComponents/Dialog';
+import isEmpty from '@/utilfunctions/isEmpty';
+import { UserActions } from '@/store/slices/UserSlice';
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
@@ -52,6 +55,7 @@ const InviteTab = ({ roleList }: propType) => {
   const [user, setUser] = useState('');
 
   const inviteNewUserPending = useAppSelector((state) => state.user.inviteNewUserPending);
+  const projectUserInvitesError = useAppSelector((state) => state.user.projectUserInvitesError);
 
   const submission = () => {
     dispatch(InviteNewUser(`${VITE_API_URL}/users/invite`, { ...values, projectId }));
@@ -64,79 +68,108 @@ const InviteTab = ({ roleList }: propType) => {
   );
 
   return (
-    <div className="fmtm-flex fmtm-h-[calc(100%-24px)] fmtm-w-full fmtm-gap-5">
-      <form
-        onSubmit={handleSubmit}
-        className="fmtm-flex fmtm-flex-col fmtm-gap-5 fmtm-bg-white fmtm-rounded-xl fmtm-p-6 fmtm-w-[17.5rem] fmtm-h-full fmtm-overflow-y-scroll scrollbar"
+    <>
+      <Dialog
+        open={!isEmpty(projectUserInvitesError)}
+        onOpenChange={(state) => {
+          if (!state) dispatch(UserActions.SetProjectUserInvitesError([]));
+        }}
       >
-        <RadioButton
-          value={values?.inviteVia || ''}
-          topic=""
-          options={inviteOptions}
-          direction="row"
-          onChangeData={(value) => {
-            handleCustomChange('inviteVia', value);
-          }}
-        />
-        <div className="fmtm-w-full">
-          <div className="fmtm-flex fmtm-gap-2 fmtm-mb-2">
-            <InputTextField
-              id="name"
-              name="name"
-              label="Invite User"
-              value={user}
-              onChange={(e) => setUser(e.target.value)}
-              fieldType="text"
-              classNames="fmtm-flex-grow"
-              placeholder={values.inviteVia === 'osm' ? 'Enter Username' : 'Enter Gmail'}
-            />
-            <AssetModules.AddIcon
-              disabled={!user}
-              className="fmtm-bg-red-600 fmtm-text-white fmtm-rounded-full hover:fmtm-bg-red-700 hover:fmtm-cursor-pointer fmtm-mt-9"
-              onClick={() => {
-                if (!user) return;
-                handleCustomChange('user', [...values.user, user]);
-                setUser('');
-              }}
-            />
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>There were some problems while inviting users</DialogTitle>
+          </DialogHeader>
+          <div className="fmtm-border-[1px] fmtm-p-2 fmtm-border-red-medium fmtm-rounded-lg fmtm-bg-red-50">
+            {projectUserInvitesError.map((inviteError, i) => (
+              <p className="fmtm-text-red-medium">
+                {i + 1}. {inviteError}
+              </p>
+            ))}
           </div>
-          {values.user.length > 0 && (
-            <Chips
-              data={values.user}
-              clearChip={(i) => {
-                handleCustomChange(
-                  'user',
-                  values.user.filter((_, index) => index !== i),
-                );
-              }}
-              className="fmtm-w-full fmtm-flex-wrap"
-            />
-          )}
-          {errors?.user && <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm">{errors.user}</p>}
-        </div>
-        <div>
-          <p className={`fmtm-text-[1rem] fmtm-mb-2 fmtm-font-semibold !fmtm-bg-transparent`}>Assign as</p>
-          <Select2
-            options={roleList || []}
-            value={values.role}
-            onChange={(value: any) => {
-              handleCustomChange('role', value);
-            }}
-            placeholder="Role"
-            className="naxatw-w-1/5 naxatw-min-w-[9rem]"
-          />
-          {errors.role && <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm">{errors.role}</p>}
-        </div>
-        <div className="fmtm-flex fmtm-justify-center">
-          <Button type="submit" variant="primary-red" isLoading={inviteNewUserPending}>
-            ASSIGN
+          <Button
+            variant="primary-grey"
+            className="fmtm-ml-auto"
+            onClick={() => dispatch(UserActions.SetProjectUserInvitesError([]))}
+          >
+            Dismiss
           </Button>
+        </DialogContent>
+      </Dialog>
+
+      <div className="fmtm-flex fmtm-h-[calc(100%-24px)] fmtm-w-full fmtm-gap-5">
+        <form
+          onSubmit={handleSubmit}
+          className="fmtm-flex fmtm-flex-col fmtm-gap-5 fmtm-bg-white fmtm-rounded-xl fmtm-p-6 fmtm-w-[17.5rem] fmtm-h-full fmtm-overflow-y-scroll scrollbar"
+        >
+          <RadioButton
+            value={values?.inviteVia || ''}
+            topic=""
+            options={inviteOptions}
+            direction="row"
+            onChangeData={(value) => {
+              handleCustomChange('inviteVia', value);
+            }}
+          />
+          <div className="fmtm-w-full">
+            <div className="fmtm-flex fmtm-gap-2 fmtm-mb-2">
+              <InputTextField
+                id="name"
+                name="name"
+                label="Invite User"
+                value={user}
+                onChange={(e) => setUser(e.target.value)}
+                fieldType="text"
+                classNames="fmtm-flex-grow"
+                placeholder={values.inviteVia === 'osm' ? 'Enter Username' : 'Enter Gmail'}
+              />
+              <AssetModules.AddIcon
+                disabled={!user}
+                className="fmtm-bg-red-600 fmtm-text-white fmtm-rounded-full hover:fmtm-bg-red-700 hover:fmtm-cursor-pointer fmtm-mt-9"
+                onClick={() => {
+                  if (!user) return;
+                  handleCustomChange('user', [...values.user, user]);
+                  setUser('');
+                }}
+              />
+            </div>
+            {values.user.length > 0 && (
+              <Chips
+                data={values.user}
+                clearChip={(i) => {
+                  handleCustomChange(
+                    'user',
+                    values.user.filter((_, index) => index !== i),
+                  );
+                }}
+                className="fmtm-w-full fmtm-flex-wrap"
+              />
+            )}
+            {errors?.user && <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm">{errors.user}</p>}
+          </div>
+          <div>
+            <p className={`fmtm-text-[1rem] fmtm-mb-2 fmtm-font-semibold !fmtm-bg-transparent`}>Assign as</p>
+            <Select2
+              options={roleList || []}
+              value={values.role}
+              onChange={(value: any) => {
+                handleCustomChange('role', value);
+              }}
+              placeholder="Role"
+              className="naxatw-w-1/5 naxatw-min-w-[9rem]"
+            />
+            {errors.role && <p className="fmtm-form-error fmtm-text-red-600 fmtm-text-sm">{errors.role}</p>}
+          </div>
+          <div className="fmtm-flex fmtm-justify-center">
+            <Button type="submit" variant="primary-red" isLoading={inviteNewUserPending}>
+              ASSIGN
+            </Button>
+          </div>
+        </form>
+        <div className="fmtm-flex-1">
+          <InviteTable />
         </div>
-      </form>
-      <div className="fmtm-flex-1">
-        <InviteTable />
       </div>
-    </div>
+    </>
   );
 };
 
