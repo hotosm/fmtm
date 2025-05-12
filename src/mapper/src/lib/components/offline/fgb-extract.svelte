f83eb6db7bdda5f61b610f5fe6088667a6ff65f7
Nishit Suwal <suwalnishit@gmail.com>
2025-05-12 10:59:48 +0000

    feat(fgb-extract): extarct stack add

---
+++ src/mapper/src/lib/components/offline/fgb-extract.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { writeOfflineExtract } from '$lib/map/extracts';
+	import { m } from '$translations/messages.js';
+
+	interface Props {
+		projectId: number;
+		extract_url: string;
+	}
+	let { projectId, extract_url }: Props = $props();
+</script>
+
+<div class="extract">
+	<hot-button
+		onclick={() => writeOfflineExtract(projectId, extract_url)}
+		onkeydown={(e: KeyboardEvent) => {
+			e.key === 'Enter' && writeOfflineExtract(projectId, extract_url);
+		}}
+		role="button"
+		tabindex="0"
+		size="small"
+		class="button"
+	>
+		<hot-icon slot="prefix" name="download" class="icon"></hot-icon>
+		<span>{m['offline.store_features_offline']()}</span>
+	</hot-button>
+</div>
