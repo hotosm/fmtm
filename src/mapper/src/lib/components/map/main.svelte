069ccf62a067876cc7f1ef98259e58af7c234f0a
Nishit Suwal <suwalnishit@gmail.com>
2025-06-03 10:46:57 +0000

    feat(main): handle new line feature

--- src/mapper/src/lib/components/map/main.svelte
+++ src/mapper/src/lib/components/map/main.svelte
@@ -160,7 +160,12 @@
 	// })
 	let displayDrawHelpText: boolean = $state(false);
 	type DrawModeOptions = 'point' | 'linestring' | 'delete-selection' | 'polygon';
-	const currentDrawMode: DrawModeOptions = drawGeomType ? (drawGeomType.toLowerCase() as DrawModeOptions) : 'point';
+	const currentDrawMode: DrawModeOptions =
+		drawGeomType === 'POLYLINE'
+			? 'linestring'
+			: drawGeomType
+				? (drawGeomType.toLowerCase() as DrawModeOptions)
+				: 'point';
 	const drawControl = new MaplibreTerradrawControl({
 		modes: [currentDrawMode, 'select', 'delete'],
 		// Note We do not open the toolbar options, allowing the user
@@ -737,7 +742,6 @@
 				manageHoverState
 			/>
 		{:else if drawGeomType === MapGeomTypes.POINT}
-			<!-- id="new-geom-symbol-layer" -->
 			<SymbolLayer
 				id="new-entity-point-layer"
 				applyToClusters={false}
@@ -762,6 +766,32 @@
 					'icon-allow-overlap': true,
 					'icon-size': ['case', ['==', ['get', 'entity_id'], entitiesStore.selectedEntity?.entity_id || ''], 1.6, 1],
 				}}
+			/>
+		{:else if drawGeomType === MapGeomTypes.POLYLINE}
+			<LineLayer
+				id="new-entity-line-layer"
+				layout={{ 'line-cap': 'round', 'line-join': 'round' }}
+				paint={{
+					'line-color': [
+						'match',
+						['get', 'status'],
+						'READY',
+						cssValue('--entity-ready'),
+						'OPENED_IN_ODK',
+						cssValue('--entity-opened-in-odk'),
+						'SURVEY_SUBMITTED',
+						cssValue('--entity-survey-submitted'),
+						'VALIDATED',
+						cssValue('--entity-validated'),
+						'MARKED_BAD',
+						cssValue('--entity-marked-bad'),
+						cssValue('--entity-ready'), // default color if no match is found
+					],
+					'line-width': ['case', ['==', ['get', 'entity_id'], entitiesStore.selectedEntity?.entity_id || ''], 4, 3],
+					'line-opacity': ['case', ['==', ['get', 'entity_id'], entitiesStore.selectedEntity?.entity_id || ''], 1, 0.8],
+				}}
+				beforeLayerType="symbol"
+				manageHoverState
 			/>
 		{/if}
 	</GeoJSON>
