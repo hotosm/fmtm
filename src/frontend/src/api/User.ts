c81d8694c3ccf2123b83bc6a074a8ed6e7945584
Nishit Suwal <suwalnishit@gmail.com>
2025-06-05 04:49:56 +0000

    refactor(user): use /user/usernames api instead of /user api for PM user search

--- src/frontend/src/api/User.ts
+++ src/frontend/src/api/User.ts
@@ -56,19 +56,16 @@ export const UpdateUserRole = (url: string, payload: { role: 'READ_ONLY' | 'ADMI
   };
 };
 
-export const GetUserListForSelect = (
-  url: string,
-  params: { page: number; org_id: number; results_per_page: number; search: string },
-) => {
+export const GetUserListForSelect = (url: string, params: { search: string }) => {
   return async (dispatch: AppDispatch) => {
     dispatch(UserActions.SetUserListForSelectLoading(true));
 
     const getUserList = async (url: string) => {
       try {
-        const response: AxiosResponse<{ results: userType[]; pagination: paginationType }> = await axios.get(url, {
+        const response: AxiosResponse<{ sub: string; username: string }[]> = await axios.get(url, {
           params,
         });
-        dispatch(UserActions.SetUserListForSelect(response.data.results));
+        dispatch(UserActions.SetUserListForSelect(response.data));
       } catch (error) {
       } finally {
         dispatch(UserActions.SetUserListForSelectLoading(false));
