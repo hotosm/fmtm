aa038c46f02adcfe2f1e7bb26abf98e2d721f0ad
Nishit Suwal <suwalnishit@gmail.com>
2025-06-09 10:54:00 +0000

    refactor(organization): request HOT's ODK server workflow

--- src/frontend/src/components/CreateEditOrganization/validation/OrganizationDetailsValidation.ts
+++ src/frontend/src/components/CreateEditOrganization/validation/OrganizationDetailsValidation.ts
@@ -13,8 +13,8 @@ interface OrganisationValues {
   odk_central_password: string;
   osm_profile: string;
   community_type: string;
-  fillODKCredentials: boolean;
   associated_email: string;
+  odk_server_type: string;
 }
 interface ValidationErrors {
   logo?: string;
@@ -27,8 +27,8 @@ interface ValidationErrors {
   odk_central_password?: string;
   osm_profile?: string;
   community_type?: string;
-  fillODKCredentials?: boolean;
   associated_email?: string;
+  odk_server_type?: string;
 }
 
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -61,19 +61,23 @@ function OrganizationDetailsValidation(values: OrganisationValues) {
     errors.associated_email = 'Invalid Email.';
   }
 
-  if (values?.odk_central_url && !isValidUrl(values.odk_central_url)) {
+  if (!values?.odk_server_type) {
+    errors.odk_server_type = 'ODK Server Type is Required.';
+  }
+
+  if (values?.odk_server_type === 'OWN' && values?.odk_central_url && !isValidUrl(values.odk_central_url)) {
     errors.odk_central_url = 'Invalid URL.';
   }
 
-  if (values?.fillODKCredentials && isInputEmpty(values.odk_central_url)) {
+  if (values?.odk_server_type === 'OWN' && isInputEmpty(values.odk_central_url)) {
     errors.odk_central_url = 'ODK Central URL is Required.';
   }
 
-  if (values?.fillODKCredentials && isInputEmpty(values.odk_central_user)) {
+  if (values?.odk_server_type === 'OWN' && isInputEmpty(values.odk_central_user)) {
     errors.odk_central_user = 'ODK Central Email is Required.';
   }
 
-  if (values?.fillODKCredentials && isInputEmpty(values.odk_central_password)) {
+  if (values?.odk_server_type === 'OWN' && isInputEmpty(values.odk_central_password)) {
     errors.odk_central_password = 'ODK Central Password is Required.';
   }
 
