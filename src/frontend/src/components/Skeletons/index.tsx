53bdbbb2b23d9fd5e6cd42978b73ee64b021c922
Nishit Suwal <suwalnishit@gmail.com>
2025-06-12 07:06:28 +0000

    refactor(skeletons): shift skeleton loader component under /componenets/Skeletons

--- src/frontend/src/components/Skeletons/index.tsx
+++ src/frontend/src/components/Skeletons/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { cn } from '@/utilfunctions/shadcn';
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+export const Skeleton = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
   return <div className={cn('fmtm-animate-pulse fmtm-rounded-md fmtm-bg-grey-300', className)} {...props} />;
-}
-
-export { Skeleton };
+};
