2f39e1ad729ab867c09fa9b627d5ff13583fd00a
Nishit Suwal <suwalnishit@gmail.com>
2025-06-16 08:04:14 +0000

    refactor(manageProject): only show update form & users tab if the project is published

--- src/frontend/src/views/ManageProject.tsx
+++ src/frontend/src/views/ManageProject.tsx
@@ -13,14 +13,9 @@ import FormUpdate from '@/components/ManageProject/Form';
 import { useIsOrganizationAdmin, useIsProjectManager } from '@/hooks/usePermissions';
 import Forbidden from '@/views/Forbidden';
 import ManageProjectSkeleton from '@/components/Skeletons/ManageProject';
+import { project_status } from '@/types/enums';
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
-
-const tabList = [
-  { id: 'details', name: 'Details', icon: <AssetModules.InfoIcon className="!fmtm-text-[1.125rem]" /> },
-  { id: 'form', name: 'Form', icon: <AssetModules.SubmissionIcon className="!fmtm-text-[1.125rem]" /> },
-  { id: 'users', name: 'Users', icon: <AssetModules.PersonIcon className="!fmtm-text-[1.125rem]" /> },
-];
 
 const ManageProject = () => {
   const dispatch = useAppDispatch();
@@ -31,6 +26,22 @@ const ManageProject = () => {
   const editProjectDetails = useAppSelector((state) => state.createproject.editProjectDetails);
   const isProjectDeletePending = useAppSelector((state) => state.createproject.isProjectDeletePending);
   const projectDetailsLoading = useAppSelector((state) => state.createproject.projectDetailsLoading);
+
+  const tabList = [
+    { id: 'details', name: 'Details', icon: <AssetModules.InfoIcon className="!fmtm-text-[1.125rem]" />, show: true },
+    {
+      id: 'form',
+      name: 'Form',
+      icon: <AssetModules.SubmissionIcon className="!fmtm-text-[1.125rem]" />,
+      show: editProjectDetails.status === project_status.PUBLISHED,
+    },
+    {
+      id: 'users',
+      name: 'Users',
+      icon: <AssetModules.PersonIcon className="!fmtm-text-[1.125rem]" />,
+      show: editProjectDetails.status === project_status.PUBLISHED,
+    },
+  ];
 
   const isProjectManager = useIsProjectManager(projectId as string);
   const isOrganizationAdmin = useIsOrganizationAdmin(editProjectDetails.organisation_id as null | number);
@@ -74,17 +85,20 @@ const ManageProject = () => {
           {/* left container */}
           <div className="fmtm-bg-white fmtm-h-full fmtm-rounded-xl sm:fmtm-w-[17.5rem] fmtm-p-6 fmtm-flex sm:fmtm-flex-col fmtm-flex-wrap sm:fmtm-flex-nowrap fmtm-gap-x-5">
             <div className="fmtm-flex-1 fmtm-flex sm:fmtm-flex-col fmtm-h-fit">
-              {tabList.map((tab) => (
-                <button
-                  key={tab.id}
-                  onClick={() => setSelectedTab(tab.id)}
-                  className={`fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-px-5 fmtm-py-3 fmtm-rounded fmtm-duration-200 ${
-                    selectedTab === tab.id ? 'fmtm-text-red-medium fmtm-bg-red-light' : 'hover:fmtm-text-red-medium'
-                  }`}
-                >
-                  {tab.icon} {tab.name}
-                </button>
-              ))}
+              {tabList.map(
+                (tab) =>
+                  tab.show && (
+                    <button
+                      key={tab.id}
+                      onClick={() => setSelectedTab(tab.id)}
+                      className={`fmtm-flex fmtm-items-center fmtm-gap-2 fmtm-px-5 fmtm-py-3 fmtm-rounded fmtm-duration-200 ${
+                        selectedTab === tab.id ? 'fmtm-text-red-medium fmtm-bg-red-light' : 'hover:fmtm-text-red-medium'
+                      }`}
+                    >
+                      {tab.icon} {tab.name}
+                    </button>
+                  ),
+              )}
             </div>
             <Dialog open={toggleDeleteModal} onOpenChange={setToggleDeleteModal}>
               <DialogTrigger>
