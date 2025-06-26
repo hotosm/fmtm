75dd46a4e6338cd4c7eba28923cc9895fda809ac
Nishit Suwal <suwalnishit@gmail.com>
2025-06-26 09:02:34 +0000

    feat(constants): add project visibility and upload area options

--- src/frontend/src/components/CreateProject/constants/index.tsx
+++ src/frontend/src/components/CreateProject/constants/index.tsx
@@ -1,3 +1,6 @@
+import { projectVisibilityOptionsType } from '@/store/types/ICreateProject';
+import { project_visibility } from '@/types/enums';
+
 export interface ICreateProjectSteps {
   step: number;
   label: string;
@@ -29,5 +32,37 @@ export const createProjectSteps: ICreateProjectSteps[] = [
     step: 5,
     label: '05',
     name: 'Split Tasks',
+  },
+];
+
+export const projectVisibilityOptions: projectVisibilityOptionsType[] = [
+  {
+    name: 'project_visibility',
+    value: project_visibility.PUBLIC,
+    label: 'Public',
+  },
+  {
+    name: 'project_visibility',
+    value: project_visibility.PRIVATE,
+    label: 'Private',
+  },
+];
+
+type uploadAreaOptionsType = {
+  name: 'upload_area';
+  value: 'draw' | 'upload_file';
+  label: string;
+};
+
+export const uploadAreaOptions: uploadAreaOptionsType[] = [
+  {
+    name: 'upload_area',
+    value: 'draw',
+    label: 'Draw',
+  },
+  {
+    name: 'upload_area',
+    value: 'upload_file',
+    label: 'Upload File',
   },
 ];
