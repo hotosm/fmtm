604f5cd3f1658d7cf2e9329f10649736b5cac34b
Nishit Suwal <suwalnishit@gmail.com>
2025-06-27 06:49:34 +0000

    feat(04-MapData): step 4 map data form setup

--- src/frontend/src/components/CreateProject/04-MapData.tsx
+++ src/frontend/src/components/CreateProject/04-MapData.tsx
@@ -1,7 +1,240 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Controller, useFormContext } from 'react-hook-form';
+import { geojson as fgbGeojson } from 'flatgeobuf';
+import { valid } from 'geojson-validation';
+import type { FeatureCollection } from 'geojson';
+
+import { newGeomOptions, primaryGeomOptions } from './constants';
+import { CommonActions } from '@/store/slices/CommonSlice';
+import { dataExtractGeojsonType } from '@/store/types/ICreateProject';
+import { convertFileToGeojson } from '@/utilfunctions/convertFileToGeojson';
+import { GeoGeomTypesEnum } from '@/types/enums';
+import { useAppDispatch } from '@/types/reduxTypes';
+import { fileType } from '@/store/types/ICommon';
+
+import FieldLabel from '@/components/common/FieldLabel';
+import RadioButton from '@/components/common/RadioButton';
+import Switch from '@/components/common/Switch';
+import Button from '@/components/common/Button';
+import UploadArea from '@/components/common/UploadArea';
+
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const MapData = () => {
-  return <div>MapData</div>;
+  const dispatch = useAppDispatch();
+
+  const form = useFormContext();
+  const { watch, control, setValue } = form;
+  const values = watch();
+
+  const [fetchingOSMData, setFetchingOSMData] = useState(false);
+
+  const dataExtractOptions = [
+    {
+      name: 'data_extract',
+      value: 'osm_data_extract',
+      label: 'Fetch data from OSM',
+      disabled: values.primaryGeomType === 'POLYLINE',
+    },
+    { name: 'data_extract', value: 'custom_data_extract', label: 'Upload custom map data' },
+    { name: 'data_extract', value: 'no_data_extract', label: 'No existing data' },
+  ];
+
+  const changeFileHandler = async (file: fileType, fileInputRef: React.RefObject<HTMLInputElement | null>) => {
+    if (!file) {
+      resetMapDataFile();
+      return;
+    }
+    const uploadedFile = file?.file;
+    const fileType = uploadedFile.name.split('.').pop();
+
+    // Handle geojson and fgb types, return featurecollection geojson
+    let extractFeatCol;
+    if (fileType && ['json', 'geojson'].includes(fileType)) {
+      // already geojson format, so we simply append
+      setValue('customDataExtractFile', file);
+      extractFeatCol = await convertFileToGeojson(uploadedFile);
+    } else if (fileType && ['fgb'].includes(fileType)) {
+      // deserialise the fgb --> geojson for upload
+      const arrayBuffer = new Uint8Array(await uploadedFile.arrayBuffer());
+      extractFeatCol = fgbGeojson.deserialize(arrayBuffer);
+      // Set converted geojson to state for splitting
+      const geojsonFromFgbFile = {
+        ...file,
+        file: new File([JSON.stringify(extractFeatCol)], 'custom_extract.geojson', { type: 'application/json' }),
+      };
+      setValue('customDataExtractFile', geojsonFromFgbFile);
+    }
+
+    validateDataExtractGeojson(extractFeatCol, fileInputRef);
+  };
+
+  const resetMapDataFile = () => {
+    setValue('customDataExtractFile', null);
+    setValue('dataExtractGeojson', null);
+  };
+
+  const validateDataExtractGeojson = (
+    extractFeatCol: FeatureCollection,
+    fileInputRef: React.RefObject<HTMLInputElement | null>,
+  ) => {
+    const isGeojsonValid = valid(extractFeatCol, true);
+
+    if (isGeojsonValid?.length === 0 && extractFeatCol) {
+      setValue('dataExtractGeojson', extractFeatCol);
+    } else {
+      resetMapDataFile();
+      if (fileInputRef.current) fileInputRef.current.value = '';
+      dispatch(
+        CommonActions.SetSnackBar({
+          message: `The uploaded GeoJSON is invalid and contains the following errors: ${isGeojsonValid?.map((error) => `\n${error}`)}`,
+          duration: 10000,
+        }),
+      );
+    }
+  };
+
+  // Create a File object from the geojson Blob
+  const getFileFromGeojson = (geojson) => {
+    const geojsonBlob = new Blob([JSON.stringify(geojson)], { type: 'application/json' });
+    return new File([geojsonBlob], 'data.geojson', { type: 'application/json' });
+  };
+
+  // Generate OSM data extract
+  const generateDataExtract = async () => {
+    const dataExtractRequestFormData = new FormData();
+    const projectAoiGeojsonFile = getFileFromGeojson(values.AOIGeojson);
+
+    dataExtractRequestFormData.append('geojson_file', projectAoiGeojsonFile);
+    dataExtractRequestFormData.append('osm_category', values.formExampleSelection);
+    dataExtractRequestFormData.append('geom_type', values.primaryGeomType);
+    if (values.primaryGeomType == GeoGeomTypesEnum.POINT)
+      dataExtractRequestFormData.append('centroid', values.includeCentroid);
+
+    setFetchingOSMData(true);
+    try {
+      const response = await axios.post(`${VITE_API_URL}/projects/generate-data-extract`, dataExtractRequestFormData, {
+        params: { project_id: 4 },
+      });
+
+      const dataExtractGeojsonUrl = response.data.url;
+
+      // Extract fgb and set geojson to map
+      const geojsonExtractFile = await fetch(dataExtractGeojsonUrl);
+      const geojsonExtract = await geojsonExtractFile.json();
+      if ((geojsonExtract && (geojsonExtract as dataExtractGeojsonType))?.features?.length > 0) {
+        setValue('dataExtractGeojson', geojsonExtract);
+      } else {
+        dispatch(
+          CommonActions.SetSnackBar({
+            message: 'Map has no features. Please try adjusting the map area.',
+          }),
+        );
+      }
+    } catch (error) {
+      dispatch(
+        CommonActions.SetSnackBar({
+          message: 'Error generating map data',
+        }),
+      );
+    } finally {
+      setFetchingOSMData(false);
+    }
+  };
+
+  return (
+    <div className="fmtm-flex fmtm-flex-col fmtm-gap-[1.125rem] fmtm-w-full">
+      <div>
+        <FieldLabel label="What type of geometry do you wish to map?" astric className="fmtm-mb-1" />
+        <Controller
+          control={control}
+          name="primaryGeomType"
+          render={({ field }) => (
+            <RadioButton value={field.value} options={primaryGeomOptions} onChangeData={field.onChange} />
+          )}
+        />
+      </div>
+
+      {values.primaryGeomType === GeoGeomTypesEnum.POINT && (
+        <div className="fmtm-flex fmtm-items-center fmtm-gap-2">
+          <FieldLabel label="Include polygon centroids" className="fmtm-mb-1" />
+          <Controller
+            control={control}
+            name="includeCentroid"
+            render={({ field }) => (
+              <Switch ref={field.ref} checked={field.value} onCheckedChange={field.onChange} className="" />
+            )}
+          />
+        </div>
+      )}
+
+      <div className="fmtm-flex fmtm-items-center fmtm-gap-2">
+        <FieldLabel label="I want to use a mix of geometry types" className="fmtm-mb-1" />
+        <Controller
+          control={control}
+          name="useMixedGeomTypes"
+          render={({ field }) => (
+            <Switch ref={field.ref} checked={field.value} onCheckedChange={field.onChange} className="" />
+          )}
+        />
+      </div>
+
+      {values.useMixedGeomTypes && (
+        <div>
+          <FieldLabel label="New geometries collected should be of type" astric className="fmtm-mb-1" />
+          <Controller
+            control={control}
+            name="newGeomType"
+            render={({ field }) => (
+              <RadioButton value={field.value} options={newGeomOptions} onChangeData={field.onChange} />
+            )}
+          />
+        </div>
+      )}
+      <div>
+        <FieldLabel label="Upload your own map data or use OSM" astric className="fmtm-mb-1" />
+        <Controller
+          control={control}
+          name="dataExtractType"
+          render={({ field }) => (
+            <RadioButton value={field.value} options={dataExtractOptions} onChangeData={field.onChange} />
+          )}
+        />
+
+        {values.dataExtractType === 'osm_data_extract' && (
+          <Button
+            variant="primary-red"
+            onClick={() => {
+              resetMapDataFile();
+              generateDataExtract();
+            }}
+            isLoading={fetchingOSMData}
+          >
+            Fetch OSM Data
+          </Button>
+        )}
+
+        {values.dataExtractType === 'custom_data_extract' && (
+          <>
+            <UploadArea
+              title="Upload Map Data"
+              label="The supported file formats are .geojson, .json, .fgb"
+              data={values.customDataExtractFile ? [values.customDataExtractFile] : []}
+              onUploadFile={(updatedFiles, fileInputRef) => {
+                changeFileHandler(updatedFiles?.[0] as fileType, fileInputRef);
+              }}
+              acceptedInput=".geojson,.json,.fgb"
+            />
+          </>
+        )}
+        <p className="fmtm-text-gray-500">
+          Total number of features:{' '}
+          <span className="fmtm-font-bold">{values.dataExtractGeojson?.features?.length || 0}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MapData;
