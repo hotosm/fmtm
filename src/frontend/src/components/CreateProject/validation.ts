3161dee341974eb6b235b74e676a4c8611c3c38b
Nishit Suwal <suwalnishit@gmail.com>
2025-06-30 11:46:09 +0000

    feat(validation): validation add for step 1, 2, 3, & 5

--- src/frontend/src/components/CreateProject/validation.ts
+++ src/frontend/src/components/CreateProject/validation.ts
@@ -1,113 +1,192 @@
 import { z } from 'zod/v4';
 import { isValidUrl } from '@/utilfunctions/urlChecker';
-import { project_visibility } from '@/types/enums';
+import { project_visibility, task_split_type } from '@/types/enums';
 
 // Utility regex
 const noUnderscore = /^[^_]+$/;
 
 export const basicDetailsValidationSchema = z
   .object({
-    // STEP 1: Basic Details
-    // name: z
-    //   .string()
-    //   .min(1, 'Project Name is Required.')
-    //   .regex(noUnderscore, 'Project Name should not contain _ (underscore)'),
-    // short_description: z.string().min(1, 'Short Description is Required'),
-    // description: z.string().min(1, 'Description is Required'),
-    // organisation_id: z.number({ error: 'Organization is Required' }),
-    // hasODKCredentials: z.boolean(),
-    // useDefaultODKCredentials: z.boolean(),
-    // odk_central_url: z.string().optional(),
-    // odk_central_user: z.string().optional(),
-    // odk_central_password: z.string().optional(),
-    // project_admins: z.array(z.string()).min(1, 'At least one Project Admin shall be selected'),
-    // uploadAreaSelection: z.string().min(1, 'Upload Area Selection is Required'),
-    // uploadedAOIFile: z.object({ id: z.string(), file: z.instanceof(File), previewURL: z.string() }).optional(),
-    // AOIGeojson: z.unknown().refine((val) => val !== undefined, {
-    //   message: 'AOI GeoJSON is required',
-    // }),
-    // AOIArea: z.string().optional(),
-    //-----------------------------------------------------------------------------------------
-    // STEP 2: Project Details
-    //   username: z.string().optional(), // not validated in your current logic
-    //   id: z.string().optional(), // not validated in your current logic
-    //   useDefaultODKCredentials: z.boolean(),
-    //   hasCustomTMS: z.boolean(),
-    //   custom_tms_url: z.string().optional(),
+    name: z
+      .string()
+      .min(1, 'Project Name is Required')
+      .regex(noUnderscore, 'Project Name should not contain _ (underscore)'),
+    short_description: z.string().min(1, 'Short Description is Required'),
+    description: z.string().min(1, 'Description is Required'),
+    organisation_id: z.number({ error: 'Organization is Required' }),
+    hasODKCredentials: z.boolean(),
+    useDefaultODKCredentials: z.boolean(),
+    odk_central_url: z.string().optional(),
+    odk_central_user: z.string().optional(),
+    odk_central_password: z.string().optional(),
+    project_admins: z.array(z.string()).min(1, 'At least one Project Admin shall be selected'),
+    uploadAreaSelection: z.string().min(1, 'Upload Area Selection is Required'),
+    uploadedAOIFile: z.object({ id: z.string(), file: z.instanceof(File), previewURL: z.string() }).optional(),
+    outline: z.unknown().refine((val) => val !== undefined, {
+      message: 'Project AOI is required',
+    }),
+    outlineArea: z.string().optional(),
   })
-  .superRefine((values, ctx) => {
+  .check((ctx) => {
+    const values = ctx.value;
+    if (!values.useDefaultODKCredentials) {
+      if (!values.odk_central_url) {
+        ctx.issues.push({
+          input: values.odk_central_url,
+          path: ['odk_central_url'],
+          message: 'ODK URL is Required',
+          code: 'custom',
+        });
+      } else if (!isValidUrl(values.odk_central_url)) {
+        ctx.issues.push({
+          input: values.odk_central_url,
+          path: ['odk_central_url'],
+          message: 'Invalid URL',
+          code: 'custom',
+        });
+      }
+      if (!values.odk_central_user) {
+        ctx.issues.push({
+          input: values.odk_central_user,
+          path: ['odk_central_user'],
+          message: 'ODK Central User is Required',
+          code: 'custom',
+        });
+      }
+      if (!values.odk_central_password) {
+        ctx.issues.push({
+          input: values.odk_central_password,
+          path: ['odk_central_password'],
+          message: 'ODK Central Password is Required',
+          code: 'custom',
+        });
+      }
+    }
     if (values.uploadAreaSelection === 'upload_file' && !values.uploadedAOIFile) {
-      ctx.addIssue({
+      ctx.issues.push({
+        input: values.uploadedAOIFile,
         path: ['uploadedAOIFile'],
         message: 'AOI Geojson File is Required',
-        code: z.ZodIssueCode.custom,
+        code: 'custom',
       });
     }
-
-    // Conditional validation for ODK fields
-    // if (!values.useDefaultODKCredentials) {
-    //   if (!values.odk_central_url) {
-    //     ctx.addIssue({
-    //       path: ['odk_central_url'],
-    //       message: 'ODK URL is Required.',
-    //       code: z.ZodIssueCode.custom,
-    //     });
-    //   } else if (!isValidUrl(values.odk_central_url)) {
-    //     ctx.addIssue({
-    //       path: ['odk_central_url'],
-    //       message: 'Invalid URL.',
-    //       code: z.ZodIssueCode.custom,
-    //     });
-    //   }
-    //   if (!values.odk_central_user) {
-    //     ctx.addIssue({
-    //       path: ['odk_central_user'],
-    //       message: 'ODK Central User is Required.',
-    //       code: z.ZodIssueCode.custom,
-    //     });
-    //   }
-    //   if (!values.odk_central_password) {
-    //     ctx.addIssue({
-    //       path: ['odk_central_password'],
-    //       message: 'ODK Central Password is Required.',
-    //       code: z.ZodIssueCode.custom,
-    //     });
-    //   }
-    // }
-    // // Conditional validation for Custom TMS
-    // if (values.hasCustomTMS) {
-    //   if (!values.custom_tms_url) {
-    //     ctx.addIssue({
-    //       path: ['custom_tms_url'],
-    //       message: 'Custom TMS is Required.',
-    //       code: z.ZodIssueCode.custom,
-    //     });
-    //   } else if (!isValidUrl(values.custom_tms_url)) {
-    //     ctx.addIssue({
-    //       path: ['custom_tms_url'],
-    //       message: 'Invalid Custom TMS URL.',
-    //       code: z.ZodIssueCode.custom,
-    //     });
-    //   }
-    // }
   });
 
-export const projectDetailsValidationSchema = z.object({
-  visibility: z.enum(project_visibility, { error: 'Project Visibility must be selected' }),
-  hashtags: z.array(z.string()),
-  hasCustomTMS: z.boolean(),
-  custom_tms_url: z.string().optional(),
-  per_task_instructions: z.string().optional(),
-  use_odk_collect: z.boolean(),
-});
+export const projectDetailsValidationSchema = z
+  .object({
+    visibility: z.enum(project_visibility, { error: 'Project Visibility must be selected' }),
+    hashtags: z.array(z.string()),
+    hasCustomTMS: z.boolean(),
+    custom_tms_url: z.string().optional(),
+    per_task_instructions: z.string().optional(),
+    use_odk_collect: z.boolean(),
+  })
+  .check((ctx) => {
+    const values = ctx.value;
+    if (values.hasCustomTMS && !values.custom_tms_url) {
+      ctx.issues.push({
+        input: values.custom_tms_url,
+        path: ['custom_tms_url'],
+        message: 'Custom TMS URL is Required',
+        code: 'custom',
+      });
+    }
+  });
 
-export const uploadSurveyValidationSchema = z.object({});
+export const uploadSurveyValidationSchema = z
+  .object({
+    formExampleSelection: z.refine((val) => val, {
+      message: 'Please select a form category',
+    }),
+    xlsFormFile: z.any().optional(),
+    isXlsFormFileValid: z.boolean(),
+  })
+  .check((ctx) => {
+    const values = ctx.value;
+    if (!values.xlsFormFile) {
+      ctx.issues.push({
+        input: values.xlsFormFile,
+        path: ['xlsFormFile'],
+        message: 'File is Required',
+        code: 'custom',
+      });
+    }
+    if (values.xlsFormFile && !values.isXlsFormFileValid) {
+      ctx.issues.push({
+        input: values.xlsFormFile,
+        path: ['xlsFormFile'],
+        message: 'File is Invalid',
+        code: 'custom',
+      });
+    }
+  });
 
 export const mapDataValidationSchema = z.object({});
 
-export const splitTasksValidationSchema = z.object({});
+export const splitTasksValidationSchema = z
+  .object({
+    task_split_type: z.enum(task_split_type, {
+      error: 'Task Split Type is Required',
+    }),
+    dimension: z.number().optional(),
+    average_buildings_per_task: z.number().optional(),
+    splitGeojsonBySquares: z.any().optional(),
+    splitGeojsonByAlgorithm: z.any().optional(),
+  })
+  .check((ctx) => {
+    const values = ctx.value;
+
+    if (
+      values.task_split_type === task_split_type.DIVIDE_ON_SQUARE &&
+      values.dimension !== undefined &&
+      values.dimension < 10
+    ) {
+      ctx.issues.push({
+        minimum: 10,
+        message: 'Dimension must be at least 10',
+        input: values.dimension,
+        code: 'custom',
+        path: ['dimension'],
+      });
+    }
+
+    if (
+      values.task_split_type === task_split_type.TASK_SPLITTING_ALGORITHM &&
+      values.average_buildings_per_task !== undefined &&
+      values.average_buildings_per_task < 1
+    ) {
+      ctx.issues.push({
+        minimum: 1,
+        message: 'Average buildings per task must be greater than 0',
+        input: values.average_buildings_per_task,
+        code: 'custom',
+        path: ['average_buildings_per_task'],
+      });
+    }
+
+    if (values.task_split_type === task_split_type.DIVIDE_ON_SQUARE && !values.splitGeojsonBySquares) {
+      ctx.issues.push({
+        message: 'Please generate the task splitting GeoJSON by squares',
+        input: values.splitGeojsonBySquares,
+        code: 'custom',
+        path: ['splitGeojsonBySquares'],
+      });
+    }
+
+    if (values.task_split_type === task_split_type.TASK_SPLITTING_ALGORITHM && !values.splitGeojsonByAlgorithm) {
+      ctx.issues.push({
+        message: 'Please generate the task splitting GeoJSON by algorithm',
+        input: values.splitGeojsonByAlgorithm,
+        code: 'custom',
+        path: ['splitGeojsonByAlgorithm'],
+      });
+    }
+  });
 
 export const createProjectValidationSchema = {
   ...basicDetailsValidationSchema,
   ...projectDetailsValidationSchema,
+  ...uploadSurveyValidationSchema,
+  ...mapDataValidationSchema,
+  ...splitTasksValidationSchema,
 };
