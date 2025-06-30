4b942b997b2c47429bba20f63e95da4225bb554b
spwoodcock <sam.woodcock@protonmail.com>
2025-06-30 16:05:09 +0100

    feat(backend): add highways xlsform for #2630

--- src/backend/packages/osm-fieldwork/osm_fieldwork/xlsforms/__init__.py
+++ src/backend/packages/osm-fieldwork/osm_fieldwork/xlsforms/__init__.py
@@ -6,3 +6,4 @@ xlsforms_path = os.path.dirname(os.path.abspath(__file__))
 
 buildings = f"{xlsforms_path}/buildings.xls"
 healthcare = f"{xlsforms_path}/health.xls"
+highways = f"{xlsforms_path}/highways.xls"
